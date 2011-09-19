c2502db58eaf168b4b65222930fcabaed295c7bd
JB <jblemetayer@actualys.com>
2011-09-19 15:35:07 +0200

    Ajout de la fonctionnalité de liaison d'interpro au compte

--- project/lib/model/couchdb/_Compte/_Compte.class.php
+++ project/lib/model/couchdb/_Compte/_Compte.class.php
@@ -3,6 +3,9 @@ abstract class _Compte extends Base_Compte {
     const STATUS_NOUVEAU = 'NOUVEAU';
     const STATUS_INSCRIT = 'INSCRIT';
     
+    const STATUT_VALIDATION_ATTENTE = "ATTENTE";
+    const STATUT_VALIDATION_VALIDE = "VALIDE";
+    
     /**
      *
      * @param string $mot_de_passe 
@@ -12,6 +15,21 @@ abstract class _Compte extends Base_Compte {
         $salt = pack("CCCC", mt_rand(), mt_rand(), mt_rand(), mt_rand());
         $hash = "{SSHA}" . base64_encode(pack("H*", sha1($mot_de_passe . $salt)) . $salt);
         $this->_set('mot_de_passe', $hash);
+    }
+    /**
+     * 
+     */
+    public function getNbChaiByInterproId() {
+        $result = array();
+        foreach ($this->getTiers() as $tier) {
+            if (array_key_exists($tier->getInterpro(), $result)) {
+                $result[$tier->getInterpro()] = $result[$tier->getInterpro()] + 1;
+            }
+            else {
+                $result[$tier->getInterpro()] = 1;
+            }
+        }
+        return $result;
     }
     
 }
