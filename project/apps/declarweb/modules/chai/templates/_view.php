055fe8e7c80ac1387d6951d691d4293fa0e8c3fe
Tangui Morlier <tangui@tangui.eu.org>
2011-09-19 18:45:46 +0200

    Affichage de l'interpro Siret que si chai géré par interpro courante

--- project/apps/declarweb/modules/chai/templates/_view.php
+++ project/apps/declarweb/modules/chai/templates/_view.php
@@ -1,6 +1,7 @@
 <p><span>Nom : </span><?php echo $chai->getNom() ?></p>
-<p><span>Siret : </span><?php echo $chai->getSiret() ?></p>
+<p><span>Interpro : </span><?php echo $chai->getInterproObj()->nom; ?></p>
 <?php if ($interpro->get('_id') == $chai->getInterpro()): ?>
+<p><span>Siret : </span><?php echo $chai->getSiret() ?></p>
 <div class="btn">
     <a href="<?php echo url_for('compte_update_chai', array('id' => $chai->getIdentifiant())) ?>" class="modifier">Modifier</a>
 </div>
