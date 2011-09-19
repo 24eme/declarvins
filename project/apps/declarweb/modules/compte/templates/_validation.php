44b28a77bfb85a4af003353a8bdd81286cfe1698
Tangui Morlier <tangui@tangui.eu.org>
2011-09-19 17:09:57 +0200

    Validation de compte + messages

--- project/apps/declarweb/modules/compte/templates/_validation.php
+++ project/apps/declarweb/modules/compte/templates/_validation.php
@@ -1,9 +1,22 @@
-<?php echo $message ?>
-<?php if (!$valide): ?>
+<p><?php 
+if (!$valide_interpro) {
+  echo "Vous n'avez pas validé ce compte pour votre interpro.";
+ }else{
+  echo "Vous avez validé ce compte pour votre interpro.";
+ }
+?></p>
+<p><?php
+if (!$compte_active) {
+  echo "Ce compte n'est pas activé : toutes les interpros ne l'ont pas validé.";
+ }else{
+  echo "Ce compte est activé : toutes les interpros l'ont validé.";
+ }
+?></p>
+<?php if (!$valide_interpro && !$compte_active): ?>
 <form method="post" action="<?php echo url_for('@compte_validation') ?>">
 <input type="hidden" name="interpro_id" value="<?php echo $interpro->get('_id') ?>" />
 <div class="btnValidation">
-    <input type="image" src="/images/boutons/btn_valider.png" alt="Valider" />
+    <input type="submit" value="Valider" />
 </div>
 </form>
 <?php endif; ?>
