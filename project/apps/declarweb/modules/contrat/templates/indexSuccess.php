99af7583adb19386b976ea702ad8c54e7551e625
Tangui Morlier <tangui@teymour.(none)>
2011-09-19 18:43:55 +0200

    Présentation de l'écran d'accueil (wording)

--- project/apps/declarweb/modules/contrat/templates/indexSuccess.php
+++ project/apps/declarweb/modules/contrat/templates/indexSuccess.php
@@ -1,5 +1,5 @@
 <div class="clearfix" id="application_dr">
-    <h2 class="titre_principal">Identification</h2>
+    <h2 class="titre_principal">Consultation/Validation d'un contrat</h2>
     <div id="mon_compte">
         <?php include_partial('contrat/form', array('form' => $formLogin))?>
     </div>
