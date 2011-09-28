f41132cceff592a25b256e1a6271e22ec38b6fc8
JB <jblemetayer@actualys.com>
2011-09-28 14:27:47 +0200

    Clean du code

--- project/apps/declarweb/modules/contrat/actions/actions.class.php
+++ project/apps/declarweb/modules/contrat/actions/actions.class.php
@@ -10,27 +10,6 @@
  */
 class contratActions extends sfActions
 {
- /**
-  * Executes index action
-  *
-  * @param sfRequest $request A request object
-  */
-  public function executeIndex(sfWebRequest $request)
-  {
-    $this->formLogin = new LoginForm();
-    
-        
-    if ($request->isMethod(sfWebRequest::POST)) {
-
-        $this->formLogin->bind($request->getParameter($this->formLogin->getName()));
-        if ($this->formLogin->isValid()) {
-            $values = $this->formLogin->getValues();
-            $this->getUser()->setAttribute('interpro_id', $values['interpro']);
-            $this->getUser()->setAttribute('contrat_id', $values['contrat']);
-            $this->redirect('@compte');
-        }
-    }
-  }
 
     /**
      * 
