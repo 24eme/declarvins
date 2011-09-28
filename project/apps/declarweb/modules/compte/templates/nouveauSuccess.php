a107d334433719d725e87e7b202641a7ae7cd2e3
JB <jblemetayer@actualys.com>
2011-09-28 14:27:03 +0200

    Ajout de la fonctionnalité de création de compte

--- project/apps/declarweb/modules/compte/templates/nouveauSuccess.php
+++ project/apps/declarweb/modules/compte/templates/nouveauSuccess.php
@@ -1,2 +1,40 @@
 <h1>Etape 3 : Création de compte</h1>
 
+<?php if($sf_user->hasFlash('notice')) : ?>
+<p class="flash_message"><?php echo $sf_user->getFlash('notice'); ?></p>
+<?php endif; ?>
+<form method="post" action="<?php echo url_for('@compte_nouveau') ?>">
+	<div class="ligne_form">
+	<?php echo $form->renderHiddenFields(); ?>
+	<?php echo $form->renderGlobalErrors(); ?>
+	
+	<?php echo $form['login']->renderError() ?>
+	<?php echo $form['login']->renderLabel() ?>
+	<?php echo $form['login']->render() ?>
+	</div>
+	<div class="ligne_form">
+	<?php echo $form['mdp1']->renderError() ?>
+	<?php echo $form['mdp1']->renderLabel() ?>
+	<?php echo $form['mdp1']->render() ?>
+	</div>
+	<div class="ligne_form">
+	<?php echo $form['mdp2']->renderError() ?>
+	<?php echo $form['mdp2']->renderLabel() ?>
+	<?php echo $form['mdp2']->render() ?>
+	</div>
+	<div class="ligne_form">
+	<?php echo $form['email1']->renderError() ?>
+	<?php echo $form['email1']->renderLabel() ?>
+	<?php echo $form['email1']->render() ?>
+	</div>
+	<div class="ligne_form">
+	<?php echo $form['email2']->renderError() ?>
+	<?php echo $form['email2']->renderLabel() ?>
+	<?php echo $form['email2']->render() ?>
+	</div>
+
+	<div class="btn">
+		<input type="submit" value="Valider" />
+	</div>
+</form>
+
--- project/lib/form/compte/CompteTiersAjoutForm.class.php
+++ project/lib/form/compte/CompteTiersAjoutForm.class.php
@@ -1,29 +1,11 @@
 <?php
 class CompteTiersAjoutForm extends CompteForm {
-
-    public function configure() {
-        parent::configure();
-		$this->setWidget('nom', new sfWidgetFormInputText());
-		$this->setWidget('prenom', new sfWidgetFormInputText());
-		$this->setWidget('fonction', new sfWidgetFormInputText());
-		$this->setWidget('telephone', new sfWidgetFormInputText());
-		$this->setWidget('fax', new sfWidgetFormInputText());
-		
-		$this->getWidget('nom')->setLabel('Nom*: ');
-		$this->getWidget('prenom')->setLabel('Prénom*: ');
-		$this->getWidget('fonction')->setLabel('Fonction*: ');
-		$this->getWidget('telephone')->setLabel('Téléphone*: ');
-		$this->getWidget('fax')->setLabel('Fax: ');
-		
-		$this->setValidator('nom', new sfValidatorString(array('required' => true), array('required' => 'Champ obligatoire')));
-		$this->setValidator('prenom', new sfValidatorString(array('required' => true), array('required' => 'Champ obligatoire')));
-		$this->setValidator('fonction', new sfValidatorString(array('required' => true), array('required' => 'Champ obligatoire')));
-		$this->setValidator('telephone', new sfValidatorString(array('required' => true), array('required' => 'Champ obligatoire')));
-		$this->setValidator('fax', new sfValidatorString(array('required' => false)));
-        
-        $this->widgetSchema->setNameFormat('comptetiers[%s]');
-
-    }
+	
+	public function configure() {
+		if (!$this->getOption('contrat'))
+			throw new sfException('l\'objet contrat doit être passé au CompteTiersAjoutForm');
+		parent::configure();
+	}
     
     public function doUpdateObject($values) {
         parent::doUpdateObject($values);
@@ -31,6 +13,13 @@ class CompteTiersAjoutForm extends CompteForm {
         {
             $this->getObject()->set('_id', 'COMPTE-'.$this->getObject()->getLogin());
             $this->getObject()->setPasswordSSHA($values['mdp1']);
+            // Duplication des infos du contrat dans le compte
+            $contrat = $this->getOption('contrat');
+            $this->getObject()->setNom($contrat->getNom());
+            $this->getObject()->setPrenom($contrat->getPrenom());
+            $this->getObject()->setFonction($contrat->getFonction());
+            $this->getObject()->setTelephone($contrat->getTelephone());
+            $this->getObject()->setFax($contrat->getFax());
         }
     }
 
