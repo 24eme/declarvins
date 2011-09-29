ae4f62efd7ba28da3fd5be9a4fd392447feabe30
JB <jblemetayer@actualys.com>
2011-09-29 10:27:00 +0200

    Clean des fichiers qui ne sont plus utilisés dans la nouvelle version du processus de création de contrat

--- project/apps/declarweb/modules/contrat/templates/indexSuccess.php
+++
@@ -1,6 +0,0 @@
-<div class="clearfix" id="application_dr">
-    <h2 class="titre_principal">Consultation/Validation d'un contrat</h2>
-    <div id="mon_compte">
-        <?php include_partial('contrat/form', array('form' => $formLogin))?>
-    </div>
-</div>
