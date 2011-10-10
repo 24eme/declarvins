95adea88d270ee3bf0b2ad8a351192556a078ffa
JB <jblemetayer@actualys.com>
2011-10-10 11:59:59 +0200

    Ajout de la librairie printable pour dompdf

---
+++ project/apps/declarweb/lib/printable/PageableOutput.class.php
@@ -0,0 +1,38 @@
+<?php
+
+class PageableOutput {
+
+  protected $title;
+  protected $link;
+  protected $subtitle;
+  protected $filename;
+  protected $file_dir;
+  
+
+  public function __construct($title, $subtitle, $filename = '', $file_dir = null, $link = ' de ') {
+    $this->title = $title;
+    $this->link = $link;
+    $this->subtitle = $subtitle;
+    $this->filename = $filename;
+    $this->file_dir = $file_dir;
+    $this->init();
+  }
+
+  public function addHtml($html) {
+  }
+
+  public function output() {
+  }
+
+  public function isCached() {
+  }
+
+  public function removeCache() {
+    return true;
+  }
+
+  public function generatePDF($no_cache = false) {
+  }
+
+}
+
