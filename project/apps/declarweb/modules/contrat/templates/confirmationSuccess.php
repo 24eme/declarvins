8fbb92fec11be6445c765427fe443102ac16cae1
JB <jblemetayer@actualys.com>
2011-10-11 14:21:21 +0200

    On retire le target _blank du lien de génération du PDF

--- project/apps/declarweb/modules/contrat/templates/confirmationSuccess.php
+++ project/apps/declarweb/modules/contrat/templates/confirmationSuccess.php
@@ -1,4 +1,4 @@
 <h1>Confirmation</h1>
 <p>Merci,<br />Vous allez recevoir un e-mail contenant votre contrat en pièce jointe.</p>
 <br />
-<a href="<?php echo url_for('@contrat_pdf') ?>" target="_blank">Pdf</a>
+<a href="<?php echo url_for('@contrat_pdf') ?>">Pdf</a>
