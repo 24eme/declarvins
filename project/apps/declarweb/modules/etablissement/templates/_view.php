15c7018e4f043238669df680e47cfd7a5c067695
JB <jblemetayer@actualys.com>
2011-10-12 18:50:53 +0200

    Ajout de la fonctionnalité lier / delier

--- project/apps/declarweb/modules/etablissement/templates/_view.php
+++ project/apps/declarweb/modules/etablissement/templates/_view.php
@@ -2,4 +2,10 @@
 <p><span>Interpro : </span><?php //echo $etablissement->getInterproObj()->nom; ?></p>
 <?php if ($interpro->get('_id') == $etablissement->getInterpro()): ?>
 <p><span>Siret : </span><?php echo $etablissement->getSiret() ?></p>
+<div class="btn">
+	<?php if ($etablissement->statut != _Tiers::STATUT_ARCHIVER): ?>
+    <a href="<?php echo url_for('validation_archiver', array('etablissement' => $etablissement->getIdentifiant())) ?>" class="modifier">Archiver</a> | 
+    <?php endif; ?>
+    <a href="<?php echo url_for('validation_delier', array('etablissement' => $etablissement->getIdentifiant())) ?>" class="modifier">Délier</a>
+</div>
 <?php endif; ?>
