1855f8f3ebd5d27b8404cfb6482ab79ef90051ef
Vincent LAURENT <vlaurent@actualys.com>
2011-10-13 20:34:41 +0200

    Création d'un compte, gestion suppression/annulation etablissement + ajustement intégration html

--- project/apps/declarweb/modules/contrat/templates/modificationEtablissementSuccess.php
+++ project/apps/declarweb/modules/contrat/templates/modificationEtablissementSuccess.php
@@ -15,7 +15,7 @@
                 <h1>Étape 2 : <strong>Création de compte</strong></h1>
 
                 <h2>Veuillez saisir les informations pour l'établissement <?php echo $etablissement->getKey() + 1; ?></h2>    
-                <?php include_partial('contrat/formEtablissement', array('form' => $form)); ?>
+                <?php include_partial('contrat/formEtablissement', array('form' => $form, 'new' => false, "recapitulatif" => ($recapitulatif == 1))); ?>
             </form>
             <?php
         endif;
