4bf136ce355eb49d6ea5e504a5b8d770a6500290
JB <jblemetayer@actualys.com>
2011-10-13 14:48:26 +0200

    Modifs des infos à afficher pour les etablissements

--- project/lib/model/couchdb/Etablissement/Etablissement.class.php
+++ project/lib/model/couchdb/Etablissement/Etablissement.class.php
@@ -1,4 +1,17 @@
 <?php
 class Etablissement extends BaseEtablissement {
     
+    protected $_interpro = null;
+    
+    /**
+     * @return _Compte
+     */
+    public function getInterproObject() {
+        if (is_null($this->_interpro)) {
+            $this->_interpro = InterproClient::getInstance()->retrieveDocumentById($this->interpro);
+        }
+        
+        return $this->_interpro;
+    }
+    
 }
