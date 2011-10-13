11da9166944c1d440b300a2ae83282d38c7edb57
Vincent LAURENT <vlaurent@actualys.com>
2011-10-13 19:32:36 +0200

    Intégration HTML de l'étape 2 de la création du contrat

--- project/apps/declarweb/modules/contrat/templates/nouveauEtablissementSuccess.php
+++ project/apps/declarweb/modules/contrat/templates/nouveauEtablissementSuccess.php
@@ -1,115 +1,16 @@
 <script type="text/javascript">
-	var familles = '<?php echo json_encode(sfConfig::get('app_etablissements_familles')) ?>';
+    var familles = '<?php echo json_encode(sfConfig::get('app_etablissements_familles')) ?>';
 </script>
-<?php if($sf_user->hasFlash('success')) : ?>
-<p class="flash_message"><?php echo $sf_user->getFlash('success'); ?></p>
+<?php if ($sf_user->hasFlash('success')) : ?>
+    <p class="flash_message"><?php echo $sf_user->getFlash('success'); ?></p>
 <?php endif; ?>
-<p>Nouvel établissement</p>
-<form method="post" action="<?php echo url_for('@contrat_etablissement_nouveau'); ?>">
-	<div class="ligne_form ligne_form_label">
-	<?php echo $form->renderHiddenFields(); ?>
-	<?php echo $form->renderGlobalErrors(); ?>
 
-	<?php echo $form['raison_sociale']->renderError() ?>
-	<?php echo $form['raison_sociale']->renderLabel() ?>
-	<?php echo $form['raison_sociale']->render() ?>
-	</div>
-	<div class="ligne_form ligne_form_label">
-	<?php echo $form['siret']->renderError() ?>
-	<?php echo $form['siret']->renderLabel() ?>
-	<?php echo $form['siret']->render() ?>
-	</div>
-	<div class="ligne_form ligne_form_label">
-	<?php echo $form['cni']->renderError() ?>
-	<?php echo $form['cni']->renderLabel() ?>
-	<?php echo $form['cni']->render() ?>
-	</div>
-	<div class="ligne_form ligne_form_label">
-	<?php echo $form['cvi']->renderError() ?>
-	<?php echo $form['cvi']->renderLabel() ?>
-	<?php echo $form['cvi']->render() ?>
-	</div>
-	<div class="ligne_form ligne_form_label">
-	<?php echo $form['no_accises']->renderError() ?>
-	<?php echo $form['no_accises']->renderLabel() ?>
-	<?php echo $form['no_accises']->render() ?>
-	</div>
-	<div class="ligne_form ligne_form_label">
-	<?php echo $form['no_tva_intracommunautaire']->renderError() ?>
-	<?php echo $form['no_tva_intracommunautaire']->renderLabel() ?>
-	<?php echo $form['no_tva_intracommunautaire']->render() ?>
-	</div>
-	<div class="ligne_form ligne_form_label">
-	<?php echo $form['adresse']->renderError() ?>
-	<?php echo $form['adresse']->renderLabel() ?>
-	<?php echo $form['adresse']->render() ?>
-	</div>
-	<div class="ligne_form ligne_form_label">
-	<?php echo $form['code_postal']->renderError() ?>
-	<?php echo $form['code_postal']->renderLabel() ?>
-	<?php echo $form['code_postal']->render() ?>
-	</div>
-	<div class="ligne_form ligne_form_label">
-	<?php echo $form['commune']->renderError() ?>
-	<?php echo $form['commune']->renderLabel() ?>
-	<?php echo $form['commune']->render() ?>
-	</div>
-	<div class="ligne_form ligne_form_label">
-	<?php echo $form['telephone']->renderError() ?>
-	<?php echo $form['telephone']->renderLabel() ?>
-	<?php echo $form['telephone']->render() ?>
-	</div>
-	<div class="ligne_form ligne_form_label">
-	<?php echo $form['fax']->renderError() ?>
-	<?php echo $form['fax']->renderLabel() ?>
-	<?php echo $form['fax']->render() ?>
-	</div>
-	<div class="ligne_form ligne_form_label">
-	<?php echo $form['email']->renderError() ?>
-	<?php echo $form['email']->renderLabel() ?>
-	<?php echo $form['email']->render() ?>
-	</div>
-	<div class="ligne_form ligne_form_label">
-	<?php echo $form['famille']->renderError() ?>
-	<?php echo $form['famille']->renderLabel() ?>
-	<?php echo $form['famille']->render() ?>
-	</div>
-	<div class="ligne_form ligne_form_label">
-	<?php echo $form['sous_famille']->renderError() ?>
-	<?php echo $form['sous_famille']->renderLabel() ?>
-	<?php echo $form['sous_famille']->render() ?>
-	</div>
-	<div class="ligne_form ligne_form_label">
-	<?php echo $form['service_douane']->renderError() ?>
-	<?php echo $form['service_douane']->renderLabel() ?>
-	<?php echo $form['service_douane']->render() ?>
-	</div>
-	
-	<div class="ligne_form ligne_form_label">
-		<span>L'adresse de votre comptabilité est-elle différente de la précédente ?</span>
-		<input type="radio" id="r1" name="adresse_comptabilite" value="Oui" onclick="voirFormAdresseComptabilite()" />
-		<label for="r1">Oui</label>
-		<input type="radio" id="r2" name="adresse_comptabilite" value="Non" onclick="masquerFormAdresseComptabilite()" checked="checked" />
-		<label for="r2">Non</label>
-	</div>
-	<div id="adresseComptabilite" style="display:none;">
-		<div class="ligne_form ligne_form_label">
-		<?php echo $form['comptabilite_adresse']->renderError() ?>
-		<?php echo $form['comptabilite_adresse']->renderLabel() ?>
-		<?php echo $form['comptabilite_adresse']->render() ?>
-		</div>
-		<div class="ligne_form ligne_form_label">
-		<?php echo $form['comptabilite_code_postal']->renderError() ?>
-		<?php echo $form['comptabilite_code_postal']->renderLabel() ?>
-		<?php echo $form['comptabilite_code_postal']->render() ?>
-		</div>
-		<div class="ligne_form ligne_form_label">
-		<?php echo $form['comptabilite_commune']->renderError() ?>
-		<?php echo $form['comptabilite_commune']->renderLabel() ?>
-		<?php echo $form['comptabilite_commune']->render() ?>
-		</div>
-	</div>
-	<div class="btn">
-		<input type="submit" value="Ajouter" />
-	</div>
-</form>
+<!-- #contenu -->
+<section id="contenu">
+    <form id="creation_compte" action="<?php echo url_for('@contrat_etablissement_nouveau'); ?>" method="post">
+        <h1>Étape 2 : <strong>Création de compte</strong></h1>
+
+        <h2>Veuillez saisir les informations pour le nouvel établissement</h2>
+        <?php include_partial('contrat/formEtablissement', array('form' => $form)); ?>
+    </form>
+</section>
