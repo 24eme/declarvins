84b8ae1aa2dd83d68a9d6e07cf938186a0642cf5
Vincent LAURENT <vince.laurent@gmail.com>
2011-10-14 14:58:45 +0200

    Tache d'import des douanes

---
+++ project/lib/task/importDouaneTask.class.php
@@ -0,0 +1,65 @@
+<?php
+
+class importDouaneTask extends sfBaseTask
+{
+  protected function configure()
+  {
+    // // add your own arguments here
+    // $this->addArguments(array(
+    //   new sfCommandArgument('my_arg', sfCommandArgument::REQUIRED, 'My argument'),
+    // ));
+
+    $this->addOptions(array(
+      new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name'),
+      new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+      new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'default'),
+      // add your own options here
+    ));
+
+    $this->namespace        = 'import';
+    $this->name             = 'douane';
+    $this->briefDescription = '';
+    $this->detailedDescription = <<<EOF
+The [importDouane|INFO] task does things.
+Call it with:
+
+  [php symfony importDouane|INFO]
+EOF;
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    // initialize the database connection
+    $databaseManager = new sfDatabaseManager($this->configuration);
+    $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
+    
+    $douanes = sfCouchdbManager::getClient("Douane")->getAll();
+    foreach($douanes as $douane) {
+        $douane->delete();
+    }
+    
+    $douanes = array(
+        "Aix-en-Provence",
+        "Brignoles",
+        "Draguignan",
+        "Châteauneuf du Pape",
+        "Cairanne",
+        "Portes les Valence",
+        "Tain l'Hermitage",
+        "Bagnols sur Ceze",
+        "Nîmes",
+        "Privas",
+        "Orange",
+        "Sablet",
+        "Saint Egreve");
+    
+    foreach($douanes as $nom) {
+        $douane = new Douane();
+        $douane->set('_id', "DOUANE-".strtoupper(str_replace(array(" ","â", "-", "î", "'") ,array("", "a", "", "i", "") ,$nom)));
+        $douane->nom = $nom;
+        $douane->save();
+    }
+
+    // add your code here
+  }
+}
