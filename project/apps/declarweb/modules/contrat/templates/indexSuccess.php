33f983a10b8e731ced6d52cb557edcbcfc28e599
JB <jblemetayer@actualys.com>
2011-10-17 14:59:29 +0200

    Template de la page explicative

---
+++ project/apps/declarweb/modules/contrat/templates/indexSuccess.php
@@ -0,0 +1,10 @@
+<section id="contenu">
+    <div id="creation_compte">
+        <h1>Lorem ipsum</h1>
+        <p>
+        	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut arcu sit amet eros pharetra condimentum. Mauris pretium est quis dolor suscipit tristique. Cras sit amet sodales ligula. Suspendisse purus magna, posuere et rhoncus a, lacinia id massa. Proin facilisis dapibus metus. Nunc bibendum, metus laoreet auctor aliquam, orci metus placerat massa, ac tristique elit dui id nisi. Pellentesque et neque in diam sagittis euismod at id purus. Quisque gravida sollicitudin convallis. Integer lectus felis, feugiat vel pulvinar ac, commodo vitae ante. Curabitur tristique ullamcorper nisl vitae mollis. 
+        	<br /><br />
+        	<a href="<?php echo url_for('@contrat_nouveau') ?>">Je déclare en ligne</a>
+        </p>
+    </div>
+</section>
