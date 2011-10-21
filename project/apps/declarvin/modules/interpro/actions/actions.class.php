eea8f94bcb943ececbae1db6c8cb27d14ac5ef54
JB <jblemetayer@actualys.com>
2011-10-21 14:32:35 +0200

    Modification du nom du projet declarweb en declarvin

--- project/apps/declarvin/modules/interpro/actions/actions.class.php
+++ project/apps/declarvin/modules/interpro/actions/actions.class.php
@@ -3,7 +3,7 @@
 /**
  * interpro actions.
  *
- * @package    declarweb
+ * @package    declarvin
  * @subpackage interpro
  * @author     Your name here
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
