18c81dcdff0c350b87b8bcb0d7732ad430dc585a
JB <jblemetayer@actualys.com>
2011-10-21 14:32:35 +0200

    Modification du nom du projet declarweb en declarvin

--- project/apps/declarvin/modules/etablissement/actions/actions.class.php
+++ project/apps/declarvin/modules/etablissement/actions/actions.class.php
@@ -3,7 +3,7 @@
 /**
  * etablissement actions.
  *
- * @package    declarweb
+ * @package    declarvin
  * @subpackage etablissement
  * @author     Your name here
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
--- project/apps/declarvin/modules/validation/actions/actions.class.php
+++ project/apps/declarvin/modules/validation/actions/actions.class.php
@@ -3,7 +3,7 @@
 /**
  * validation actions.
  *
- * @package    declarweb
+ * @package    declarvin
  * @subpackage validation
  * @author     Your name here
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
