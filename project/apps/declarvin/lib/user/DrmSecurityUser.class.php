1f6cb94432ed6ede4b3f537f8f016731cceb28c7
JB <jblemetayer@actualys.com>
2011-11-02 18:41:47 +0100

    Gestion des sessions et credentials

---
+++ project/apps/declarvin/lib/user/DrmSecurityUser.class.php
@@ -0,0 +1,138 @@
+<?php
+
+abstract class DrmSecurityUser extends TiersSecurityUser {
+    
+	const CREDENTIAL_DRM = 'drm';
+    const CREDENTIAL_DRM_EN_COURS = 'drm_en_cours';
+    const CREDENTIAL_DRM_VALIDE = 'drm_valide';
+    
+    protected $_credentials_drm = array(self::CREDENTIAL_DRM,
+    									self::CREDENTIAL_DRM_EN_COURS, 
+                                        self::CREDENTIAL_DRM_VALIDE);
+    
+    protected $_drm = null;
+    
+    /**
+     *
+     * @param sfEventDispatcher $dispatcher
+     * @param sfStorage $storage
+     * @param type $options 
+     */
+    public function initialize(sfEventDispatcher $dispatcher, sfStorage $storage, $options = array()) {
+        parent::initialize($dispatcher, $storage, $options);
+        
+        if (!$this->isAuthenticated())
+        {
+            $this->signOutDrm();
+        }
+    }
+    
+    /**
+    * 
+    */
+    protected function clearCredentialsDrm() {
+        foreach($this->_credentials_drm as $credential) {
+            $this->removeCredential($credential);
+        }
+    }
+    
+    /**
+     * 
+     */
+    public function signOutDrm() {
+        $this->_drm = null;
+        $this->clearCredentialsDrm();
+    }
+    
+    /**
+     * @return DR
+     */
+    public function getDrm() {
+        $this->requireDrm();
+        $this->requireTiers();
+        if (is_null($this->_drm)) {
+            $this->_drm = $this->getTiers()->getDrm($this->getCampagne());
+            if (!$this->_drm) {
+                $drm = new DRM();
+                $drm->set('_id', 'DRM-'.$this->getTiers()->identifiant.'-'.$this->getCampagne());
+                return $drm;
+            }
+        }
+
+        return $this->_drm;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getCampagneDrm() {
+      return CurrentClient::getCurrent()->campagne;
+    }
+    /**
+     * returns true if editable
+     */
+    public function isDrmEditable() {
+    	return true;
+    }
+
+    /**
+     * 
+     */
+    public function initCredentialsDrm() {
+        $this->requireDrm();
+        $drm = $this->getDrm();
+        $this->clearCredentialsDrm();
+        /*if ($this->isDrmEditable()) {
+            if ($drm->isValideeTiers() || $drm->isValideeCiva()) {
+                $this->addCredential(self::CREDENTIAL_DECLARATION_VALIDE);
+            } else {
+                $this->addCredential(self::CREDENTIAL_DECLARATION_EN_COURS);
+                $this->addCredentialsEtapeDrm();
+            }
+        }*/
+    }
+    
+    /**
+     * 
+     */
+    protected function requireDrm() {
+        $this->requireTiers();
+        if (!$this->hasCredential(self::CREDENTIAL_DRM)) {
+            throw new sfException("you must be logged in with a tiers");
+        }
+    }
+    
+    /**
+     *
+     * @param _Tiers $tiers 
+     */
+    public function signInTiers($tiers) {
+        parent::signInTiers($tiers);
+        if($this->hasCredential(myUser::CREDENTIAL_DRM)) {
+            $this->initCredentialsDrm();
+        }
+    }
+
+    /**
+     *
+     * @param string $namespace 
+     */
+    public function signOutCompte($namespace) {
+        $this->signOutDrm();
+        parent::signOutCompte($namespace);
+    }
+
+    /**
+     * 
+     */
+    public function signOutTiers() {
+        $this->signOutDrm();
+        parent::signOutTiers();
+    }
+    
+    public function removeDrm() {
+    	$this->getDrm()->delete();
+        $this->signOutDrm();
+        $this->initCredentialsDrm();
+    }
+}
