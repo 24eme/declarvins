43e18e8d16bdaff682dcf4b439067764516976e1
JB <jblemetayer@actualys.com>
2011-11-03 14:20:33 +0100

    Ajout de la page de selection d'établissement

---
+++ project/apps/declarvin/modules/etablissement/templates/selectionEtablissementSuccess.php
@@ -0,0 +1,16 @@
+<section id="contenu">
+	<?php include_partial('global/navTop') ?>
+	<div id="creation_compte" style="width:70%; float: left;">
+		<h1>Sélection des établissements</h1>
+		<p>Accédez ici au formulaire de DRM, à l'historique de vos DRM ou demandez à procéder à une DRM rectificative</p>
+		<span>Attention, vous n'avez plus que 4 jours pour soumettre votre DRM du mois en cours</span>
+		<br /><br />
+		<ul>
+			<li><a href="#">Etablissement 1</a></li>
+			<li><a href="#">Etablissement 2</a></li>
+			<li><a href="#">Etablissement 3</a></li>
+		</ul>
+	</div>
+	<?php include_partial('global/aides') ?>
+	<div style="clear:both;">&nbsp;</div>
+</section>
---
+++ project/apps/declarvin/templates/_navTop.php
@@ -0,0 +1,22 @@
+<div style="width: 100%">
+<ul style="width: 70%; float: left;">
+	<li style="display: inline;"><a href="#">Accueil |</a></li>
+	<li style="display: inline;"><a href="#">&nbsp;Mon profil |</a></li>
+	<li style="display: inline;"><a href="#">&nbsp;Vrac |</a></li>
+	<li style="display: inline;"><a href="#">&nbsp;DRM |</a></li>
+	<li style="display: inline;"><a href="#">&nbsp;DAI/DS |</a></li>
+	<li style="display: inline;"><a href="#">&nbsp;DR |</a></li>
+	<li style="display: inline;"><a href="#">&nbsp;Divers</a></li>
+</ul>
+<ul style="width: 30%; float: right;display: inline;">
+	<li style="display: inline;">
+		<select>
+			<option value="etablissement1">Etablissement 1</option>
+			<option value="etablissement2">Etablissement 2</option>
+			<option value="etablissement3">Etablissement 3</option>
+		</select>
+	</li>
+	<li style="display: inline;"><a href="<?php echo url_for('@etablissement_selection') ?>">Sortir</a></li>
+</ul>
+<div style="clear: both">&nbsp;</div>
+</div>
