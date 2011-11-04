9042e6ebf26a8c127e9de948358dadfcdbc46dca
JB <jblemetayer@actualys.com>
2011-11-04 11:08:41 +0100

    Ajout du formulaire de login admin

---
+++ project/lib/form/admin/AdminCompteLoginForm.class.php
@@ -0,0 +1,40 @@
+<?php
+
+class AdminCompteLoginForm extends BaseForm {
+
+    /**
+     * 
+     */
+    public function configure() {
+        $this->setWidgets(array(
+                'login'   => new sfWidgetFormInputText(),
+        ));
+
+        $this->widgetSchema->setLabels(array(
+                'login'  => 'Login : ',
+        ));
+
+        $this->setValidators(array(
+                'login' => new sfValidatorString(array('required' => true)),
+        ));
+        
+        $this->widgetSchema->setNameFormat('admin[%s]');
+
+        $this->validatorSchema['login']->setMessage('required', 'Champ obligatoire');
+        $this->validatorSchema->setPostValidator(new ValidatorAdminCompteLogin());
+    }
+
+    /**
+     * 
+     * @return _Tiers;
+     */
+    public function process() {
+        if ($this->isValid()) {
+            return $this->getValue('compte');
+        } else {
+            throw new sfException("must be valid");
+        }
+    }
+
+}
+
