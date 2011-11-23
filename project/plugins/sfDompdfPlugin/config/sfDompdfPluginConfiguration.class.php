c3b3021361b6b110d1daf835d7412313083d0ca0
JB <jblemetayer@actualys.com>
2011-11-23 14:57:05 +0100

    Remplacement du sfDompdf par acDompdf

--- project/plugins/sfDompdfPlugin/config/sfDompdfPluginConfiguration.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class sfDompdfPluginConfiguration extends sfPluginConfiguration
-{
-  public function initialize()
-  {
-    sfConfig::set('sf_autoloading_functions', array(array('sfDOMPDFBridge', 'autoload')));
-  }
-}
