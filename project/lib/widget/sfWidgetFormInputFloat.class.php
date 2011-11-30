ec0220b3ebd5c1752b8384c709e392b3c0d2676a
Vincent LAURENT <vince.laurent@gmail.com>
2011-11-30 14:39:27 +0100

    Widget pour les nombres décimaux à deux chiffres

---
+++ project/lib/widget/sfWidgetFormInputFloat.class.php
@@ -0,0 +1,13 @@
+<?php
+
+class sfWidgetFormInputFloat extends sfWidgetFormInputText
+{
+  public function render($name, $value = null, $attributes = array(), $errors = array())
+  {
+    sfApplicationConfiguration::getActive()->loadHelpers('civa');
+    $value = sprintFloat($value);
+    $attributes['autocomplete'] = 'off';
+    //$attributes['class'] = 'num';
+    return parent::render($name, $value, $attributes, $errors);
+  }
+}
