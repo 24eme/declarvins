086f31a9a307daac604018cee53f1d0a7fc24c64
Vincent LAURENT <vince.laurent@gmail.com>
2011-12-02 15:07:26 +0100

    modules drm_recap

--- project/plugins/DrmPlugin/modules/drm_recap/actions/actions.class.php
+++ project/plugins/DrmPlugin/modules/drm_recap/actions/actions.class.php
@@ -4,10 +4,12 @@ class drm_recapActions extends sfActions
 {
     
     public function executeIndex(sfWebRequest $request) {
-        $this->appellation = $this->getRoute()->getObject();
+        $this->forward('drm_recap', 'appellation');
     }
     
-    public function executeAppellationAjout(sfWebRequest $request) {
+    public function executeAppellationAjoutAjax(sfWebRequest $request) {
+        $this->forward404Unless($request->isXmlHttpRequest());
+        
         $drm = $this->getUser()->getDrm();
         $this->label = $this->getRoute()->getObject();
 
@@ -17,9 +19,54 @@ class drm_recapActions extends sfActions
             $this->form->bind($request->getParameter($this->form->getName()));
             if ($this->form->isValid()) {
                 $this->form->save();
-                $this->redirect('drm_recap_appellation_ajout', $this->label);
+                return $this->renderText(json_encode(array("success" => true, 
+                                                           "url" => $this->generateUrl('drm_recap_appellation', $this->form->getAppellation()))));
             }
         }
+
+        return $this->renderText(json_encode(array("success" => false, 
+                                                       "content" => $this->getPartial('drm_recap/popupAppellationAjout', array('label' => $this->label, 'form' => $this->form)))));
+    }
+    
+    public function executeAppellation(sfWebRequest $request) {
+        $this->init();
+    }
+    
+    public function executeAjout(sfWebRequest $request) {
+        
+        $this->init();
+        $this->drm_appellation->couleurs->add('rouge')->details->add();
+
+        $this->setTemplate('appellation');
+    }
+    
+    public function executeUpdate(sfWebRequest $request) {
+        $this->init();
+  
+        $this->form = new DRMDetailForm($this->getRoute()->getDrmDetail());
+        $this->form->bind($request->getParameter($this->form->getName()));
+        
+        if($this->form->isValid()) {
+            $this->form->save();
+            if ($request->isXmlHttpRequest()) {
+                return $this->renderText(json_encode(array("success" => true, "content" => "")));
+            } else {
+                $this->redirect('drm_recap_appellation', $this->config_appellation);
+            }
+        }
+        
+        if ($request->isXmlHttpRequest()) {
+            return $this->renderText(json_encode(array("success" => false, "content" => $this->getPartial('drm_recap/itemFormErrors', array('form' => $this->form)))));
+        } else {
+            $this->setTemplate('appellation');
+        }
+    }
+    
+    protected function init() {
+        $this->form = null;
+        $this->form_appellation_ajout = null;
+        $this->config_appellation = $this->getRoute()->getConfigAppellation();
+        $this->drm_appellation = $this->getRoute()->getDrmAppellation();
     }
     
 }
