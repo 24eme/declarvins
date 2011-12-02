7c95a30119214797baf245a5dec7a988049cbc3b
Vincent LAURENT <vince.laurent@gmail.com>
2011-12-02 15:07:26 +0100

    modules drm_recap

---
+++ project/plugins/DrmPlugin/modules/drm_recap/templates/_itemForm.php
@@ -0,0 +1,6 @@
+<div style="float: left;">
+    <form class="form_detail" action="<?php echo url_for('drm_recap_update', $form->getObject()) ?>" method="post">
+        <?php echo $form ?>
+        <input type="submit" valud="Valider" />
+    </form>
+</div>
---
+++ project/plugins/DrmPlugin/modules/drm_recap/templates/_ongletsAppellations.php
@@ -0,0 +1,14 @@
+<ul>
+    <li>
+<?php foreach($config_appellation->getLabel()->appellations as $appellation): ?>
+    <?php if($drm_appellation->getLabel()->appellations->exist($appellation->getKey())): ?>
+        <a href="<?php echo url_for('drm_recap_appellation', $appellation) ?>" <?php if($appellation->getKey() == $config_appellation->getKey()): ?>style="font-weight:bold;"<?php endif; ?>>
+            <?php echo $appellation->libelle ?>
+        </a>
+    <?php endif; ?>
+<?php endforeach; ?>
+    </li>
+</ul>
+
+<a href="#">Ajouter une appellation</a>
+  
