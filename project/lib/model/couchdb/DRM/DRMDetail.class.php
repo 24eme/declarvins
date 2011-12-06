d2f1c3966f10989edbde9ae3fa88bdfca98d284f
Vincent LAURENT <vince.laurent@gmail.com>
2011-12-06 11:37:20 +0100

    Modification du modèle DRM

--- project/lib/model/couchdb/DRM/DRMDetail.class.php
+++ project/lib/model/couchdb/DRM/DRMDetail.class.php
@@ -1,10 +1,12 @@
 <?php
+
 /**
  * Model for DRMDetail
  *
  */
-
 class DRMDetail extends BaseDRMDetail {
+
+    protected $_couleur = null;
     
     /**
      *
@@ -13,4 +15,38 @@ class DRMDetail extends BaseDRMDetail {
     public function getCouleur() {
         return $this->getParent()->getParent();
     }
+
+    /**
+     *
+     * @return DRMCouleur
+     */
+    public function getAppellation() {
+        return $this->getCouleur()->getAppellation();
+    }
+
+    public function updateProduit($produit = null) {
+        if (is_null($produit)) {
+            $produit = $this->getDocument()->produits->add($this->getAppellation()->getLabel()->getKey())->add();
+        }
+
+        $produit->denomination = $this->denomination;
+        $produit->label = $this->label;
+        $produit->couleur = $this->getCouleurValue();
+        $produit->appellation = $this->getAppellation()->getKey();
+        
+        return $this;
+    }
+    
+    public function getCouleurValue() {
+        if (is_null($this->_couleur)) {
+            return $this->getCouleur()->getKey();
+        }
+        
+        return $this->_couleur;
+    }
+    
+    public function setCouleurValue($value) {
+       $this->_couleur = $value;
+    }
+
 }
