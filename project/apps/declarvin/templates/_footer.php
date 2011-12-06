1b982a9989b1cc38ba8546aebef81257f4585c54
Vincent LAURENT <vince.laurent@gmail.com>
2011-12-06 11:31:59 +0100

    Templates DRM modifications

---
+++ project/apps/declarvin/templates/_footer.php
@@ -0,0 +1,10 @@
+		<!-- #footer -->
+		<footer id="footer">
+			<p>Copyright 2011 - <a href="#">Plan du site</a> - <a href="#">Contact</a> - <a href="#">Mentions légales</a> - <a href="#">Crédits</a></p>
+			<ul id="logos_footer">
+				<li><a href="#"><img src="/images/visuels/logo_inter_rhone.png" alt="Inter Rhône" /></a></li>
+				<li><a href="#"><img src="/images/visuels/logo_inter_vins.png" alt="Inter Vins" /></a></li>
+				<li><a href="#"><img src="/images/visuels/logo_vins_provence.png" alt="Vins de Provence" /></a></li>
+			</ul>
+		</footer>
+		<!-- fin #footer -->
