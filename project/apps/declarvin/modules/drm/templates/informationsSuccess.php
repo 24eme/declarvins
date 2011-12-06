8a3a7885b1b3d6fa4e58576d7f08b07a48402ccb
Vincent LAURENT <vince.laurent@gmail.com>
2011-12-06 11:31:59 +0100

    Templates DRM modifications

--- project/apps/declarvin/modules/drm/templates/informationsSuccess.php
+++ project/apps/declarvin/modules/drm/templates/informationsSuccess.php
@@ -1,34 +1,37 @@
+<?php include_partial('global/navTop'); ?>
+
 <section id="contenu">
-	<?php include_partial('global/navTop', array('active' => 'drm')) ?>
-	<div id="creation_compte" style="width:100%; float: left;">
-		<h1>Déclaration Récapitulative Mensuelle</h1>
-		<p>DRM 2011 - MARS</p>
-		<br />
-		<?php include_partial('drm/etapes', array('active' => 'informations', 'pourcentage' => '5')) ?>
-		<p>Veuillez tout d'abord confirmer les informations ci-dessous :</p>
-		<ul style="width: 780px; float:left; margin: 50px;">
-			<li style="width: 50%; float:left; margin: 5px 0;"><b>CVI :</b></li>
-			<li style="width: 50%; float:left; margin: 5px 0;"><?php echo $tiers->cvi ?>&nbsp;</li>
-			<li style="width: 50%; float:left; margin: 5px 0;"><b>N° SIRET :</b></li>
-			<li style="width: 50%; float:left; margin: 5px 0;"><?php echo $tiers->siret ?>&nbsp;</li>
-			<li style="width: 50%; float:left; margin: 5px 0;"><b>N° entrepositaire agréé :</b></li>
-			<li style="width: 50%; float:left; margin: 5px 0;"><?php echo $tiers->no_tva_intracommunautaire ?>&nbsp;</li>
-			<li style="width: 50%; float:left; margin: 5px 0;"><b>Nom / Raison Sociale / Adresse :</b></li>
-			<li style="width: 50%; float:left; margin: 5px 0;"><?php echo $tiers->nom ?>,<br /><?php echo $tiers->siege->adresse ?><br /><?php echo $tiers->siege->code_postal ?> <?php echo $tiers->siege->commune ?>&nbsp;</li>
-			<li style="width: 50%; float:left; margin: 5px 0;"><b>Lieu ou est tenu la comptabilité matière :</b></li>
-			<li style="width: 50%; float:left; margin: 5px 0;"><?php if (!$tiers->comptabilite->adresse): ?>IDEM<?php else: ?><?php echo $tiers->comptabilite->adresse ?><br /><?php echo $tiers->comptabilite->code_postal ?> <?php echo $tiers->comptabilite->commune ?><?php endif; ?>&nbsp;</li>
-			<li style="width: 50%; float:left; margin: 5px 0;"><b>Service des douanes :</b></li>
-			<li style="width: 50%; float:left; margin: 5px 0;"><?php echo $tiers->service_douane ?>&nbsp;</li>
-			<li style="width: 50%; float:left; margin: 5px 0;"><b>Adresse et n° du Chai :</b></li>
-			<li style="width: 50%; float:left; margin: 5px 0;">?</li>
-			<li style="width: 50%; float:left; margin: 5px 0;"><b>Caution RR :</b></li>
-			<li style="width: 50%; float:left; margin: 5px 0;">?</li>
-			<li style="width: 50%; float:left; margin: 5px 0;"><b>Je confirme l'exactitude de ces informations :</b></li>
-			<li style="width: 50%; float:left; margin: 5px 0;"><input type="checkbox" /></li>
-			<li style="width: 50%; float:left; margin: 5px 0;"><b>Je souhaite modifier mes informations de structure :</b></li>
-			<li style="width: 50%; float:left; margin: 5px 0;"><input type="checkbox" /></li>
-			<li style="width: 50%; float:left; margin: 5px 0;"><input type="button" value="Valider &raquo;" onclick="javascript:document.location.href='<?php echo url_for('@drm_mouvements_generaux') ?>'" /></li>
-		</ul>
-	</div>
-	<div style="clear:both;">&nbsp;</div>
+
+    <?php include_partial('drm_global/header'); ?>
+    <?php include_component('drm_global', 'etapes', array('etape' => 'informations', 'pourcentage' => '5')); ?>
+
+    <!-- #principal -->
+    <section id="principal">
+
+        <p>Veuillez tout d'abord confirmer les informations ci-dessous :</p>
+        <ul style="width: 780px; float:left; margin: 20px 30px 30px 50px;">
+            <li style="width: 50%; float:left; margin: 5px 0;"><b>CVI :</b></li>
+            <li style="width: 50%; float:left; margin: 5px 0;"><?php echo $tiers->cvi ?>&nbsp;</li>
+            <li style="width: 50%; float:left; margin: 5px 0;"><b>N° SIRET :</b></li>
+            <li style="width: 50%; float:left; margin: 5px 0;"><?php echo $tiers->siret ?>&nbsp;</li>
+            <li style="width: 50%; float:left; margin: 5px 0;"><b>N° entrepositaire agréé :</b></li>
+            <li style="width: 50%; float:left; margin: 5px 0;"><?php echo $tiers->no_tva_intracommunautaire ?>&nbsp;</li>
+            <li style="width: 50%; float:left; margin: 5px 0;"><b>Nom / Raison Sociale / Adresse :</b></li>
+            <li style="width: 50%; float:left; margin: 5px 0;"><?php echo $tiers->nom ?>,<br /><?php echo $tiers->siege->adresse ?><br /><?php echo $tiers->siege->code_postal ?> <?php echo $tiers->siege->commune ?>&nbsp;</li>
+            <li style="width: 50%; float:left; margin: 5px 0;"><b>Lieu ou est tenu la comptabilité matière :</b></li>
+            <li style="width: 50%; float:left; margin: 5px 0;"><?php if (!$tiers->comptabilite->adresse): ?>IDEM<?php else: ?><?php echo $tiers->comptabilite->adresse ?><br /><?php echo $tiers->comptabilite->code_postal ?> <?php echo $tiers->comptabilite->commune ?><?php endif; ?>&nbsp;</li>
+            <li style="width: 50%; float:left; margin: 5px 0;"><b>Service des douanes :</b></li>
+            <li style="width: 50%; float:left; margin: 5px 0;"><?php echo $tiers->service_douane ?>&nbsp;</li>
+            <li style="width: 50%; float:left; margin: 5px 0;"><b>Numéro d'Accise</b></li>
+            <li style="width: 50%; float:left; margin: 5px 0;">16879908</li>
+            <li style="width: 50%; float:left; margin: 40px 0 5px 0;"><b>Je confirme l'exactitude de ces informations :</b></li>
+            <li style="width: 50%; float:left;margin: 40px 0 5px 0;"><input type="radio" value="1" name="confirm" /></li>
+            <li style="width: 50%; float:left; margin: 5px 0;"><b>Je souhaite modifier mes informations de structure :</b></li>
+            <li style="width: 50%; float:left; margin: 5px 0;"><input type="radio" value="0" name="confirm" /></li>
+        </ul>
+
+        <div id="btn_etape_dr">
+            <a href="<?php echo url_for('drm_mouvements_generaux') ?>" class="btn_suiv">Valider</a>
+        </div>
+    </section>
 </section>
--- project/apps/declarvin/modules/drm/templates/monEspaceSuccess.php
+++ project/apps/declarvin/modules/drm/templates/monEspaceSuccess.php
@@ -1,19 +1,23 @@
+<?php include_partial('global/navTop'); ?>
+
 <section id="contenu">
-	<?php include_partial('global/navTop', array('active' => 'drm')) ?>
-	<div id="creation_compte" style="width:70%; float: left;">
-		<h1>Déclaration Récapitulative Mensuelle</h1>
-		<p>Bienvenue sur votre espace DRM. Que pensez-vous faire ?</p>
-		<br /><br />
-		<ul>
-			<li><a href="#">Accédez à mon historique de DRM &raquo;</a></li>
-                        <?php if($sf_user->getDrm()->isNew()): ?>
-			<li><a href="<?php echo url_for('@drm_init') ?>">Commencer ma DRM &raquo;</a></li>
-                        <?php else: ?>
-                        <li><a href="<?php echo url_for('@drm_init') ?>">Continuer ma DRM en cours &raquo;</a></li>
-                        <?php endif; ?>
-			<li><a href="#">Soumettre une DRM récapitulative &raquo;</a></li>
-		</ul>
-	</div>
-	<?php include_partial('global/aides') ?>
-	<div style="clear:both;">&nbsp;</div>
+    <h1>Déclaration Récapitulative Mensuelle</h1>
+    <p id="date_drm">Bienvenue sur votre espace DRM. Que pensez-vous faire ?</p>
+    <?php //include_partial('global/navTop', array('active' => 'drm')) ?>
+    <div id="creation_compte" style="width:70%; float: left;">
+
+        <br /><br />
+        <ul>
+                <!--<li><a href="#">Accédez à mon historique de DRM &raquo;</a></li>-->
+        <?php if ($sf_user->getDrm()->isNew()): ?>
+                    <li><a href="<?php echo url_for('@drm_init') ?>">Commencer ma DRM &raquo;</a></li>
+        <?php else: ?>
+                    <li><a href="<?php echo url_for('@drm_init') ?>">Continuer ma DRM en cours &raquo;</a></li>
+        <?php endif; ?>
+                <!--<li><a href="#">Soumettre une DRM récapitulative &raquo;</a></li>-->
+        </ul>
+
+    </div>
+    <?php include_partial('global/aides') ?>
+    <div style="clear:both;">&nbsp;</div>
 </section>
