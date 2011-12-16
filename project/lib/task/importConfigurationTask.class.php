eedad18cda4dd41e9e2ededdf4a0479ff26e0c5a
JB <jblemetayer@actualys.com>
2011-12-16 17:41:39 +0100

    Mise à jour de la configuration en prenant en compte les labels

--- project/lib/task/importConfigurationTask.class.php
+++ project/lib/task/importConfigurationTask.class.php
@@ -6,7 +6,7 @@ class importConfigurationTask extends sfBaseTask
   {
     // // add your own arguments here
     $this->addArguments(array(
-       new sfCommandArgument('file', sfCommandArgument::REQUIRED, 'Appellations File'),
+       new sfCommandArgument('file', sfCommandArgument::OPTIONAL, 'Appellations File'),
     ));
 
     $this->addOptions(array(
@@ -40,23 +40,39 @@ EOF;
     $current = new Current();
     $current->campagne = '2011-11';
     $current->save();
-    
-    if ($configuration = acCouchdbManager::getClient()->retrieveDocumentById('CONFIGURATION')) {
-        $configuration->delete();
+    $configuration = acCouchdbManager::getClient()->retrieveDocumentById('CONFIGURATION');
+    if (isset($arguments['file']) && !empty($arguments['file'])) {
+	    if ($configuration) {
+	        $configuration->delete();
+	    }
+	    
+	    $configuration = new Configuration();
+	    $aop = $configuration->declaration->labels->add('AOP')->libelle = 'AOP';
+	    $igp = $configuration->declaration->labels->add('IGP')->libelle = 'IGP';
+	    $vinsansig = $configuration->declaration->labels->add('VINSSANSIG')->libelle = "Vins sans IG";
+	    foreach (file($arguments['file']) as $a) {
+	        $datas = explode(";", $a);
+	        $configuration->declaration->labels->get($datas[0])->appellations->add(str_replace("\n", "", $datas[2]))->libelle = $datas[1];
+	    }
+    	$configuration->save();
+    } elseif ($configuration) {
+    	$configuration->label->add('AB', 'Agriculture Biologique');
+    	$configuration->label->add('AR', 'Agriculture Raisonnée');
+    	$configuration->label->add('BD', 'Biodynamie');
+    	$configuration->label->add('AC', 'Agri confiance');
+    	$configuration->label->add('TV', 'Terra Vitis');
+    	$configuration->label->add('DD', 'Vigneron développement durable');
+    	$configuration->label->add('NMP', 'Nutrition Méditérannéenne en Provence');
+    	$configuration->label->add('HVE', 'Haute Valeur Environnementale');
+    	$configuration->label->add('FU', 'Elevage en fût');
+    	$configuration->label->add('DO', 'Domaine');
+    	$configuration->label->add('CH', 'Château');
+    	$configuration->label->add('CL', 'Clos');
+    	$configuration->label->add('CC', 'Cru Classé');
+    	$configuration->label->add('BT', 'Mise en bouteille ("conditionné") à la propriété');
+    	$configuration->save();
+    } else {
+    	$this->logSection("configuration", "Appellation file needed", null, 'ERROR');
     }
-    
-    $configuration = new Configuration();
-    $aop = $configuration->declaration->labels->add('AOP')->libelle = 'AOP';
-    $igp = $configuration->declaration->labels->add('IGP')->libelle = 'IGP';
-    $vinsansig = $configuration->declaration->labels->add('VINSSANSIG');
-    $vinsansig->libelle = 'Vins sans IG';
-    $vinsansig->appellations->add("VINSSANSIG")->libelle = "Vins sans IG";
-   
-    foreach (file($arguments['file']) as $a) {
-        $datas = explode(";", $a);
-        $configuration->declaration->labels->get($datas[0])->appellations->add(str_replace("\n", "", $datas[2]))->libelle = $datas[1];
-    }
-    
-    $configuration->save();
   }
 }
