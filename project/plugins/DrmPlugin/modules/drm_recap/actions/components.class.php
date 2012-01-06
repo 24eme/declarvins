6b51246bb7278626a4aa834927d0bc2c56ff26d2
JB <jblemetayer@actualys.com>
2012-01-06 11:36:44 +0100

    Calcul du nombre de detail complété pour les appellations

--- project/plugins/DrmPlugin/modules/drm_recap/actions/components.class.php
+++ project/plugins/DrmPlugin/modules/drm_recap/actions/components.class.php
@@ -16,6 +16,7 @@ class drm_recapComponents extends sfComponents {
     
     public function executeOnglets() {
         $this->appellations = array();
+        $this->appellations_updated = array();
         foreach($this->getUser()->getDrm()->produits->get($this->config_appellation->getLabel()->getKey()) as $appellation) {
         	foreach ($this->getUser()->getDrm()->produits->get($this->config_appellation->getLabel()->getKey())->get($appellation->getKey()) as $produit) {
 	            if ($produit->stock_vide) {
@@ -23,10 +24,20 @@ class drm_recapComponents extends sfComponents {
 	            }
 	            if (!array_key_exists($produit->getAppellation()->getKey(), $this->appellations)) {
 	                $this->appellations[$produit->getAppellation()->getKey()] = 0;
+	                $this->appellations_updated[$produit->getAppellation()->getKey()] = 0;
 	            }
 	            
 	            if (!$produit->pas_de_mouvement) {
-	                 $this->appellations[$produit->getAppellation()->getKey()] += 1;
+	                $this->appellations[$produit->getAppellation()->getKey()] += 1;
+		            foreach ($this->getUser()->getDrm()->declaration->certifications->get($this->config_appellation->getLabel()->getKey())->appellations->get($appellation->getKey())->couleurs as $couleur) {
+		            	foreach ($couleur->cepages as $cepage) {
+		            		foreach ($cepage->details as $detail) {
+		            			if ($detail->total_entrees || $detail->total_sorties) {
+		            				$this->appellations_updated[$produit->getAppellation()->getKey()] += 1;
+		            			}
+		            		}
+		            	}
+		            }
 	            }
         	}
         }
