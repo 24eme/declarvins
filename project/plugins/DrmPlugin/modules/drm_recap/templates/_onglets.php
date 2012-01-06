6cc310602ae232146db94db84a446410be7f40fe
JB <jblemetayer@actualys.com>
2012-01-06 11:36:44 +0100

    Calcul du nombre de detail complété pour les appellations

--- project/plugins/DrmPlugin/modules/drm_recap/templates/_onglets.php
+++ project/plugins/DrmPlugin/modules/drm_recap/templates/_onglets.php
@@ -5,14 +5,14 @@
             <li class="actif">
                 <strong>
                     <?php echo $appellation->libelle ?> 
-                    <span class="completion completion_validee">(0/<?php echo $appellations[$appellation_key] ?>)</span>
+                    <span class="completion completion_validee">(<?php echo $appellations_updated[$appellation_key] ?>/<?php echo $appellations[$appellation_key] ?>)</span>
                 </strong>
             </li>
         <?php else: ?>
             <li>
                 <a href="<?php echo url_for('drm_recap_appellation', $appellation) ?>">
                     <?php echo $appellation->libelle ?> 
-                    <span class="completion">(0/<?php echo $appellations[$appellation_key] ?>)</span>
+                    <span class="completion">(<?php echo $appellations_updated[$appellation_key] ?>/<?php echo $appellations[$appellation_key] ?>)</span>
                 </a>
             </li>
         <?php endif; ?>
