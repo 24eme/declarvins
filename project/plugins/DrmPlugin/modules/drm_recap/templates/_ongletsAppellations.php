771a775a9f67f8520da27dd2eb35d08ec0996680
JB <jblemetayer@actualys.com>
2012-01-10 18:16:55 +0100

    Ajout de l'intégration et de la fonctionnalité de suppression d'un item sur la page des mouvements generaux

--- project/plugins/DrmPlugin/modules/drm_recap/templates/_ongletsAppellations.php
+++ project/plugins/DrmPlugin/modules/drm_recap/templates/_ongletsAppellations.php
@@ -2,7 +2,7 @@
     <li>
 <?php foreach($config_appellation->getLabel()->appellations as $appellation): ?>
     <?php if($drm_appellation->getLabel()->appellations->exist($appellation->getKey())): ?>
-        <a href="<?php echo url_for('drm_recap_appellation', $appellation) ?>" <?php if($appellation->getKey() == $config_appellation->getKey()): ?>style="font-weight:bold;"<?php endif; ?>>
+        <a href="<?php echo url_for('drm_recap_appellation', $appellation) ?>" <?php if($appellation->getKey() == $config_appellation->getKey()): ?>class="actif"<?php endif; ?>>
             <?php echo $appellation->libelle ?>
         </a>
     <?php endif; ?>
