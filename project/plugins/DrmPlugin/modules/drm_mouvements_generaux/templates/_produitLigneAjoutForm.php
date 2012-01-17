a54056965aaecac7f5c59f60ef9b935c184213b5
JB <jblemetayer@actualys.com>
2012-01-17 19:16:00 +0100

    Correction des bugs sur la popup d'ajout de produit

--- project/plugins/DrmPlugin/modules/drm_mouvements_generaux/templates/_produitLigneAjoutForm.php
+++ project/plugins/DrmPlugin/modules/drm_mouvements_generaux/templates/_produitLigneAjoutForm.php
@@ -1,4 +1,4 @@
-<div id="popup_ajout_produit" class="popup_contenu">
+<div id="popup_ajout_produit_<?php echo $certification ?>" class="popup_contenu">
 	<form  class="popup_form" id="subForm" action="<?php echo url_for('@drm_mouvements_generaux_product_form') ?>" method="post">
 		<?php echo $form->renderGlobalErrors() ?>
 		<?php echo $form->renderHiddenFields() ?>
@@ -15,28 +15,15 @@
 		</div>
 		<div class="ligne_form">
 			<label for="produit_label"><?php echo $form['label']->renderLabel() ?> </label>
-			<?php echo $form['label']->render(array('class' => 'select_multiple')) ?>
+			<div style="width: 240px; height: 100px; display: inline-block; overflow-x: hidden; overflow-y: scroll;">
+				<?php echo $form['label']->render(array('class' => 'select_multiple')) ?>
+			</div>
 			<span class="error"><?php echo $form['label']->renderError() ?></span>
 		</div>
 		<div class="ligne_form">
 			<label for="produit_label_supplementaire"><?php echo $form['label_supplementaire']->renderLabel() ?> </label>
 			<?php echo $form['label_supplementaire']->render() ?>
 			<span class="error"><?php echo $form['label_supplementaire']->renderError() ?></span>
-		</div>
-		<div class="ligne_form">
-			<label for="produit_disponible"><?php echo $form['disponible']->renderLabel() ?> </label>
-			<?php echo $form['disponible']->render(array('class' => 'num num_float')) ?>
-			<span class="error"><?php echo $form['disponible']->renderError() ?></span>
-		</div>
-		<div class="ligne_form">
-			<label for="produit_stock_vide"><?php echo $form['stock_vide']->renderLabel() ?> </label>
-			<?php echo $form['stock_vide']->render(array()) ?>
-			<span class="error"><?php echo $form['stock_vide']->renderError() ?></span>
-		</div>
-		<div class="ligne_form">
-			<label for="produit_pas_de_mouvement"><?php echo $form['pas_de_mouvement']->renderLabel() ?> </label>
-			<?php echo $form['pas_de_mouvement']->render(array()) ?>
-			<span class="error"><?php echo $form['pas_de_mouvement']->renderError() ?></span>
 		</div>
 		<div class="ligne_form_btn">
 			<button name="annuler" class="btn_annuler btn_fermer" type="reset">Annuler</button>
