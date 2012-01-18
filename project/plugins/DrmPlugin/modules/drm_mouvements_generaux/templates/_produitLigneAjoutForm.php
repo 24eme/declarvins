e4d4fdcd4c91b44ff9f4aa8b6b0668c1d57c84fc
Vincent LAURENT <vlaurent@actualys.com>
2012-01-18 21:18:21 +0100

    Correction  du bug des labels imbriqués

--- project/plugins/DrmPlugin/modules/drm_mouvements_generaux/templates/_produitLigneAjoutForm.php
+++ project/plugins/DrmPlugin/modules/drm_mouvements_generaux/templates/_produitLigneAjoutForm.php
@@ -4,24 +4,24 @@
 		<?php echo $form->renderHiddenFields() ?>
 		<input type="hidden" name="certification" value="<?php echo $certification ?>" />
 		<div class="ligne_form">
-			<label for="produit_appellation"><?php echo $form['appellation']->renderLabel() ?> </label>
+			<?php echo $form['appellation']->renderLabel() ?>
 			<?php echo $form['appellation']->render() ?>
 			<span class="error"><?php echo $form['appellation']->renderError() ?></span>
 		</div>
 		<div class="ligne_form">
-			<label for="produit_couleur"><?php echo $form['couleur']->renderLabel() ?> </label>
+			<?php echo $form['couleur']->renderLabel() ?>
 			<?php echo $form['couleur']->render() ?>
 			<span class="error"><?php echo $form['couleur']->renderError() ?></span>
 		</div>
 		<div class="ligne_form">
-			<label for="produit_label"><?php echo $form['label']->renderLabel() ?> </label>
+			<?php echo $form['label']->renderLabel() ?>
 			<div style="width: 240px; height: 100px; display: inline-block; overflow-x: hidden; overflow-y: scroll;">
 				<?php echo $form['label']->render(array('class' => 'select_multiple')) ?>
 			</div>
 			<span class="error"><?php echo $form['label']->renderError() ?></span>
 		</div>
 		<div class="ligne_form">
-			<label for="produit_label_supplementaire"><?php echo $form['label_supplementaire']->renderLabel() ?> </label>
+			<?php echo $form['label_supplementaire']->renderLabel() ?>
 			<?php echo $form['label_supplementaire']->render() ?>
 			<span class="error"><?php echo $form['label_supplementaire']->renderError() ?></span>
 		</div>
