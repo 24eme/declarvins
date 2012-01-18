8b1e1365c94a8aa72e893d83f66367ed6b4f762e
Vincent LAURENT <vlaurent@actualys.com>
2012-01-18 12:54:28 +0100

    Suppression du controle sur le label supplementaire dans le form

--- project/plugins/DrmPlugin/lib/validator/DRMLabelValidator.class.php
+++ project/plugins/DrmPlugin/lib/validator/DRMLabelValidator.class.php
@@ -4,32 +4,24 @@ class DRMLabelValidator extends sfValidatorSchema
     public function configure($options = array(), $messages = array()) 
     {
     	$this->addRequiredOption('object');
-        $this->addOption('label_supplementaire_field', 'label_supplementaire');
         $this->addOption('throw_global_error', false);
-        $this->setMessage('invalid', 'Si vous ne selectionnez pas de label, vous devez spécifier un label supplémentaire');
         $this->addMessage('exist', 'Ce(s) label(s) existe(nt) déjà pour cette appellation dans cette couleur');
     }
 
     protected function doClean($values) 
     {
-        if ((isset($values['label']) && !empty($values['label'])) || (isset($values['label_supplementaire']) && !empty($values['label_supplementaire']))) {
-        	if ($this->existDetail($values)) {
-        		if ($this->getOption('throw_global_error')) {
-		            throw new sfValidatorError($this, 'exist');
-		        }
-		        throw new sfValidatorErrorSchema($this, array($this->getOption('label_supplementaire_field') => new sfValidatorError($this, 'exist')));
-        	}
-			return $values;
-        }
-        if ($this->getOption('throw_global_error')) {
-            throw new sfValidatorError($this, 'invalid');
-        }
-        throw new sfValidatorErrorSchema($this, array($this->getOption('label_supplementaire_field') => new sfValidatorError($this, 'invalid')));
+    	if ($this->existDetail($values)) {
+    		if ($this->getOption('throw_global_error')) {
+	            throw new sfValidatorError($this, 'exist');
+	        }
+	        throw new sfValidatorErrorSchema($this, array('label' => new sfValidatorError($this, 'exist')));
+    	}
+        return $values;
     }
     
     protected function existDetail($values) 
     {
-    	return $this->getObject()->getDocument()->exist('declaration/certifications/'.$this->getObject()->getCertification()->getKey().'/appellations/'.$values['appellation'].'/couleurs/'.$values['couleur'].'/cepages/'.$values['cepage'].'/details/'.$this->getLabelKey($values['label']));    
+    	return $this->getObject()->getDocument()->exist('declaration/certifications/'.$this->getObject()->getCertification()->getKey().'/appellations/'.$values['appellation'].'/couleurs/'.$values['couleur'].'/cepages/'.$values['cepage'].'/details/'.$this->getLabelKey($sfValidatorErroralues['label']));    
     }
 
     protected function getObject() 
