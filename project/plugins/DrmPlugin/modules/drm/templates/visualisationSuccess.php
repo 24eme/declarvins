f76292b9ac4256d5f8f53cec9a6291f697084554
Vincent LAURENT <vlaurent@actualys.com>
2012-03-20 17:35:49 +0100

    Mise en forme de la visualisation

--- project/plugins/DrmPlugin/modules/drm/templates/visualisationSuccess.php
+++ project/plugins/DrmPlugin/modules/drm/templates/visualisationSuccess.php
@@ -13,25 +13,47 @@
             <?php include_partial('drm/recap', array('drm' => $drm)) ?>
         </div>
 
-    	<h2>Droits total</h2>
-    	<p><span>CVO: </span><?php echo echoFloat($drm->getTotalCvo()) ?>€</p>
-    	<p><span>Douane: </span><?php echo echoFloat($drm->getTotalDouane()) ?>€</p>
-    	<h2>Droits par code</h2>
-    	<table>
-    		<tr>
-    			<th align="left" width="150px">Code</th>
-    			<th align="left" width="80px">CVO</th>
-    			<th align="left" width="80px">Douane</th>
-    		</tr>
-    		<?php foreach ($drm->getTotalDroitByCode() as $code => $value): $value = $value->getRawValue(); ?>
-    		<tr>
-    			<td><?php echo $code ?></td>
-    			<td><?php echo (isset($value['cvo']))? echoFloat($value['cvo']) : echoFloat(0.00); ?></td>
-    			<td><?php echo (isset($value['douane']))? echoFloat($value['douane']) : echoFloat(0.00); ?></td>
-    		</tr>
-    		<?php endforeach; ?>
-    	</table>
-    	<a href="<?php echo url_for('drm_pdf', array('campagne_rectificative' => $drm->getCampagneAndRectificative())) ?>">Pdf</a>
+        <div id="contenu_onglet">
+            <div class="tableau_ajouts_liquidations">
+                <h2>Droits totaux</h2>
+                <table class="tableau_recap" style="width: 300px;">
+                    <tbody>
+                        <tr class="alt">
+                            <td>CVO</td>
+                            <td><?php echo echoFloat($drm->getTotalCvo()) ?>€</td>
+                        </tr>
+                        <tr class="alt">
+                            <td>Douane</td>
+                            <td><?php echo echoFloat($drm->getTotalDouane()) ?>€</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="tableau_ajouts_liquidations">
+                <h2>Droits par code</h2>
+                <table class="tableau_recap">
+                    <thead>
+                        <tr>
+                            <th>Code</th>
+                            <th>CVO</th>
+                            <th>Douane</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($drm->getTotalDroitByCode() as $code => $value): $value = $value->getRawValue(); ?>
+                        <tr class="alt">
+                            <td><?php echo $code ?></td>
+                            <td><?php echo (isset($value['cvo']))? echoFloat($value['cvo']) : echoFloat(0.00); ?></td>
+                            <td><?php echo (isset($value['douane']))? echoFloat($value['douane']) : echoFloat(0.00); ?></td>
+                        </tr>
+                        <?php endforeach; ?>
+                    <tbody>
+                </table>
+            </div>
+        </div>
+
+    	<a href="<?php echo url_for('drm_pdf', array('campagne_rectificative' => $drm->getCampagneAndRectificative())) ?>">Télécharger le PDF</a>
+
         <div id="btn_etape_dr">
             <?php if($drm_suivante && $drm_suivante->isRectificative()): ?>
             <a href="<?php echo url_for('drm_init', array('campagne_rectificative' => $drm_suivante->getCampagneAndRectificative())) ?>" class="btn_suiv">
