d03f546888598228be20e31c2b6bc3537ab36693
JB <jblemetayer@actualys.com>
2012-04-05 14:48:28 +0200

    suppression du module drm_login que l'on utilise plus

--- project/plugins/DrmPlugin/lib/routing/DrmRouting.class.php
+++ project/plugins/DrmPlugin/lib/routing/DrmRouting.class.php
@@ -34,8 +34,6 @@ class DrmRouting {
     static public function listenToRoutingLoadConfigurationEvent(sfEvent $event) {
         $r = $event->getSubject();
         
-		$r->prependRoute('drm_login', new sfRoute('/drm/login', array('module' => 'drm_login', 
-                                                                                'action' => 'index')));
 		
         $r->prependRoute('drm_mon_espace', new sfRoute('/drm/mon-espace', array('module' => 'drm', 
                                                                                 'action' => 'monEspace')));
