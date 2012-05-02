4f4a850b78de08c9493db771d0875a0d4449d837
JB <jblemetayer@actualys.com>
2012-05-02 19:18:48 +0200

    Condition suppression DRM pour admin si pas envoyée

--- project/plugins/DrmPlugin/modules/drm/templates/_historiqueItem.php
+++ project/plugins/DrmPlugin/modules/drm/templates/_historiqueItem.php
@@ -10,7 +10,7 @@
 	<td>En cours</td>
 	<td>
 		<a href="<?php echo url_for('drm_init', array('campagne_rectificative' => $campagne_rectificative)); ?>">Accéder à la déclaration en cours</a><br />
-	   </td><td style="border: 0px; padding-left: 0px;">
+	   </td><td style="border: 0px; padding-left: 0px;background-color: #ffffff;">
 		<a href="<?php echo url_for('drm_delete', array('campagne_rectificative' => $campagne_rectificative)); ?>" class="btn_reinitialiser"><span><img src="/images/pictos/pi_supprimer.png"/></span></a>
 	</td>
 <?php else: ?>
@@ -23,6 +23,11 @@
 		<br />
 		<?php endif; ?>
 			<a href="<?php echo url_for('drm_visualisation', array('campagne_rectificative' => $campagne_rectificative)) ?>" class="btn_reinitialiser"><span>Visualiser</span></a>
-		</td>							
+		</td>	
+		<?php if ($sf_user->hasCredential(myUser::CREDENTIAL_ADMIN) && !$drm->isEnvoyee()): ?>	
+	<td style="border: 0px; padding-left: 0px;background-color: #ffffff;">
+		<a href="<?php echo url_for('drm_delete', array('campagne_rectificative' => $campagne_rectificative)); ?>" class="btn_reinitialiser"><span><img src="/images/pictos/pi_supprimer.png"/></span></a>
+	</td>
+	<?php endif; ?>					
 	<?php endif; ?>
 </tr>
