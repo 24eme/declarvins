0ed2f2760cf7e4d750919d1f97e465b3d3d38fde
JB <jblemetayer@actualys.com>
2012-05-03 14:18:49 +0200

    Suppression du champs prenom

--- lib/model/acVinCompte/bridge/_Compte.class.php
+++ lib/model/acVinCompte/bridge/_Compte.class.php
@@ -3,6 +3,6 @@
 abstract class _Compte extends acVinCompte {
 	public function getGecos() 
     {
-      return $this->login.', '.$this->prenom.' '.$this->nom;
+      return $this->login.', '.$this->nom;
     }
 }
