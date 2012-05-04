c0afae831e83191293ece4d48f3cda774f694320
Charlotte Devichet <cdevichet@actualys.com>
2012-05-04 10:03:39 +0000

    messages d'aide

--- project/plugins/DrmPlugin/modules/drm_mouvements_generaux/templates/_popupAjout.php
+++ project/plugins/DrmPlugin/modules/drm_mouvements_generaux/templates/_popupAjout.php
@@ -1,4 +1,4 @@
 <div id="popup_ajout_produit_<?php echo $certification ?>" class="popup_contenu">
-        <p><?php echo acCouchdbManager::getClient('Messages')->getMessage('msg_modification_infos'); ?></p>
+        <p><?php echo acCouchdbManager::getClient('Messages')->getMessage('msg_popup_ajout'); ?></p>
 	<?php include_partial('form', array('form' => $form, 'certification' => $certification)) ?>
 </div>
