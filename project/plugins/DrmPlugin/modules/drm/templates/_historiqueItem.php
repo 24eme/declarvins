b17afdd7b4932988ef6e37c9c77ea2371fe17118
JB <jblemetayer@actualys.com>
2012-05-10 18:02:45 +0200

    Correction bouton suppression DRM admin

--- project/plugins/DrmPlugin/modules/drm/templates/_historiqueItem.php
+++ project/plugins/DrmPlugin/modules/drm/templates/_historiqueItem.php
@@ -14,9 +14,10 @@
 	<td>En cours</td>
 	<td>
 		<a href="<?php echo url_for('drm_init', array('campagne_rectificative' => $campagne_rectificative)); ?>">Accéder à la déclaration en cours</a><br />
-	   </td><td style="border: 0px; padding-left: 0px;background-color: #ffffff;">
-		<a href="<?php echo url_for('drm_delete', array('campagne_rectificative' => $campagne_rectificative)); ?>" class="btn_reinitialiser"><span><img src="/images/pictos/pi_supprimer.png"/></span></a>
-	</td>
+	   </td>
+	   <td style="border: 0px; padding-left: 0px;background-color: #ffffff;">
+	       <a href="<?php echo url_for('drm_delete', array('campagne_rectificative' => $campagne_rectificative)); ?>" class="btn_reinitialiser"><span><img src="/images/pictos/pi_supprimer.png"/></span></a>
+	   </td>
 <?php else: ?>
 	<td>OK</td>
 	<td>
