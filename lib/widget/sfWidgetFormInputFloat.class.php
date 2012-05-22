0a63f9bc81d0c21da20b580e64d6993474899ed7
Vincent LAURENT <vlaurent@actualys.com>
2012-05-22 15:31:31 +0200

    Ajout des premières librairies

---
+++ lib/widget/sfWidgetFormInputFloat.class.php
@@ -0,0 +1,18 @@
+<?php
+
+class sfWidgetFormInputFloat extends sfWidgetFormInputText
+{
+  public function render($name, $value = null, $attributes = array(), $errors = array())
+  {
+    sfApplicationConfiguration::getActive()->loadHelpers('Float');
+    $value = sprintFloat($value);
+    $attributes['autocomplete'] = 'off';
+    if (!isset($attributes['class']))
+      $attributes['class'] = '';
+    //$attributes['class'] = ' num num_float';
+    if (!($value*1)) 
+      $attributes['class'] .= ' num_light';
+
+    return parent::render($name, $value, $attributes, $errors);
+  }
+}
