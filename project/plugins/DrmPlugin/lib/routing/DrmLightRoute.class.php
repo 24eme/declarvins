72ff0c83f1d9f3f41cbf104497bb4beb0341de07
JB <jblemetayer@actualys.com>
2012-05-29 18:50:52 +0200

    Sécurité sur la drm

--- project/plugins/DrmPlugin/lib/routing/DrmLightRoute.class.php
+++ project/plugins/DrmPlugin/lib/routing/DrmLightRoute.class.php
@@ -24,7 +24,13 @@ class DrmLightRoute extends sfRequestRoute {
         if (!$drm) {
             throw new sfError404Exception(sprintf('No DRM found for this campagne-rectificative "%s".',  $parameters['campagne_rectificative']));
         }
-
+	
+		if (isset($this->options['must_be_valid']) && $this->options['must_be_valid'] === true && !$drm->isValidee()) {
+			throw new sfError404Exception('DRM must be validated');
+		}
+		if (isset($this->options['must_be_not_valid']) && $this->options['must_be_not_valid'] === true && $drm->isValidee()) {
+			throw new sfError404Exception('DRM must not be validated');
+		}
         return $drm;
     }
 
--- project/plugins/DrmPlugin/lib/routing/DrmRoute.class.php
+++ project/plugins/DrmPlugin/lib/routing/DrmRoute.class.php
@@ -23,7 +23,12 @@ class DrmRoute extends sfObjectRoute {
         if (!$this->drm) {
             throw new sfError404Exception(sprintf('No DRM found for this campagne-rectificative "%s".',  $parameters['campagne_rectificative']));
         }
-
+		if (isset($this->options['must_be_valid']) && $this->options['must_be_valid'] === true && !$this->drm->isValidee()) {
+			throw new sfError404Exception('DRM must be validated');
+		}
+		if (isset($this->options['must_be_not_valid']) && $this->options['must_be_not_valid'] === true && $this->drm->isValidee()) {
+			throw new sfError404Exception('DRM must not be validated');
+		}
         return $this->drm;
     }
 
