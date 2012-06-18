72fcd9be899fca1d7d45a894036dd20cd017698e
JB <jblemetayer@actualys.com>
2012-06-18 15:52:44 +0200

    On masque toutes les références au noeud mention

--- project/lib/model/couchdb/Configuration/ConfigurationGenre.class.php
+++ project/lib/model/couchdb/Configuration/ConfigurationGenre.class.php
@@ -13,14 +13,14 @@ class ConfigurationGenre extends BaseConfigurationGenre {
     
     public function setDonneesCsv($datas) {
     	$this->getCertification()->setDonneesCsv($datas);
-    	$this->libelle = ($datas[ProduitCsvFile::CSV_PRODUIT_GENRE_LIBELLE])? $datas[ProduitCsvFile::CSV_PRODUIT_GENRE_LIBELLE] : null;
+    	/*$this->libelle = ($datas[ProduitCsvFile::CSV_PRODUIT_GENRE_LIBELLE])? $datas[ProduitCsvFile::CSV_PRODUIT_GENRE_LIBELLE] : null;
     	$this->code = ($datas[ProduitCsvFile::CSV_PRODUIT_GENRE_CODE])? $datas[ProduitCsvFile::CSV_PRODUIT_GENRE_CODE] : null;
     	if (ProduitCsvFile::CSV_PRODUIT_GENRE_CODE_APPLICATIF_DROIT == $datas[ProduitCsvFile::CSV_PRODUIT_DOUANE_NOEUD]) {
     		$this->setDroitDouaneCsv($datas);
     	}
     	if (ProduitCsvFile::CSV_PRODUIT_GENRE_CODE_APPLICATIF_DROIT == $datas[ProduitCsvFile::CSV_PRODUIT_CVO_NOEUD]) {
     		$this->setDroitCvoCsv($datas);
-    	}
+    	}*/
     }
     
   	public function hasDepartements() {
