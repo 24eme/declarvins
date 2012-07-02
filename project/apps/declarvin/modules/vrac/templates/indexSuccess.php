4e85d201982f3177a6fb54983042f1c345e5c5cd
JB <jblemetayer@actualys.com>
2012-07-02 17:20:56 +0200

    Ajout de l'action derriere le formulaire de recherche

--- project/apps/declarvin/modules/vrac/templates/indexSuccess.php
+++ project/apps/declarvin/modules/vrac/templates/indexSuccess.php
@@ -2,7 +2,7 @@
 <section id="contenu">
 	<div class="popup_form">
 		<div class="ligne_form_btn" style="margin:0; text-align: left;">
-			<form action="" method="get" style="width: 500px; float:left;">
+			<form action="<?php echo url_for('vrac_recherche') ?>" method="get" style="width: 500px; float:left;">
 				<input name="identifiant" value="<?php echo (isset($identifiant)) ? $identifiant : '' ; ?>"/>
 				<button type="submit" class="btn_valider">Rechercher</button>
 			</form>
