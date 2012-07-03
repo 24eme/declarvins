20e4fbcbc525688916845f31c86b9fab02e3f47b
JB <jblemetayer@actualys.com>
2012-07-03 18:50:27 +0200

    Fonctions manquantes pour l'adaptation du plugin a la drm

--- lib/model/Vrac/client/VracClient.php
+++ lib/model/Vrac/client/VracClient.php
@@ -52,5 +52,35 @@ class VracClient extends acCouchdbClient {
       return $this->startkey(array($soussigneParam))
               ->endkey(array($soussigneParam, array()))->limit(300)->getView('vrac', 'soussigneidentifiant');
     }
+    /*
+     * Methodes de l'ancien plugin
+     */
+
+    public function retrieveById($id, $hydrate = acCouchdbClient::HYDRATE_DOCUMENT) {
+        return parent::retrieveDocumentById('VRAC-'.$id, $hydrate);
+    }
+    
+
+    public function retrieveFromEtablissementsAndHash($etablissement, $hash, $mustActive = true, $hydrate = acCouchdbClient::HYDRATE_DOCUMENT) {
+      $contrats = array();
+      foreach ($this->startkey(array($etablissement))
+	       ->endkey(array($etablissement, array()))->getView('vrac', 'all', $hydrate)->rows as $c) {
+	       	if (strpos($hash, $c->key[1]) !== false) {
+      	if ($mustActive && $c->key[3] == Configuration::STATUT_CONTRAT_NONSOLDE) {
+      		$contrats[] = parent::retrieveDocumentById($c->key[2]);
+      	}
+	       	}
+      }
+      return $contrats;
+    }
+
+    public function retrieveFromEtablissements($etablissement, $hydrate = acCouchdbClient::HYDRATE_DOCUMENT) {
+      $contrats = array();
+      foreach ($this->startkey(array($etablissement))
+	       ->endkey(array($etablissement, array()))->getView('vrac', 'all', $hydrate)->rows as $c) {
+	$contrats[] = parent::retrieveDocumentById($c->key[2]);
+      }
+      return $contrats;
+    }
     
  }
