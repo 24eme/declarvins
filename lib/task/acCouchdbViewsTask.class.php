3789b4d3f32cd8d17d736930dc736cb79d65c608
Vincent LAURENT <vince.laurent@gmail.com>
2012-07-04 16:34:21 +0200

    Tache de creation et de consultation des vues couchdb

--- lib/task/acCouchdbViewsTask.class.php
+++ lib/task/acCouchdbViewsTask.class.php
@@ -1,24 +1,20 @@
 <?php
 
-class acCouchdbLoadViewTask extends sfBaseTask {
+class acCouchdbViewsTask extends sfBaseTask {
 
     protected function configure() {
 
         $this->addArguments(array(
-            new sfCommandArgument('model', sfCommandArgument::REQUIRED, 'Document model'),
-            new sfCommandArgument('name', sfCommandArgument::REQUIRED, 'View name'),
         ));
 
         $this->addOptions(array(
             new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name'),
             new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
             new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'default'),
-            new sfCommandOption('dir', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', sfConfig::get("sf_lib_dir") . "/model/couchdb"),
-            new sfCommandOption('id', null, sfCommandOption::PARAMETER_REQUIRED, 'Document id to generate'),
         ));
 
         $this->namespace = 'couchdb';
-        $this->name = 'load-view';
+        $this->name = 'views';
         $this->briefDescription = '';
         $this->detailedDescription = <<<EOF
 The [acCouchdbLoadView|INFO] task does things.
@@ -29,7 +25,26 @@ EOF;
     }
 
     protected function execute($arguments = array(), $options = array()) {
+    	$databaseManager = new sfDatabaseManager($this->configuration);
+        $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
 
-        
+    	$file_regexp = "/([a-zA-Z0-9\-\_]+)\.([a-zA-Z0-9\-\_]+)\.(map|reduce)\.view\.js$/";
+
+    	$files = sfFinder::type("file")->name($file_regexp)->relative()->in(sfConfig::get('sf_root_dir'));
+
+    	$views = array();
+
+    	foreach($files as $file) {
+    		preg_match($file_regexp, $file, $matches);
+    		$name = '_design/'.$matches[1]."/_view/".$matches[2];
+    		$views[$name] = $file;
+    	}
+
+    	ksort($views);
+
+    	foreach($views as $view => $path) {
+    		
+    		$this->logSection($view, $path);
+    	}
     }
 }
