3d131e857e520b210b2c2d1d57b82144ce9a4b17
Vincent LAURENT <vince.laurent@gmail.com>
2012-07-05 14:50:48 +0200

    lien ajout de compte

--- project/apps/declarvin/modules/acVinCompteAdmin/templates/indexSuccess.php
+++ project/apps/declarvin/modules/acVinCompteAdmin/templates/indexSuccess.php
@@ -5,5 +5,6 @@
     <div id="mon_compte">
         <?php include_partial('compte_recherche_form', array('form' => $form))?>
     </div>
+    <a href="<?php echo url_for("compte_ajout") ?>">Nouveau</a>
 </div>
 </section>
