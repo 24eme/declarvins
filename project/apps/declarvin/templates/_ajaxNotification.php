66b8027193ffed10c51884429b51433651be9474
Vincent LAURENT <vlaurent@actualys.com>
2012-07-06 19:21:09 +0200

    Gestion de la mise à jour de la revision d'un form en js apres un post ajax, et ce de manière global

---
+++ project/apps/declarvin/templates/_ajaxNotification.php
@@ -0,0 +1,7 @@
+<div id="ajax_form_progress_notification">
+    Enregistrement en cours...
+</div>
+<div id="ajax_form_error_notification">
+    Oops, une erreur est survenue ! </br>
+    Vous pouvez recharger la page ou si le problème persiste contactez votre interpro.
+</div>
