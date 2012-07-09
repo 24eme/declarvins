ec64e7f757f2c1811fea02b33d8717d73e8ca3f8
Vincent LAURENT <vlaurent@actualys.com>
2012-07-09 11:36:48 +0200

    Le css de la notification ne permettait pas l'alignement

--- project/apps/declarvin/templates/_ajaxNotification.php
+++ project/apps/declarvin/templates/_ajaxNotification.php
@@ -1,7 +1,9 @@
-<div id="ajax_form_progress_notification">
-    Enregistrement en cours...
-</div>
-<div id="ajax_form_error_notification">
-    Oops, une erreur est survenue ! </br>
-    Vous pouvez recharger la page ou si le problème persiste contactez votre interpro.
+<div id="ajax_notifications">
+	<div id="ajax_form_progress_notification">
+	    Enregistrement en cours...
+	</div>
+	<div id="ajax_form_error_notification">
+	    Oops, une erreur est survenue ! </br>
+	    Vous pouvez recharger la page ou si le problème persiste contactez votre interpro.
+	</div>
 </div>
