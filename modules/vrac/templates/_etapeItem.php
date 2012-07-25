099c2d3a81403ea968d4ce24d41e2235191b7f22
Charlotte Devichet <cdevichet@actualys.com>
2012-07-25 13:55:00 +0000

    modification par JB

--- modules/vrac/templates/_etapeItem.php
+++ modules/vrac/templates/_etapeItem.php
@@ -1,8 +1,14 @@
-<li>
-	<?php if ($isLink): ?>
-    <a href="<?php echo url_for('vrac_etape', array('sf_subject' => $vrac, 'step' => $etape)) ?>"><?php echo $label ?></a>    
+<li<?php if ($last): ?> class="dernier<?php if ($isActive): ?> actif passe<?php endif ?>"<?php elseif($first): ?> class="premier<?php if ($isActive): ?> actif passe<?php endif ?>"<?php endif; ?>>
+    <?php if ($isLink): ?>
+        <a href="<?php echo url_for('vrac_etape', array('sf_subject' => $vrac, 'step' => $etape)) ?>"><?php echo $label ?></a>    
     <?php else: ?>
-        <?php if($isActive): ?><strong><?php endif; ?><?php echo $label ?><?php if($isActive): ?></strong><?php endif; ?>
+        <?php if($isActive): ?>
+            <strong><?php echo $label ?></strong>
+        <?php else: ?>
+            <?php if ($last): ?><span><?php endif; ?>
+            <span><?php echo $label ?></span>
+            <?php if ($last): ?></span><?php endif; ?>
+        <?php endif; ?>
     <?php endif; ?>
 </li>
 
--- modules/vrac/templates/_etapes.php
+++ modules/vrac/templates/_etapes.php
@@ -1,7 +1,17 @@
-<ol id="rail_etapes">
-	<?php 
-		foreach ($etapes as $etape => $etapeLibelle) {
-			include_partial('etapeItem',array('vrac' => $vrac, 'actif' => $actif, 'etape' => $etape, 'label' => $etapeLibelle, 'isActive' => ($actif == $etape), 'isLink' => !$configurationVracEtapes->hasSup($etape, $actif)));
-		}	
-	?>  
-</ol>
+<div id="contrats_etapes">
+    <ol id="rail_etapes">
+            <?php 
+                    $nbEtapes = count($etapes);
+                    $counter = 0;
+                    $first = true;
+                    foreach ($etapes as $etape => $etapeLibelle) {
+                            $counter++;
+                            include_partial('etapeItem',array('vrac' => $vrac, 'actif' => $actif, 'etape' => $etape, 'label' => $etapeLibelle, 'isActive' => ($actif == $etape), 'isLink' => !$configurationVracEtapes->hasSup($etape, $actif), 'last' => ($nbEtapes == $counter), 'first' => $first));
+                            
+                            if ($first) {
+                                $first = false;
+                            }
+                    }	
+            ?>  
+    </ol>
+</div>
