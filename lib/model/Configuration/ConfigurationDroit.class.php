830e514237d18173c51fdd40243e3c422c5741db
JB <jblemetayer@actualys.com>
2012-07-27 16:05:49 +0200

    Fonction de test d'existance d'un droit

--- lib/model/Configuration/ConfigurationDroit.class.php
+++ lib/model/Configuration/ConfigurationDroit.class.php
@@ -4,5 +4,10 @@
  *
  */
 
-class ConfigurationDroit extends BaseConfigurationDroit {
+class ConfigurationDroit extends BaseConfigurationDroit 
+{
+	public function isEmpty()
+	{
+		return is_null($this->taux);
+	}
 }
