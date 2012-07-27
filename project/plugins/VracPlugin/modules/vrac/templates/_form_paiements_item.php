7c05eab2e504cc639067ef1b1a8d6a34264ca172
Charlotte Devichet <cdevichet@actualys.com>
2012-07-27 10:06:38 +0000

    integration vracs

--- project/plugins/VracPlugin/modules/vrac/templates/_form_paiements_item.php
+++ project/plugins/VracPlugin/modules/vrac/templates/_form_paiements_item.php
@@ -1,12 +1,10 @@
 <tr>
     <td>
         <?php echo $form['date']->renderError() ?>
-        <?php echo $form['date']->renderLabel() ?>
         <?php echo $form['date']->render() ?>
     </td>
     <td>
         <?php echo $form['montant']->renderError() ?>
-        <?php echo $form['montant']->renderLabel() ?>
         <?php echo $form['montant']->render() ?>
     </td>
     <td>
--- project/plugins/VracPlugin/modules/vrac/templates/_form_retiraisons_item.php
+++ project/plugins/VracPlugin/modules/vrac/templates/_form_retiraisons_item.php
@@ -1,22 +1,18 @@
 <tr>
     <td>
         <?php echo $form['lot_cuve']->renderError() ?>
-        <?php echo $form['lot_cuve']->renderLabel() ?>
         <?php echo $form['lot_cuve']->render() ?>
     </td>
     <td>
         <?php echo $form['date_retiraison']->renderError() ?>
-        <?php echo $form['date_retiraison']->renderLabel() ?>
         <?php echo $form['date_retiraison']->render() ?>
     </td>
     <td>
         <?php echo $form['volume_retire']->renderError() ?>
-        <?php echo $form['volume_retire']->renderLabel() ?>
         <?php echo $form['volume_retire']->render() ?>
     </td>
     <td>
         <?php echo $form['montant_paiement']->renderError() ?>
-        <?php echo $form['montant_paiement']->renderLabel() ?>
         <?php echo $form['montant_paiement']->render() ?>
     </td>
     <td>
