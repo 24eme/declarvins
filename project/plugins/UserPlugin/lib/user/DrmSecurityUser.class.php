3a810fe608bc3f13f1457f55b1d897c91e199d88
JB <jblemetayer@actualys.com>
2012-07-30 17:33:28 +0200

    Correction bug rectificative

--- project/plugins/UserPlugin/lib/user/DrmSecurityUser.class.php
+++ project/plugins/UserPlugin/lib/user/DrmSecurityUser.class.php
@@ -41,123 +41,14 @@ abstract class DRMSecurityUser extends TiersSecurityUser {
     public function signOutDRM() {
         $this->_drm = null;
         $this->clearCredentialsDRM();
-    }
-    
-    /**
-     * @return DR
-     */
-    public function getDRM() {
-    	$this->requireDRM();
-    	$this->requireTiers();
-    	if (is_null($this->_drm)) {
-    		$lastDRM = $this->getDRMHistorique()->getLastDRM();
-
-    		if ($lastDRM && $drm = DRMClient::getInstance()->find(key($lastDRM))) {
-    			if (!$drm->isValidee()) {
-    				$this->_drm = $drm;
-    			} else {
-    				$this->_drm = $drm->generateSuivante($this->getCampagneDRM());
-    			}
-    		} else {
-    			$this->_drm = new DRM();
-    			$this->_drm->identifiant = $this->getTiers()->identifiant;
-    			$this->_drm->campagne = $this->getCampagneDRM();
-    		}
-    	}
-    	return $this->_drm;
-    }
-    
-    /**
-     * @return DR
-     */
-    public function createDRMByCampagne($campagne = null) {
-    	if (!$campagne) {
-    		$campagne = date('Y-m');
-    	}
-    	$campagneTab = explode('-', $campagne);
-    	$date_campagne = new DateTime($campagneTab[0].'-'.$campagneTab[1].'-01');
-       	$date_campagne->modify('-1 month');
-       	$prev_campagne = DRMClient::getInstance()->getCampagne($date_campagne->format('Y'), $date_campagne->format('m'));
-       	$prev_drm = DRMClient::getInstance()->findLastByIdentifiantAndCampagne($this->getTiers()->identifiant, $prev_campagne);
-       	if ($prev_drm) {
-           $this->_drm = $prev_drm->generateSuivante($campagne);
-       	} else {
-       		$lastDRM = $this->getDRMHistorique()->getLastDRM();
-    		if ($lastDRM && $drm = DRMClient::getInstance()->find(key($lastDRM))) {
-    			$this->_drm = $drm->generateSuivante($campagne, false);
-    		} else {
-		    	$this->_drm = new DRM();
-		    	$this->_drm->identifiant = $this->getTiers()->identifiant;
-		    	$this->_drm->campagne = $campagne;
-    		}
-       	}
-        return $this->_drm;
-    }
-    /**
-     * @return DR
-     */
-    public function getLastDRMValide() {
-        $lastDRM = $this->getDRMHistorique()->getLastDRM();
-        if ($lastDRM && $drm = DRMClient::getInstance()->find(key($lastDRM))) {
-	  if ($drm->isValidee()) {
-	    return $drm;
-	  }
-        }
-        return null;
-    }
-    
-    /**
-     * 
-     */
-    public function getDRMHistorique() {
-    	if (is_null($this->_historique)) {
-        	$this->_historique = new DRMHistorique($this->getTiers()->identifiant);
-    	}
-    	return $this->_historique;
-    }
-    
-    public function hasDrmEnCours() 
-    {
-    	$historique = $this->getDRMHistorique();
-    	$drms = $historique->getDRMs();
-    	$hasDrmEnCours = false;
-    	foreach ($drms as $drm) {
-    		if (!($drm[DRMHistorique::VIEW_INDEX_STATUS] > 0)) {
-    			$hasDrmEnCours = true;
-    			break;
-    		}
-    	}
-    	return $hasDrmEnCours;
-    }
-    
-    /**
-     * @return string
-     */
-    public function getCampagneDRM() {
-      return CurrentClient::getCurrent()->campagne;
-    }
-    /**
-     * returns true if editable
-     */
-    public function isDRMEditable() {
-    	return true;
-    }
+    }   
 
     /**
      * 
      */
     public function initCredentialsDRM() {
         $this->requireDRM();
-        $drm = $this->getDRM();
         $this->clearCredentialsDRM();
-        /*if ($this->isDRMEditable()) {
-            if ($drm->isValideeTiers() || $drm->isValideeCiva()) {
-                $this->addCredential(self::CREDENTIAL_DECLARATION_VALIDE);
-            } else {
-                $this->addCredential(self::CREDENTIAL_DECLARATION_EN_COURS);
-                $this->addCredentialsEtapeDRM();
-            }
-        }*/
     }
     
     /**
@@ -196,11 +87,5 @@ abstract class DRMSecurityUser extends TiersSecurityUser {
     public function signOutTiers() {
         $this->signOutDRM();
         parent::signOutTiers();
-    }
-    
-    public function removeDRM() {
-    	$this->getDRM()->delete();
-        $this->signOutDRM();
-        $this->initCredentialsDRM();
     }
 }
