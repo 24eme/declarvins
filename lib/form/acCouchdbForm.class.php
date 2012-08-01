b077af4125d370417161962e0df04e561766cda5
Vincent LAURENT <vlaurent@actualys.com>
2012-08-01 17:49:23 +0200

    Form couchdb de base, permet de récuperer le document

--- lib/form/acCouchdbForm.class.php
+++ lib/form/acCouchdbForm.class.php
@@ -4,11 +4,18 @@ class acCouchdbForm extends BaseForm implements acCouchdbFormDocableInterface
 {
 
   protected $docable = null;
+  protected $doc = null;
 
   public function __construct(acCouchdbDocument $doc, $defaults = array(), $options = array(), $CSRFSecret = null) {
-    $this->docable = new acCouchdbFormDocable($this, $doc);
+    $this->doc = $doc;
+    $this->docable = new acCouchdbFormDocable($this, $this->doc);
     parent::__construct($defaults, $options, $CSRFSecret);
     $this->docable->init();
+  }
+
+  public function getDocument() {
+
+    return $this->doc;
   }
 
   public function getDocable() {
