b819311f762dedc0f00e759c0ec20316633a71a6
Charlotte Devichet <cdevichet@actualys.com>
2012-08-17 16:33:32 +0000

    integration recap

--- modules/vrac/templates/recapitulatifSuccess.php
+++ modules/vrac/templates/recapitulatifSuccess.php
@@ -1,17 +1,31 @@
-<div id="contenu">
-	<div id="rub_contrats">
-		<section id="principal"> 
-			<section id="contenu_etape">
-			<h2>La saisie est terminée !</h2>
-			<h2>N° d'enregistrement du contrat : <span><?php echo $vrac->numero_contrat ?></span></h2>
-			<?php include_partial('showContrat', array('vrac' => $vrac, 'configurationVrac' => $configurationVrac)); ?>
-			<div id="btn_etape_dr">
-				<div class="btnValidation">
-					<span>&nbsp;</span>
-					<a href="<?php echo url_for('vrac') ?>" class="btn_majeur btn_gris">Retour à la liste des contrats</a>
-				</div>
-			</div>
-			</section> 
-		</section>
-	</div>
+<div id="contenu" class="vracs">
+    <div id="rub_contrats">
+        <section id="principal"> 
+            <div id="recap_saisie" class="popup_form visualisation_contrat">
+                
+                <div id="titre">
+                    <span class="style_label">N° d'enregistrement du contrat : <?php echo $vrac->numero_contrat ?></span>
+                </div>    
+                <form action="" method="post" id="vrac_condition">  
+                    <div class="legende" id="ss_titre"><span class="style_label">Etat du contrat</span>
+                        <a id="solder_contrat" href="">Solder le contrat</a>
+                        <div>
+                            <span class="statut statut_non-solde"></span><span class="legende_statut_texte">NONSOLDE</span>
+                        </div>                            
+                    </div>
+                    <div id="ligne_btn">
+                            <a href="" id="btn_editer_contrat"  class="modifier"> Editer le contrat</a>
+                            <button type="submit" id="btn_annuler_contrat">Annuler le contrat</button>                                
+                    </div>
+                </form>
+            
+                <?php include_partial('showContrat', array('vrac' => $vrac, 'configurationVrac' => $configurationVrac)); ?>
+                
+                <div class="ligne_form_btn">
+			<a class="etape_prec" href="<?php echo url_for('vrac') ?>"><span>Retour à la liste des contrats</span></a>	
+		</div>
+                
+            </div> 
+        </section>
+    </div>
 </div>
