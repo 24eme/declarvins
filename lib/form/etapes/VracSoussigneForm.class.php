fb18ace9a73182c5231694e78297705dcccb4825
Tangui Morlier <tangui@tangui.eu.org>
2012-08-22 00:09:53 +0200

    Retour su sousigneform

---
+++ lib/form/etapes/VracSoussigneForm.class.php
@@ -0,0 +1,33 @@
+<?php
+class VracSoussigneForm extends VracForm 
+{
+   	public function configure()
+    {
+		parent::configure();
+		$this->useFields(array(
+           'vendeur_type',
+           'vendeur_identifiant',
+	       'vendeur_assujetti_tva',
+           'acheteur_type',
+           'acheteur_identifiant',
+	       'acheteur_assujetti_tva',
+	       'mandataire_exist',
+	       'mandataire_identifiant',
+	       'premiere_mise_en_marche',
+	       'cas_particulier',
+		   'vendeur',
+		   'acheteur',
+		   'mandataire',
+		   'adresse_stockage',
+		   'adresse_livraison',
+		));
+		$this->widgetSchema->setNameFormat('vrac_soussigne[%s]');
+		$this->setDefaultValues();
+    }
+
+    public function setDefaultValues() 
+    {
+    	$this->getWidget('cas_particulier')->setDefault((($this->getObject()->cas_particulier)? $this->getObject()->cas_particulier : ConfigurationVrac::CAS_PARTICULIER_DEFAULT_KEY));
+    	$this->getWidget('premiere_mise_en_marche')->setDefault(true);
+    }
+}
