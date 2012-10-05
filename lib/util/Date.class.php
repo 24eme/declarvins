00024330273b14b769dc0f8e022a872e7944d059
mathurin <mpetit@actualys.com>
2012-10-05 17:59:11 +0200

    ajout d'un helper pour les dates ajout d'une fonction permettant d'obtenir la date fin de mois + x d'une date

--- lib/util/Date.class.php
+++ lib/util/Date.class.php
@@ -9,4 +9,12 @@ class Date {
 	    return $diff->{$retour};
   	}
 
+        public static function getIsoDateFinDeMoisISO($date,$nb_mois) 
+        {
+            preg_match('/^([0-9]{4})-([0-9]{2})-([0-9]{2})$/', $date, $matches);
+            $annee = $matches[1];
+            $mois = $matches[2];
+            $lastdaymonth = mktime(0,0,0,$mois+$nb_mois+1,0,$annee);
+            return date('Y-m-d', $lastdaymonth);
+        }
 }
