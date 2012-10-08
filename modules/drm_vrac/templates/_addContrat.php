1ae854f4e09af8443ea75fd326775dd380a1011b
JB <jblemetayer@actualys.com>
2012-10-08 16:24:00 +0200

    Ajout de texte sur le lien ajouter un contrat pour la page vrac de la drm

--- modules/drm_vrac/templates/_addContrat.php
+++ modules/drm_vrac/templates/_addContrat.php
@@ -5,7 +5,7 @@
 
         <?php if (count($detail->getContratsVrac()) > 1 && count($detail->getContratsVracNonUtilises()) > 0): ?>
         <div class="btn">
-            <a href="<?php echo url_for('drm_vrac_ajout_contrat', $detail) ?>" class="btn_ajouter btn_popup" data-popup-ajax="true" data-popup="#popup_ajout_contrat_<?php echo $detail->getIdentifiantHTML() ?>" data-popup-config="configForm"></a>
+            <a href="<?php echo url_for('drm_vrac_ajout_contrat', $detail) ?>" class="btn_ajouter btn_ajouter_sombre btn_popup" data-popup-ajax="true" data-popup="#popup_ajout_contrat_<?php echo $detail->getIdentifiantHTML() ?>" data-popup-config="configForm">Lier un contrat</a>
         </div>
         <?php endif; ?>
     </td>
