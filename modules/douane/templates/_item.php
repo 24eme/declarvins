8fc3a109a51f7e4136898fccc41c50beccbabb40
Charlotte Devichet <cdevichet@actualys.com>
2012-10-10 16:20:27 +0000

    integration

--- modules/douane/templates/_item.php
+++ modules/douane/templates/_item.php
@@ -2,10 +2,18 @@
 	$value = $douane->value;
 	$key = $douane->key;
 ?>
-<tr>
+<tr <?php if($alt): ?>class="alt"<?php endif; ?>>
 	<td><?php echo $value[DouaneAllView::VALUE_DOUANE_NOM] ?></td>
 	<td><?php echo $value[DouaneAllView::VALUE_DOUANE_IDENTIFIANT] ?></td>
 	<td><?php echo $value[DouaneAllView::VALUE_DOUANE_EMAIL] ?></td>
 	<td style="text-align:center;"><?php echo $value[DouaneAllView::VALUE_DOUANE_STATUT] ?></td>
-	<td><a href="<?php echo url_for('douane_etablissements', array('id' => $key)) ?>">Etablissements</a> | <a href="<?php echo url_for('douane_modification', array('id' => $key)) ?>">Modifier</a> | <?php if ($value[DouaneAllView::VALUE_DOUANE_STATUT] == Douane::STATUT_ACTIF): ?><a href="<?php echo url_for('douane_desactiver', array('id' => $key)) ?>">Désactiver</a><?php else: ?><a href="<?php echo url_for('douane_activer', array('id' => $key)) ?>">Activer</a><?php endif; ?></td>
+	<td class="actions">
+		<a href="<?php echo url_for('douane_etablissements', array('id' => $key)) ?>" class="btn_etablissement">Etablissements</a>
+		<a href="<?php echo url_for('douane_modification', array('id' => $key)) ?>" class="btn_modifier">Modifier</a>
+		<?php if ($value[DouaneAllView::VALUE_DOUANE_STATUT] == Douane::STATUT_ACTIF): ?>
+			<a href="<?php echo url_for('douane_desactiver', array('id' => $key)) ?>" class="btn_desactiver">Désactiver</a>
+		<?php else: ?>
+			<a href="<?php echo url_for('douane_activer', array('id' => $key)) ?>" class="btn_activer">Activer</a>
+		<?php endif; ?>
+	</td>
 </tr>
