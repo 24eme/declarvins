cb7118c05cb1bb1b88445ecf927433c78631b75a
mathurin <mpetit@actualys.com>
2012-10-12 18:17:35 +0200

    mis en place d'un fonction getIsoDateFromFrenchDate permettant un formattage date depuis jj/mm/aaaa vers yyyy-mm-dd

--- lib/util/Date.class.php
+++ lib/util/Date.class.php
@@ -17,4 +17,20 @@ class Date {
             $lastdaymonth = mktime(0,0,0,$mois+$nb_mois+1,0,$annee);
             return date('Y-m-d', $lastdaymonth);
         }
+        
+        public static function getIsoDateFromFrenchDate($french_date) 
+        {
+            $matches = array();
+            try{
+            preg_match('/^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/', $french_date, $matches);
+            $jours = $matches[1];
+            $mois = $matches[2];
+            $annee = $matches[3];
+            }
+             catch (Exception $e)
+            {
+                throw new sfException("The date $french_date cannot be transformed");
+            }
+            return date('Y-m-d',mktime(0,0,0,$mois,$jours,$annee));
+        }
 }
