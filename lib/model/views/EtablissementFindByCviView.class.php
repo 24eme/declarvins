7ab805b344ca5b9219b676ced8bcad239e4fcf2e
mathurin <mpetit@actualys.com>
2012-10-19 19:00:52 +0200

    ajout de la vue findByCvi pour les etbs

---
+++ lib/model/views/EtablissementFindByCviView.class.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Description of class EtablissementFindByCviView
+ * @author mathurin
+ */
+class EtablissementFindByCviView extends acCouchdbView
+{
+	const KEY_ETABLISSEMENT_CVI = 0;
+        const VALUE_ETABLISSEMENT_ID = 0;
+
+    public static function getInstance() {
+
+        return acCouchdbManager::getView('etablissement', 'findByCvi', 'Etablissement');
+    }
+
+    public function findByCvi($cvi) {
+
+    	return $this->client->startkey(array($cvi))
+                    		->endkey(array($cvi, array()))
+                    		->getView($this->design, $this->view)->rows;
+    }
+}  
