5a4f768ceeb1cc9709fb1bd9dcfc74f4abbaa7d2
mathurin <mpetit@actualys.com>
2012-10-19 19:11:28 +0200

    ajout dans Search d'une fonction matchTermLight n'utilisant pas les exprreg

--- lib/util/Search.class.php
+++ lib/util/Search.class.php
@@ -17,7 +17,36 @@ class Search {
 	      $find = false;
 	      foreach($words_text as $key_text => $word_text) {
 	        if(preg_match($matcher, $word_text)) {
+                    
 	          $find = true;
+	          unset($words_text[$key_text]);
+	          break;
+	        }
+	        unset($words_text[$key_text]);
+	      }
+	      if(!$find) {
+	        return false;
+	      }
+	    }
+
+	    return $find;
+  	}
+        
+        public static function matchTermLight($term, $text) {
+	    $reg = "/[ ]+/";
+	    $text = self::normalize($text);
+	    $term = self::normalize($term);
+
+	    $words_text = preg_split($reg, $text);
+	    $words_term = preg_split($reg, $term);
+
+	    $text_final = "";
+
+	    foreach($words_term as $key_term => $word_term) {
+	      $find = false;
+	      foreach($words_text as $key_text => $word_text) {
+	        if($word_term == $word_text) {
+                    $find = true;
 	          unset($words_text[$key_text]);
 	          break;
 	        }
