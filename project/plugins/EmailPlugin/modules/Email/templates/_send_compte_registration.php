e1127ba9c80f186f4dc96b1eeeba1560f9434df6
JB <jblemetayer@actualys.com>
2012-10-31 17:18:37 +0100

    Debug

--- project/plugins/EmailPlugin/modules/Email/templates/_send_compte_registration.php
+++ project/plugins/EmailPlugin/modules/Email/templates/_send_compte_registration.php
@@ -2,5 +2,6 @@
  	
 Votre contrat numéro <?php echo $numero_contrat; ?> à été validé.<br />
 Vous devez créer votre compte en suivant le lien suivant : <a href="<?php echo sfContext::getInstance()->getController()->genUrl(array('sf_route' => 'compte_nouveau', 'nocontrat' => $numero_contrat), false); ?>">Création de mon compte <?php echo sfContext::getInstance()->getController()->genUrl(array('sf_route' => 'compte_nouveau', 'nocontrat' => $numero_contrat), false); ?></a>
-
+<br />
+<?php echo sfContext::getInstance()->getController()->genUrl(array('sf_route' => 'compte_nouveau', 'nocontrat' => $numero_contrat), false); ?>
 <?php echo include_partial('Email/footerMail') ?>
