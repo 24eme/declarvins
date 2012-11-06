a23362d8d6195c95b34da4cda23ba906729b06c9
JB <jblemetayer@actualys.com>
2012-11-06 15:10:29 +0100

    DRM autogénérées

---
+++ project/lib/task/generateDrmsTask.class.php
@@ -0,0 +1,65 @@
+<?php
+
+class generateDrmsTask extends sfBaseTask {
+
+    protected function configure() {
+        $this->addOptions(array(
+            new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name'),
+            new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+            new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'default'),
+                // add your own options here
+        ));
+
+        $this->namespace = 'generate';
+        $this->name = 'drms';
+        $this->briefDescription = 'Génère les drms suivantes de drm au stock null';
+        $this->detailedDescription = <<<EOF
+The [alertes|INFO] task does things.
+Call it with:
+  Génère les drms suivantes de drm au stock null
+  [php symfony alertes|INFO]
+EOF;
+    }
+
+    protected function execute($arguments = array(), $options = array()) {
+        ini_set('memory_limit', '512M');
+        set_time_limit('3600');
+        // initialize the database connection
+        $databaseManager = new sfDatabaseManager($this->configuration);
+        $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
+        $campagne = null;
+        
+        $etablissements = AlertesEtablissementsView::getInstance()->findActive();
+        $now = date('Ym');
+        foreach ($etablissements->rows as $etablissement) {
+			$identifiant = $etablissement->key[AlertesEtablissementsView::KEY_IDENTIFIANT];
+			$drms = AlertesDrmsView::getInstance()->findAllByEtablissement($identifiant);
+			$drmsTab = array();
+			$start = null;
+			foreach ($drms->rows as $drm) {
+				$date = $drm->key[AlertesDrmsView::KEY_CAMPAGNE_ANNEE].$drm->key[AlertesDrmsView::KEY_CAMPAGNE_MOIS];
+				if (!$start || $start > $date) {
+					$start = $date;
+				}
+				$drmsTab[$date] = $drm;
+			}
+			if (count($drmsTab) > 0) {
+				ksort($drmsTab);
+				$end = date('Ym');
+				if ($identifiant == '9223700100') { // a supprimer
+					while ($start <= $end) {
+						echo $start." != ".$end."\n";
+						$start = $this->getNextMonth($start);
+					}
+				} // a supprimer
+			}
+        }
+    }
+    
+    private function getNextMonth($date) {
+		$year = substr($date, 0, 4);
+		$month = substr($date, -2);
+		return ($month < 12)? sprintf('%04d', $year).sprintf('%02d', $month + 1) : sprintf('%04d', $year + 1).sprintf('%02d', 99);
+    }
+
+}
