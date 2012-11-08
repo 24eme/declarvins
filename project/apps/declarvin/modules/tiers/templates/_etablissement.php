1a4f8df174a26f1873448d0649b9ebb8dbc27c9f
JB <jblemetayer@actualys.com>
2012-11-08 12:15:35 +0100

    Modifications de texte

--- project/apps/declarvin/modules/tiers/templates/_etablissement.php
+++ project/apps/declarvin/modules/tiers/templates/_etablissement.php
@@ -3,23 +3,23 @@
 	<div id="compteModification">
 		<div class="societe">
 			<ul>
-				<?php if($etablissement->raison_sociale): ?><li>Raison Sociale : <strong><?php echo $etablissement->raison_sociale ?></strong></li><?php endif; ?>
-				<?php if($etablissement->nom): ?><li>Nom Commercial : <strong><?php echo $etablissement->nom ?></strong></li><?php endif; ?>
-				<?php if($etablissement->siret): ?><li>N° RCS / SIRET: <strong><?php echo $etablissement->siret ?></strong></li><?php endif; ?>
-				<?php if($etablissement->cni): ?><li>N° Carte Nationale d'Identité pour les exploitants individuels : <strong><?php echo $etablissement->cni ?></strong></li><?php endif; ?>
-				<?php if($etablissement->cvi): ?><li>N° CVI : <strong><?php echo $etablissement->cvi ?></strong></li><?php endif; ?>
-				<?php if($etablissement->no_accises): ?><li>N° accises : <strong><?php echo $etablissement->no_accises ?></strong></li><?php endif; ?>
-				<?php if($etablissement->no_tva_intracommunautaire): ?><li>N° TVA intracommunautaire : <strong><?php echo $etablissement->no_tva_intracommunautaire ?></strong></li><?php endif; ?>
-				<?php if($etablissement->siege->adresse): ?><li>Adresse : <strong><?php echo $etablissement->siege->adresse ?></strong></li><?php endif; ?>
-				<?php if($etablissement->siege->code_postal): ?><li>CP : <strong><?php echo $etablissement->siege->code_postal ?></strong></li><?php endif; ?>
-				<?php if($etablissement->siege->commune): ?><li>ville : <strong><?php echo $etablissement->siege->commune ?></strong></li><?php endif; ?>
-				<?php if($etablissement->telephone): ?><li>tel : <strong><?php echo $etablissement->telephone ?></strong></li><?php endif; ?>
-				<?php if($etablissement->fax): ?><li>fax : <strong><?php echo $etablissement->fax ?></strong></li><?php endif; ?>
-				<?php if($etablissement->email): ?><li>email : <strong><?php echo $etablissement->email ?></strong></li><?php endif; ?>
+				<li>Raison Sociale : <strong><?php echo $etablissement->raison_sociale ?></strong></li>
+				<li>Nom Commercial : <strong><?php echo $etablissement->nom ?></strong></li>
+				<li>N° RCS / SIRET: <strong><?php echo $etablissement->siret ?></strong></li>
+				<li>N° Carte Nationale d'Identité pour les exploitants individuels : <strong><?php echo $etablissement->cni ?></strong></li>
+				<li>N° CVI : <strong><?php echo $etablissement->cvi ?></strong></li>
+				<li>N° accises : <strong><?php echo $etablissement->no_accises ?></strong></li>
+				<li>N° TVA intracommunautaire : <strong><?php echo $etablissement->no_tva_intracommunautaire ?></strong></li>
+				<li>Adresse : <strong><?php echo $etablissement->siege->adresse ?></strong></li>
+				<li>CP : <strong><?php echo $etablissement->siege->code_postal ?></strong></li>
+				<li>ville : <strong><?php echo $etablissement->siege->commune ?></strong></li>
+				<li>tel : <strong><?php echo $etablissement->telephone ?></strong></li>
+				<li>fax : <strong><?php echo $etablissement->fax ?></strong></li>
+				<li>email : <strong><?php echo $etablissement->email ?></strong></li>
 			</ul>
 			<ul>
-				<?php if($etablissement->famille): ?><li>Famille : <strong><?php echo EtablissementFamilles::getFamilleLibelle($etablissement->famille) ?></strong></li><?php endif; ?>
-			    <?php if($etablissement->sous_famille): ?><li>Sous-famille : <strong><?php echo EtablissementFamilles::getSousFamilleLibelle($etablissement->famille, $etablissement->sous_famille) ?></strong></li><?php endif; ?>
+				<li>Famille : <strong><?php echo EtablissementFamilles::getFamilleLibelle($etablissement->famille) ?></strong></li>
+			    <li>Sous-famille : <strong><?php echo EtablissementFamilles::getSousFamilleLibelle($etablissement->famille, $etablissement->sous_famille) ?></strong></li>
             </ul>
 			<?php if ($etablissement->comptabilite->adresse): ?>
 			<h2>Lieu où est tenue la comptabilité matière (si différente de l'adresse du chai) :</h2>
@@ -30,7 +30,7 @@
 			</ul>
             <?php endif; ?>
 			<ul>
-				<?php if($etablissement->service_douane): ?><li>Dépend du service des douanes de : <strong><?php echo $etablissement->service_douane ?></strong></li><?php endif; ?>
+				<li>Dépend du service des douanes de : <strong><?php echo $etablissement->service_douane ?></strong></li>
 			</ul>
 		</div>
 	</div>
