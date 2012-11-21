a4a481aa98ec48f40792f4e1c4d9f99a4aba9329
JB <jblemetayer@actualys.com>
2012-11-21 11:35:12 +0100

    Memory a 1024

--- project/apps/edi/modules/edi/actions/actions.class.php
+++ project/apps/edi/modules/edi/actions/actions.class.php
@@ -91,7 +91,7 @@ class ediActions extends sfActions
   }
 
   public function executeStreamDRM(sfWebRequest $request) {
-  	ini_set('memory_limit', '512M');
+  	ini_set('memory_limit', '1024M');
     $date = $request->getParameter('datedebut');
     if (!$date) {
 	return $this->renderText("Pas de date définie");
