62db48e87437069cbe5ca73f7d5c3c98ea889a4f
mathurin <mpetit@actualys.com>
2012-11-27 19:34:54 +0100

    ajout d'un libelle pour l'upload

--- lib/form/UploadCSVForm.class.php
+++ lib/form/UploadCSVForm.class.php
@@ -8,6 +8,8 @@ class UploadCSVForm extends BaseForm {
     public function configure() {
       $this->setWidget('file', new sfWidgetFormInputFile(array('label' => 'Fichier')));
       $this->setValidator('file', new ValidatorImportCsv(array('file_path' => sfConfig::get('sf_data_dir').'/upload')));
+      
+      $this->widgetSchema->setLabel('file', "Choisir un fichier :");
       $this->widgetSchema->setNameFormat('csv[%s]');
     }
 
