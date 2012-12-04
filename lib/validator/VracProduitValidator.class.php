26dad56171070a382a6b75aa0e9557eb3fa00cac
JB <jblemetayer@actualys.com>
2012-12-04 14:26:18 +0100

    Controle produit interpro

--- lib/validator/VracProduitValidator.class.php
+++ lib/validator/VracProduitValidator.class.php
@@ -3,10 +3,19 @@
 class VracProduitValidator extends sfValidatorBase {
 
     public function configure($options = array(), $messages = array()) {
-        $this->addOption('determination_prix_field', 'determination_prix');
+        $this->addMessage('interpro', "Ce produit n'est pas disponible pour votre interpro");
     }
     
     protected function doClean($values) {
+    	$interpros = InterproClient::getInstance()->getInterpros();
+    	if (isset($values['produit']) && !empty($values['produit'])) {
+    		$conf = ConfigurationClient::getCurrent();
+    		$confProduit = $conf->get($values['produit']);
+    		$interproProduit = $confProduit->getGerantInterpro();
+    		if (!in_array($interproProduit->get('_id'), $interpros)) {
+    			throw new sfValidatorErrorSchema($this, array($this->getOption('produit') => new sfValidatorError($this, 'interpro')));
+    		}
+    	}
         return $values;
     }
 
