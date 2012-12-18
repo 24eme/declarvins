55770f18a9df6eb6230bce9c39742e8f0382e74b
JB <jblemetayer@actualys.com>
2012-12-18 18:09:23 +0100

    Lien sur logo

--- project/apps/declarvin/templates/_header.php
+++ project/apps/declarvin/templates/_header.php
@@ -10,7 +10,7 @@
     </nav>
 
     <div id="logo">
-        <h1><a href="#" title="Declarvin - Retour à l'accueil"><img src="/images/visuels/logo_declarvins.png" alt="Declarvin" /></a></h1>
+        <h1><a href="<?php echo url_for('ac_vin_login') ?>" title="Declarvin - Retour à l'accueil"><img src="/images/visuels/logo_declarvins.png" alt="Declarvin" /></a></h1>
         <p>La plateforme déclarative des vins du Rhône, de Provence et du Sud Est</p>
     </div>
     <?php if ($sf_user->isAuthenticated()): ?>
