4d745a13bf53a21c84f04286d4dbd76223c65c7f
JB <jblemetayer@actualys.com>
2012-12-19 18:24:50 +0100

    Erreur sur comparateur de campagne

--- project/plugins/DAIDSPlugin/lib/validator/DAIDSCampagneValidator.class.php
+++ project/plugins/DAIDSPlugin/lib/validator/DAIDSCampagneValidator.class.php
@@ -13,7 +13,7 @@ class DAIDSCampagneValidator extends sfValidatorBase {
         if (!$values['campagne']) {
         	throw new sfValidatorErrorSchema($this, array('campagne' => new sfValidatorError($this, 'required')));
         }       
-        if (DAIDSClient::getInstance()->formatToCompare($values['campagne']) > DAIDSClient::getInstance()->getCurrentPeriode()) {
+        if (DAIDSClient::getInstance()->formatToCompare($values['campagne']) > DAIDSClient::getInstance()->formatToCompare(DAIDSClient::getInstance()->getCurrentPeriode())) {
         	throw new sfValidatorErrorSchema($this, array($this->getOption('years') => new sfValidatorError($this, 'impossible')));
         } 
         $daids = DAIDSClient::getInstance()->findMasterByIdentifiantAndPeriode($this->getOption('etablissement'), $values['campagne']);
