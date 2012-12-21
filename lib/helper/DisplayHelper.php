3d103e76c74f37e2e60e15fbb41c245e5b66a319
mathurin <mpetit@actualys.com>
2012-12-21 14:51:26 +0100

    ajout d'une fct pour l'affichage de champ trop long en latex avec gestion d'un séparateur

--- lib/helper/DisplayHelper.php
+++ lib/helper/DisplayHelper.php
@@ -1,7 +1,30 @@
 <?php
 
-function display_field($object,$fieldName)
-{
-    if(is_null($object)){ echo ''; return;}
-    echo (!is_null($object->$fieldName))? $object->$fieldName : '';
+function display_field($object, $fieldName) {
+    if (is_null($object)) {
+        echo '';
+        return;
+    }
+    echo (!is_null($object->$fieldName)) ? $object->$fieldName : '';
+}
+
+function display_latex_string($string, $sep = '', $limit = null) {
+    $disp = "";
+    $disp = str_replace("&#039;", "'", $string);
+    $disp = str_replace("&amp;", "\&", $disp);
+    
+    if (!$limit && $sep == '')
+        return $disp;
+    
+    if ($sep != '')
+        $disp = str_replace($sep, "\\\\", $disp);
+    $len = strlen($disp);
+    if ($limit!=null && $len > $limit) {
+        $d = substr($disp, 0, $limit);
+        $pos = strrpos($d, ' ');        
+        if ($pos !== FALSE) {
+            $disp = substr($d, 0, $pos) . " \\\\ " . substr($disp, $pos, $len);
+        }
+    }  
+    return $disp;
 }
