5ae36cb5093ec856f721fde4ffbf599243d6de6e
JB <jblemetayer@actualys.com>
2012-12-26 16:13:56 +0100

    Corrections pour les etablissements avec prise en compte des contrats mandats

---
+++ lib/model/views/EtablissementContratView.class.php
@@ -0,0 +1,18 @@
+<?php
+
+class EtablissementContratView extends acCouchdbView
+{
+	const CONTRAT_NUMERO = 0;
+	public static function getInstance() {
+
+        return acCouchdbManager::getView('etablissement', 'contrat', 'Etablissement');
+    }
+
+    public function findByContrat($contrat) {
+
+    	return $this->client->startkey(array($contrat))
+                    		->endkey(array($contrat, array()))
+                    		->getView($this->design, $this->view);
+    }
+
+}  
