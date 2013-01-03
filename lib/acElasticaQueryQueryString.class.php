106a2e3f2f34e47149507fbe7b02c5e0fda1e460
Tangui Morlier <tangui@tangui.eu.org>
2013-01-03 12:41:56 +0100

    Simplification de l'init de QueryString

--- lib/acElasticaQueryQueryString.class.php
+++ lib/acElasticaQueryQueryString.class.php
@@ -1,4 +1,9 @@
 <?php
 
 class acElasticaQueryQueryString extends Elastica_Query_QueryString {
+  public function __construct($query_string) {
+    parent::__construct();
+    $this->setDefaultOperator('AND');
+    $this->setQuery($query_string);
+  }
 }
