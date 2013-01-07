f2e3c898742cac48cfd1964c2c9989cb86175c3f
mathurin <mpetit@actualys.com>
2013-01-07 18:53:28 +0100

    mis en forme de code

--- lib/csv/CsvFile.class.php
+++ lib/csv/CsvFile.class.php
@@ -43,14 +43,16 @@ class CsvFile
   public function getCsv() {
     if ($this->csvdata)
       return $this->csvdata;
-
     $handler = fopen($this->file, 'r');
     if (!$handler)
       throw new Exception('Cannot open csv file anymore');
     $this->csvdata = array();
     while (($data = fgetcsv($handler, 0, $this->separator)) !== FALSE) {
       if (!preg_match('/^#/', $data[0]))
-	$this->csvdata[] = $data;
+      {
+	$this->csvdata[] = $data;           
+      }
+        
     }
     fclose($handler);
     /*if ($this->ignore && !preg_match('/^\d{10}$/', $this->csvdata[0][0]))
