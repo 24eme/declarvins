6d1fca429ab90bf19bc23056594e2bdb95453a19
Mikael Guillin <mguillin@actualys.com>
2013-01-22 16:09:59 +0100

    Intégration d'éléments dans la rubrique profil

--- project/apps/declarvin/modules/tiers/templates/profilSuccess.php
+++ project/apps/declarvin/modules/tiers/templates/profilSuccess.php
@@ -1,11 +1,14 @@
 <?php include_component('global', 'navTop', array('active' => 'profil')); ?>
 <section id="contenu">
-<?php if ($hasCompte): ?>
-	<?php include_partial('form_compte', array('form' => $form, 'etablissement' => $etablissement)); ?>
-<?php endif; ?>
+	
+	<div id="profil">
+		<?php if ($hasCompte): ?>
+			<?php include_partial('form_compte', array('form' => $form, 'etablissement' => $etablissement)); ?>
+		<?php endif; ?>
 
-<?php include_partial('etablissement', array('etablissement' => $etablissement)); ?>
+		<?php include_partial('etablissement', array('etablissement' => $etablissement)); ?>
 
-<a href="<?php echo url_for('profil_statut', $etablissement) ?>" id="">Archiver l'etablissement</a>
+		<a href="<?php echo url_for('profil_statut', $etablissement) ?>" id="btn_archiver_etablissement" class="btn_violet">Archiver l'etablissement</a>
+	</div>
 
 </section>
