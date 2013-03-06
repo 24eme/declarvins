0d89f5733c8f44174845467ef32c9605fe976b32
Tangui Morlier <tangui@tangui.eu.org>
2013-03-06 17:26:56 +0100

    Ajout d'une protection sur la création des numéros d'archive

--- lib/Archivage/ArchivageDocument.class.php
+++ lib/Archivage/ArchivageDocument.class.php
@@ -28,7 +28,27 @@ class ArchivageDocument
 
         $type = $this->document->toJson()->type;
         
-        $last_numero = ArchivageAllView::getInstance()->getLastNumeroArchiveByTypeAndCampagne($type, $this->document->campagne);
-        $this->document->numero_archive = sprintf($this->format, $last_numero+1);
+	for($i = 0 ; $i < 10 ; $i++) {
+	  try {
+	    $this->getLock();
+	    $last_numero = ArchivageAllView::getInstance()->getLastNumeroArchiveByTypeAndCampagne($type, $this->document->campagne);
+	    $this->document->numero_archive = sprintf($this->format, $last_numero+1);
+	    $this->setLock($this->document->campagne.' '.$this->document->numero_archive);
+	    break;
+	  }catch(sfException $e) {
+	  }
+	}
+	if ($i >= 10) {
+	  throw new sfException('Could not acquire the archive lock');
+	}
+    }
+
+    private function getLock() {
+      $this->lock = Lock::getInstance();
+    }
+
+    private function setLock($value) {
+      $this->lock->add($this->document->type, $value);
+      $this->lock->save();
     }
 }
