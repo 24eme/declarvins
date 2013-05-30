59d28ae84cc9d8ff989c4fec460cfee1e26f1766
JB <jblemetayer@actualys.com>
2013-05-30 15:02:54 +0200

    optimisation pdf

--- project/plugins/DAIDSPlugin/modules/daids_export/templates/_pdf.php
+++ project/plugins/DAIDSPlugin/modules/daids_export/templates/_pdf.php
@@ -27,6 +27,28 @@
 	</script>
 	<?php include_partial('daids_export/pdfHeader', array('daids' => $daids)); ?>
 	<?php include_partial('daids_export/pdfFooter'); ?>
+	
+	
+	
+	<h2>Vins de la propriété</h2>
+	<table id="vins_propriete" class="triple_col bloc_bottom">
+		<tr>
+			<th><h2><?php echo $daids->entrepots->entrepot_a->libelle ?><?php if ($daids->entrepots->entrepot_a->principal):?> (principal)<?php endif; ?></h2></th>
+			<th><h2><?php echo $daids->entrepots->entrepot_b->libelle ?><?php if ($daids->entrepots->entrepot_b->principal):?> (principal)<?php endif; ?></h2></th>
+			<th><h2><?php echo $daids->entrepots->entrepot_c->libelle ?><?php if ($daids->entrepots->entrepot_c->principal):?> (principal)<?php endif; ?></h2></th>
+		</tr>
+		<tr>
+			<td class="col_left">
+				<p><?php echo $daids->entrepots->entrepot_a->commentaires ?></p>
+			</td>
+			<td class="col_center">
+				<p><?php echo $daids->entrepots->entrepot_b->commentaires ?></p>
+			</td>
+			<td class="col_right">
+				<p><?php echo $daids->entrepots->entrepot_c->commentaires ?></p>
+			</td>
+		</tr>
+	</table>
 
 	<?php $i=0; ?>
 	<?php foreach($daids->declaration->certifications as $certification_key => $certification): ?>
@@ -269,7 +291,7 @@
 																	   'hash' => 'total_douane')) ?>
 
 			</table>
-			
+			<hr />
 		<div class="legende">
 			<?php while($pagers_code[$certification_key]->getPage() <= $pagers_code[$certification_key]->getLastPage()): ?>
 			<?php $colonnes = $pagers_code[$certification_key]->getResults(); ?>
@@ -305,26 +327,6 @@
 		<hr />
 		<?php endwhile; ?>
 	<?php endforeach; ?>
-	
-	<h2>Vins de la propriété</h2>
-	<table id="vins_propriete" class="triple_col bloc_bottom">
-		<tr>
-			<th><h2><?php echo $daids->entrepots->entrepot_a->libelle ?><?php if ($daids->entrepots->entrepot_a->principal):?> (principal)<?php endif; ?></h2></th>
-			<th><h2><?php echo $daids->entrepots->entrepot_b->libelle ?><?php if ($daids->entrepots->entrepot_b->principal):?> (principal)<?php endif; ?></h2></th>
-			<th><h2><?php echo $daids->entrepots->entrepot_c->libelle ?><?php if ($daids->entrepots->entrepot_c->principal):?> (principal)<?php endif; ?></h2></th>
-		</tr>
-		<tr>
-			<td class="col_left">
-				<p><?php echo $daids->entrepots->entrepot_a->commentaires ?></p>
-			</td>
-			<td class="col_center">
-				<p><?php echo $daids->entrepots->entrepot_b->commentaires ?></p>
-			</td>
-			<td class="col_right">
-				<p><?php echo $daids->entrepots->entrepot_c->commentaires ?></p>
-			</td>
-		</tr>
-	</table>
 	
 	<?php if ($daids->valide->date_saisie): ?>
 	<?php while($pager_droits_douane->getPage() <= $pager_droits_douane->getLastPage()): ?>
