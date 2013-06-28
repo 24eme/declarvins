ab3bdfde008bc4a64eb2c0a140e296a322b84973
Hamza Iqbal <hiqbal@actualys.com>
2013-06-28 16:53:21 +0200

    Login Etablissement

--- project/apps/declarvin/modules/tiers/templates/loginSuccess.php
+++ project/apps/declarvin/modules/tiers/templates/loginSuccess.php
@@ -1,10 +1,14 @@
 <section id="contenu">
 	<h1>Sélectionnez votre établissement</h1>
-	<div id="creation_compte">
+	<div id="selection_etablissement">
 		<form action="<?php echo url_for('@tiers') ?>" method="post">
 			<?php echo $form->renderHiddenFields(); ?>
 			<?php echo $form->renderGlobalErrors(); ?>
-			<?php echo $form['tiers']->render(array('onchange' => 'submit()')) ?>
+			<?php echo $form['tiers']->render() ?>
+			
+			<div class="ligne_form_btn">
+				<button class="btn_valider" type="submit">Valider</button>
+			</div>
 		</form>
 	</div>
 </section>
