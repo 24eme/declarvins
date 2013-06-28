1c2068bb745624deea1791a701e58d7531497111
Hamza Iqbal <hiqbal@actualys.com>
2013-06-28 16:52:49 +0200

    Bouton validation contrat vrac

--- modules/vrac/templates/validationSuccess.php
+++ modules/vrac/templates/validationSuccess.php
@@ -19,19 +19,23 @@
             	<br />
                 <?php include_partial('showContrat', array('configurationVrac' => $configurationVrac, 'etablissement' => $etablissement, 'vrac' => $vrac, 'editer_etape' => false)); ?>
                 
-                <div class="ligne_form_btn">
-					<?php if (!$vrac->isValide() && !$dateValidationActeur): ?>
-					<a class="supprimer" onclick="return confirm('Confirmez-vous la suppression du contrat?')" href="<?php echo url_for('vrac_supprimer', array('sf_subject' => $vrac, 'etablissement' => $etablissement)) ?>" id="btn_annuler_contrat">Refuser</a>
-                	<form action="<?php echo url_for('vrac_validation', array('sf_subject' => $vrac, 'etablissement' => $etablissement, 'acteur' => $acteur)) ?>" method="post" id="vrac_condition">
+				
+		   
+				<?php if (!$vrac->isValide() && !$dateValidationActeur): ?>
+				<form action="<?php echo url_for('vrac_validation', array('sf_subject' => $vrac, 'etablissement' => $etablissement, 'acteur' => $acteur)) ?>" method="post" id="vrac_condition">
+					 <div class="ligne_form_btn">
+						<a class="annuler_saisie" onclick="return confirm('Confirmez-vous la suppression du contrat?')" href="<?php echo url_for('vrac_supprimer', array('sf_subject' => $vrac, 'etablissement' => $etablissement)) ?>" id="btn_annuler_contrat">Refuser</a>
 						<?php echo $form->renderHiddenFields() ?>
 						<?php echo $form->renderGlobalErrors() ?>
-						<input class="valider_etape" type="submit" value="Valider"  />
-					</form>
-					<?php endif; ?>
-					<?php if ($dateValidationActeur): ?>
+						 <button class="valider_etape" type="submit"><span>Valider</span></button>
+					</div>
+				</form>
+				<?php endif; ?>
+				<?php if ($dateValidationActeur): ?>
+				<div class="ligne_form_btn">
 					<p>Vous avez validé ce contrat le <?php echo strftime('%d/%m/%Y à %Hh%M', strtotime($dateValidationActeur)); ?></p>
-					<?php endif; ?>
 				</div>
+				<?php endif; ?>
                 
             </div> 
         </section>
