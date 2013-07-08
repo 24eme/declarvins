d3daebb6fda7afa0301c1c1130c2396732630cca
Mikael Guillin <mguillin@actualys.com>
2013-07-08 12:47:21 +0200

    Intégration du formulaire de filtrage

---
+++ project/plugins/StatistiquePlugin/modules/statistique/templates/_drmSearchOptions.php
@@ -0,0 +1,31 @@
+<nav class="options_recherche">
+	<ul>
+		<li>
+			<a href="#">Déclarant</a>
+			<ul>
+				<li><a href="#form_ajout" data-filtre="#filtre_etablissements">Etablissement</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_code_postal">Code postal</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_service_douane">Service Douane</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_famille">Famille</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_sous_famille">Sous famille</a></li>
+			</ul>
+		</li>
+		<li>
+			<a href="#">Document</a>
+			<ul>
+				<li><a href="#form_ajout" data-filtre="#filtre_identifiant">Identifiant</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_identifiant_historique">Identifiant historique</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_date_saisie">Date de saisie</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_mode_saisie">Mode de saisie</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_periode">Période</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_campagne">Campagne</a></li>
+			</ul>
+		</li>
+		<li>
+			<a href="#">Produit</a>
+			<ul>
+				<li><a href="#form_ajout" data-filtre="#filtre_produits">Produit</a></li>
+			</ul>
+		</li>
+	</ul>
+</nav>
---
+++ project/plugins/StatistiquePlugin/modules/statistique/templates/_vracSearchOptions.php
@@ -0,0 +1,40 @@
+<nav class="options_recherche">
+	<ul>
+		<li>
+			<a href="#">Acteurs</a>
+			<ul>
+				<li><a href="#form_ajout" data-filtre="#filtre_vendeur_identifiant">Vendeur</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_acheteur_identifiant">Acheteur</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_mandataire_identifiant">Courtier</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_code_postal_vendeur">Vendeur code postal</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_code_postal_acheteur">Acheteur code postal</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_famille_vendeur">Vendeur famille</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_sous_famille_vendeur">Vendeur sous famille</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_famille_acheteur">Acheteur famille</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_sous_famille_acheteur">Acheteur sous famille</a></li>
+			</ul>
+		</li>
+		<li>
+			<a href="#">Document</a>
+			<ul>
+				<li><a href="#form_ajout" data-filtre="#filtre_identifiant">Identifiant</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_cas_particuliers">Conditions particulières</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_type_transaction">Type de transaction</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_export">Conditionné export</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_annexe">Présence annexe</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_type_prix">Type de prix</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_date_limite_retiraison">Date limite de retiraison</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_date_saisie">Date de saisie</a></li>
+			</ul>
+		</li>
+		<li>
+			<a href="#">Produit</a>
+			<ul>
+				<li><a href="#form_ajout" data-filtre="#filtre_produit">Produit</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_millesime">Millesime</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_label">Label</a></li>
+				<li><a href="#form_ajout" data-filtre="#filtre_mention">Mention</a></li>
+			</ul>
+		</li>
+	</ul>
+</nav>
