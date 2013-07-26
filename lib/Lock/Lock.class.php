68a6ab01117ca1ce9ba82ba2fa47227087c48152
Vincent LAURENT <vlaurent@actualys.com>
2013-07-26 17:58:38 +0200

    Meilleur système de concurrence pour le Lock

--- lib/Lock/Lock.class.php
+++ lib/Lock/Lock.class.php
@@ -16,21 +16,31 @@ class Lock extends BaseLock {
   public static function runLock($ilockobject, $args =  null) {
     for($i = 0 ; $i < 10 ; $i++) {
       try {
-	$lock = Lock::getInstance();
-	$res = $ilockobject->executeLock($args);
-	if (!isset($res['key'])) {
-	  throw new sfException('executeLock needs to return a hashtable with an element "key"');
-	}
-	if (!isset($res['value'])) {
-	  throw new sfException('executeLock needs to return a hashtable with an element "value"');
-	}
-	$lock->add($res['key'], $res['value']);
-	$lock->save();
-	break;
+        $lock = Lock::getInstance();
+        $res = $ilockobject->executeLock($args);
+        if (!isset($res['key'])) {
+
+          throw new sfException('executeLock needs to return a hashtable with an element "key"');
+        }
+        if (!isset($res['value'])) {
+
+          throw new sfException('executeLock needs to return a hashtable with an element "value"');
+        }
+        if ($lock->exist($res['key']) && $lock->get($res['key']) == $res['value']) {
+
+          throw new sfException('the archive value need to be different from the lock one');
+        }
+        $lock->add($res['key'], $res['value']); 
+
+        $lock->save();
+        break;
       }catch(sfException $e) {
+        echo $e->getMessage()."\n";
       }
+      sleep(1);
     }
     if ($i >= 10) {
+
       throw new sfException('Could not acquire the archive lock');
     }
   }
