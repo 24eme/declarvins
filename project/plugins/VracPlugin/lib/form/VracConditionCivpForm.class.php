c9f448352a88ff5600d42625c67a327398edc96c
JB <jblemetayer@actualys.com>
2013-11-22 17:39:08 +0100

    correction message erreur

--- project/plugins/VracPlugin/lib/form/VracConditionCivpForm.class.php
+++ project/plugins/VracPlugin/lib/form/VracConditionCivpForm.class.php
@@ -3,7 +3,7 @@ class VracConditionCivpForm extends VracConditionForm
 {
     public function configure() {
         parent::configure();
-		$this->getWidget('conditions_paiement')->setLabel('Conditions de vente:');
+		$this->getWidget('conditions_paiement')->setLabel('Conditions de vente*:');
 		$this->getWidget('reference_contrat_pluriannuel')->setLabel('Si vous avez un contrat pluriannuel écrit, veuillez noter sa référence:');
     }
 
