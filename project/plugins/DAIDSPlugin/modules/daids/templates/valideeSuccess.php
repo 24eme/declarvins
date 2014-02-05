36be599225536badbf41db83226e09d299d0d711
Vincent LAURENT <vlaurent@actualys.com>
2014-02-05 17:58:04 +0100

    Page d'erreur quand édition d'une daids validee non adapté

--- project/plugins/DAIDSPlugin/modules/daids/templates/valideeSuccess.php
+++ project/plugins/DAIDSPlugin/modules/daids/templates/valideeSuccess.php
@@ -1,10 +1,10 @@
-<?php include_component('global', 'navTop', array('active' => 'drm')); ?>
+<?php include_component('global', 'navTop', array('active' => 'daids')); ?>
 <section id="contenu">
 	<h1>Un problème est survenu</h1>
-	<p>Vous avez déjà validé votre DRM.</p>
-	<p>Si vous souhaitez effectuer des corrections, veuillez saisir une DRM rectificative.</p>
+	<p>Vous avez déjà validé votre DAIDS.</p>
+	<p>Si vous souhaitez effectuer des corrections, veuillez saisir une DAIDS rectificative.</p>
 	<div id="btn_etape_dr">
-    	<a href="<?php echo url_for('drm_mon_espace', $etablissement) ?>" class="btn_suiv">
+    	<a href="<?php echo url_for('daids_mon_espace', $etablissement) ?>" class="btn_suiv">
 			<span>Retour à mon espace</span>
 		</a>
 	</div>
