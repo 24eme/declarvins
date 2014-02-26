95c1b48def437c4699892c14e6e79be4fe573543
JB <jblemetayer@actualys.com>
2014-02-26 12:23:36 +0100

    task debug etablissements

---
+++ project/lib/task/debugEtablissementsTask.class.php
@@ -0,0 +1,50 @@
+<?php
+
+class debugEtablissementsTask extends sfBaseTask
+{
+  protected function configure()
+  {
+    // // add your own arguments here
+    // $this->addArguments(array(
+    //   new sfCommandArgument('my_arg', sfCommandArgument::REQUIRED, 'My argument'),
+    // ));
+
+    $this->addOptions(array(
+      new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name', 'declarvin'),
+      new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+      new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'default'),
+      // add your own options here
+    ));
+
+    $this->namespace        = 'debug';
+    $this->name             = 'etablissements';
+    $this->briefDescription = '';
+    $this->detailedDescription = <<<EOF
+
+EOF;
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    // initialize the database connection
+    $databaseManager = new sfDatabaseManager($this->configuration);
+    $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
+    
+  		$rows = acCouchdbManager::getClient()
+              ->getView("etablissement", "all")
+              ->rows;
+      $i = 0;
+      foreach($rows as $row) {
+      	$etab = EtablissementClient::getInstance()->find($row->key[4]);
+      	if ($etab) {
+	        $etab->remove('produits');
+	        $etab->add('produits');
+	        $etab->save();
+	        $this->logSection("debug", $etab->get('_id'), null, 'SUCCESS');
+	        $i++;
+      	}
+      }
+      $this->logSection("debug", $i." etablissement(s) debuggué(s) avec succès", null, 'SUCCESS');
+    
+  }
+}
