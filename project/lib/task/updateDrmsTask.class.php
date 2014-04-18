7663a5943a84db503d2f2f4121969ba8f0232dd3
JB <jblemetayer@actualys.com>
2014-04-18 16:08:40 +0200

    Mise a jour des drm modifiées main

--- project/lib/task/updateDrmsTask.class.php
+++ project/lib/task/updateDrmsTask.class.php
@@ -26,25 +26,33 @@ EOF;
         // initialize the database connection
         $databaseManager = new sfDatabaseManager($this->configuration);
         $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
-        $campagne = null;
-        
-        $drms = DRMClient::getInstance()->findAll();
-        $i = 1;
-        $nb = count($drms->rows);
-        foreach ($drms->rows as $d) {
-        	if ($drm = DRMClient::getInstance()->find($d->id, acCouchdbClient::HYDRATE_DOCUMENT)) {
-        		if ($drm->declaration->certifications->exist('Sans IG')) {
-        			$drm->declaration->certifications->remove('Sans IG');
-        			$drm->declaration->certifications->add('VINSSANSIG');
-        			$drm->save();
-        			$this->logSection('drm', $drm->get('_id')." : succès de la mise à jour arborescence $i.");
-        		}
-        		if (!$drm->declarant->raison_sociale) {
-        			$drm->setEtablissementInformations();
-        			$drm->save();
-        			$this->logSection('drm', $drm->get('_id')." : succès de la mise à jour établissement $i.");
-        		}
-				$i++;
+        $drms = array(
+        			'DRM-C1979-2014-01', 
+        			'DRM-C1557-2014-01', 
+        			'DRM-C8793-2014-01', 
+        			'DRM-C2428-2013-12-M01', 
+        			'DRM-C7221-2014-01', 
+        			'DRM-C2536-2014-01', 
+        			'DRM-C0813-2014-01', 
+        			'DRM-C0266-2014-01', 
+        			'DRM-C8575-2014-01',
+        			'DRM-C0474-2014-01',
+        			'DRM-C1418-2014-01',
+        			'DRM-C1418--2014-01-M01',
+        			'DRM-C7080-2014-01',
+        			'DRM-C2898-2014-01',
+        			'DRM-C0461-2014-01',
+        			'DRM-C1601-2014-01',
+        			'DRM-C2832-2014-01',
+        			'DRM-C0241-2014-01',
+        			'DRM-C7659-2014-01',
+        			'DRM-C7000-2014-01'
+        );
+        foreach ($drms as $drm) {
+        	if ($d = DRMClient::getInstance()->find($drm)) {
+        		$d->update();
+        		$d->save();
+        		$this->logSection('drm', $drm." : succès de la mise à jour.");
         	}
         }
     }
