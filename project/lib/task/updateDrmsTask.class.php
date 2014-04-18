594e4b79bbb43e63f8005530e77af075e15569d6
JB <jblemetayer@actualys.com>
2014-04-18 16:44:52 +0200

    Mise a jour des drm modifiées main

--- project/lib/task/updateDrmsTask.class.php
+++ project/lib/task/updateDrmsTask.class.php
@@ -27,25 +27,6 @@ EOF;
         $databaseManager = new sfDatabaseManager($this->configuration);
         $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
         $drms = array(
-        			'DRM-C1979-2014-01', 
-        			'DRM-C1557-2014-01', 
-        			'DRM-C8793-2014-01', 
-        			'DRM-C2428-2013-12-M01', 
-        			'DRM-C7221-2014-01', 
-        			'DRM-C2536-2014-01', 
-        			'DRM-C0813-2014-01', 
-        			'DRM-C0266-2014-01', 
-        			'DRM-C8575-2014-01',
-        			'DRM-C0474-2014-01',
-        			'DRM-C1418-2014-01',
-        			'DRM-C1418--2014-01-M01',
-        			'DRM-C7080-2014-01',
-        			'DRM-C2898-2014-01',
-        			'DRM-C0461-2014-01',
-        			'DRM-C1601-2014-01',
-        			'DRM-C2832-2014-01',
-        			'DRM-C0241-2014-01',
-        			'DRM-C7659-2014-01',
         			'DRM-C7000-2014-01'
         );
         foreach ($drms as $drm) {
