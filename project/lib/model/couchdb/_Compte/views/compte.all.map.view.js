4c5d0a02831cf0c2fc78da1b0f6999eef2cf6447
Vincent LAURENT <vlaurent@actualys.com>
2014-09-01 18:25:41 +0200

    Vue pas à jour par rapport à celle de prod

--- project/lib/model/couchdb/_Compte/views/compte.all.map.view.js
+++ project/lib/model/couchdb/_Compte/views/compte.all.map.view.js
@@ -7,5 +7,6 @@ function(doc) {
     for(interpro_id in doc.interpro) {        
         emit([interpro_id, doc.type, doc.statut], [numero_contrat, doc.nom, doc.prenom, doc.login, doc.email, doc.raison_sociale, doc.telephone, doc.oioc]);
     }
+    emit([doc.contrat_valide, doc.type, doc.statut], [numero_contrat, doc.nom, doc.prenom, doc.login, doc.email, doc.raison_sociale, doc.telephone, doc.oioc]);
 
 }
