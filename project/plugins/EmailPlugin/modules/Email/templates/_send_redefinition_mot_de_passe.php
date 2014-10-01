2ec08d87ff5f52ba26545536dd9864d8583c67ea
JB <jblemetayer@actualys.com>
2014-10-01 11:25:16 +0200

    Modification texte email nouveau mot de passe

--- project/plugins/EmailPlugin/modules/Email/templates/_send_redefinition_mot_de_passe.php
+++ project/plugins/EmailPlugin/modules/Email/templates/_send_redefinition_mot_de_passe.php
@@ -8,7 +8,7 @@ Le ou les comptes suivants sont liés à votre adresse email :<br />
 	<li>Login : <strong><?php echo $login ?></strong>, merci de suivre la procédure suivante en cliquant sur ce lien : <a href="<?php echo ProjectConfiguration::getAppRouting()->generate('compte_password', array('login' => $login), true); ?>">Redéfinition de mon mot de passe</a></li>
 <?php endforeach; ?>
 </ul>
-Vous pourrez ainsi accéder à la plateforme en vous connectant avec votre login : <strong><?php echo $compte->login ?></strong> et votre mot de passe nouvellement créé.
+Vous pourrez ainsi accéder à la plateforme en vous connectant avec votre login et votre mot de passe nouvellement créé.
 Pour toute question, n'hésitez pas à <a href="<?php echo ProjectConfiguration::getAppRouting()->generate('contact', array(), true); ?>">contacter votre interprofession</a><br /><br />
 L'équipe Declarvins.net
 
