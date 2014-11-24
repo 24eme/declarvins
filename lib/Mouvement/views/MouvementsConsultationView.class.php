faef480f1b45c0e1fdad6f43a98b3a56da9fdcaf
mathurin <mpetit@actualys.com>
2014-11-24 18:03:13 +0100

    maj des mouvements

--- lib/Mouvement/views/MouvementsConsultationView.class.php
+++ lib/Mouvement/views/MouvementsConsultationView.class.php
@@ -1,7 +1,7 @@
 <?php
 
-class MouvementsConsultationView extends acCouchdbView
-{
+class MouvementsConsultationView extends acCouchdbView {
+
     const KEY_TYPE = 0;
     const KEY_ETABLISSEMENT_IDENTIFIANT = 1;
     const KEY_CAMPAGNE = 2;
@@ -11,7 +11,6 @@ class MouvementsConsultationView extends acCouchdbView
     const KEY_TYPE_HASH = 6;
     const KEY_VRAC_NUMERO = 7;
     const KEY_DETAIL_IDENTIFIANT = 8;
-
     const VALUE_ETABLISSEMENT_NOM = 0;
     const VALUE_PRODUIT_LIBELLE = 1;
     const VALUE_TYPE_LIBELLE = 2;
@@ -26,7 +25,6 @@ class MouvementsConsultationView extends acCouchdbView
 
     public static $types_document = array("DRM", "SV12");
 
-
     public static function getInstance() {
 
         return acCouchdbManager::getView('mouvement', 'consultation');
@@ -35,7 +33,7 @@ class MouvementsConsultationView extends acCouchdbView
     public function getByIdentifiantAndCampagne($id_or_identifiant, $campagne) {
         $identifiant = EtablissementClient::getInstance()->getIdentifiant($id_or_identifiant);
         $mouvements = array();
-        foreach(self::$types_document as $type) {
+        foreach (self::$types_document as $type) {
             $mouvements = array_merge($mouvements, $this->buildMouvements($this->findByTypeEtablissementAndCampagne($type, $identifiant, $campagne)->rows));
         }
 
@@ -48,31 +46,31 @@ class MouvementsConsultationView extends acCouchdbView
         $identifiant = EtablissementClient::getInstance()->getIdentifiant($id_or_identifiant);
 
         return $this->client->startkey(array($type, $identifiant))
-                            ->endkey(array($type, $identifiant, array()))
-                            ->getView($this->design, $this->view);
+                        ->endkey(array($type, $identifiant, array()))
+                        ->getView($this->design, $this->view);
     }
 
     public function findByTypeEtablissementAndCampagne($type, $id_or_identifiant, $campagne) {
         $identifiant = EtablissementClient::getInstance()->getIdentifiant($id_or_identifiant);
         return $this->client->startkey(array($type, $identifiant, $campagne))
-                            ->endkey(array($type, $identifiant, $campagne, array()))
-                            ->getView($this->design, $this->view);
+                        ->endkey(array($type, $identifiant, $campagne, array()))
+                        ->getView($this->design, $this->view);
     }
 
     public function findByTypeEtablissementAndPeriode($type, $id_or_identifiant, $campagne, $periode) {
         $identifiant = EtablissementClient::getInstance()->getIdentifiant($id_or_identifiant);
 
         return $this->client->startkey(array($type, $identifiant, $campagne, $periode))
-                            ->endkey(array($type, $identifiant, $campagne, $periode, array()))
-                            ->getView($this->design, $this->view);
+                        ->endkey(array($type, $identifiant, $campagne, $periode, array()))
+                        ->getView($this->design, $this->view);
     }
 
     protected function buildMouvements($rows) {
         $mouvements = array();
-        foreach($rows as $row) {
+        foreach ($rows as $row) {
             $mouvement = $this->buildMouvement($row);
-            $mouvement_sort = sprintf('%02d', str_replace('M', '', $mouvement->version)*1);
-            $mouvements[$mouvement->date_version.$mouvement->type.$mouvement_sort.$mouvement->doc_id.$mouvement->id] = $mouvement;
+            $mouvement_sort = sprintf('%02d', str_replace('M', '', $mouvement->version) * 1);
+            $mouvements[$mouvement->date_version . $mouvement->type . $mouvement_sort . $mouvement->doc_id . $mouvement->id] = $mouvement;
         }
 
         return $mouvements;
@@ -89,28 +87,30 @@ class MouvementsConsultationView extends acCouchdbView
         $mouvement->type_libelle = $row->value[self::VALUE_TYPE_LIBELLE];
         $mouvement->volume = $row->value[self::VALUE_VOLUME];
         $mouvement->detail_identifiant = $row->key[self::KEY_DETAIL_IDENTIFIANT];
-        $mouvement->detail_libelle = $row->value[self::VALUE_DETAIL_LIBELLE];        
-        $mouvement->date_version =  $row->value[self::VALUE_DATE_VERSION];
+        $mouvement->detail_libelle = $row->value[self::VALUE_DETAIL_LIBELLE];
+        $mouvement->date_version = $row->value[self::VALUE_DATE_VERSION];
         $mouvement->version = $row->value[self::VALUE_VERSION];
-        $mouvement->vrac_numero =  $row->key[self::KEY_VRAC_NUMERO];
-        $mouvement->vrac_destinataire =  $row->value[self::VALUE_VRAC_DESTINATAIRE];
-        $mouvement->cvo =  $row->value[self::VALUE_CVO];
-        $mouvement->facturable =  $row->value[self::VALUE_FACTURABLE];
+        $mouvement->vrac_numero = $row->key[self::KEY_VRAC_NUMERO];
+        $mouvement->vrac_destinataire = $row->value[self::VALUE_VRAC_DESTINATAIRE];
+        $mouvement->cvo = $row->value[self::VALUE_CVO];
+        $mouvement->facturable = $row->value[self::VALUE_FACTURABLE];
         $mouvement->id = $row->value[self::VALUE_MOUVEMENT_ID];
-	if ($mouvement->vrac_numero) {
-	  $mouvement->numero_archive = $row->value[self::VALUE_TYPE_LIBELLE];
-	  if (strlen($mouvement->numero_archive) != 5) {
-	    $vrac = VracClient::getInstance()->find('VRAC-'.$mouvement->vrac_numero);
-	    $mouvement->numero_archive = $vrac->numero_archive;
-	  }
-	}
+        if ($mouvement->vrac_numero) {
+            $mouvement->numero_archive = $row->value[self::VALUE_TYPE_LIBELLE];
+            if (strlen($mouvement->numero_archive) != 5) {
+                $vrac = VracClient::getInstance()->find('VRAC-' . $mouvement->vrac_numero);
+                if ($vrac->exist('numero_archive')) {
+                    $mouvement->numero_archive = $vrac->numero_archive;
+                }
+            }
+        }
         return $mouvement;
     }
 
     public function getWords($mouvements) {
         $words = array();
-        
-        foreach($mouvements as $mouvement) {
+
+        foreach ($mouvements as $mouvement) {
             $words[] = $this->getWord($mouvement);
         }
 
@@ -118,7 +118,7 @@ class MouvementsConsultationView extends acCouchdbView
     }
 
     protected function getWord($mouvement) {
-
+        
     }
 
-}  
+}
