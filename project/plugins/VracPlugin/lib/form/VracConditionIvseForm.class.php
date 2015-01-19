26265ccc6f203c869d6ea60a5243cf9808c07ab5
Jean-Baptiste LE METAYER <jblemetayer@actualys.com>
2015-01-19 17:37:10 +0100

    Adaptation contrat IVSE

--- project/plugins/VracPlugin/lib/form/VracConditionIvseForm.class.php
+++ project/plugins/VracPlugin/lib/form/VracConditionIvseForm.class.php
@@ -3,8 +3,19 @@ class VracConditionIvseForm extends VracConditionForm
 {
     public function configure() {
         parent::configure();
-		$this->getWidget('conditions_paiement')->setLabel('Conditions de vente');
+		$this->getWidget('conditions_paiement')->setLabel('Conditions de vente*:');
+		$this->getWidget('reference_contrat_pluriannuel')->setLabel('Si vous avez un contrat pluriannuel écrit, veuillez noter sa référence:');
+		$this->setWidget('vin_livre', new sfWidgetFormInputHidden());
         unset($this['clause_reserve_retiraison']);
-        unset($this['date_debut_retiraison']);
+    }
+
+
+    public function conditionneReferenceContrat() {
+      return false;
+    }
+
+
+    public function conditionneIVSE() {
+      return true;
     }
 }
