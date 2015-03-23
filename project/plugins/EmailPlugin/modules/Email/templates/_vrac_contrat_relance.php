69158f32831cd0fb57605459e882c6cbfd2a3e0c
Jean-Baptiste LE METAYER <jblemetayer@actualys.com>
2015-03-23 17:37:31 +0100

    pb lien

--- project/plugins/EmailPlugin/modules/Email/templates/_vrac_contrat_relance.php
+++ project/plugins/EmailPlugin/modules/Email/templates/_vrac_contrat_relance.php
@@ -38,13 +38,13 @@ Courtier :<br />
 <br />
 <?php endif; ?>
 Commentaire : <?php echo $vrac->commentaires ?><br /><br />
-<strong>Si vous souhaitez valider ou refuser ce contrat, cliquez sur le lien suivant : <a href="<?php echo url_for('vrac_validation', array('sf_subject' => $vrac, 'etablissement' => $etablissement, 'acteur' => $acteur), true); ?>">Valider ou refuser le contrat</a></strong><br /><br />
-Si vous n'êtes pas inscrit sur DeclarVins.net, nous vous invitons à vous inscrire en suivant le lien : <a href="<?php echo url_for('homepage', array(), true); ?>">Inscription à DeclarVins.net</a><br />
+<strong>Si vous souhaitez valider ou refuser ce contrat, cliquez sur le lien suivant : <a href="<?php echo ProjectConfiguration::getAppRouting()->generate('vrac_validation', array('sf_subject' => $vrac, 'etablissement' => $etablissement, 'acteur' => $acteur), true); ?>">Valider ou refuser le contrat</a></strong><br /><br />
+Si vous n'êtes pas inscrit sur DeclarVins.net, nous vous invitons à vous inscrire en suivant le lien : <a href="<?php echo ProjectConfiguration::getAppRouting()->generate('homepage', array(), true); ?>">Inscription à DeclarVins.net</a><br />
 Le contrat ne sera valable que lorsque vous aurez reçu la version pdf faisant figurer le numéro de contrat.<br /><br />
 Attention si le contrat n'est pas validé dans les 10 jours à compter de sa date de saisie, il sera automatiquement supprimé et non valable.<br />
 Si un des partenaires n'est pas inscrit sur DeclarVins, l'interprofession peut valider un contrat interprofessionnel en back office si elle est en possession d'une contrepartie écrite, signée. Une fois que toutes les parties ont signé, cela crée un numéro de VISA et envoie un mail avec le contrat en pdf à toutes les parties.<br /><br />
 Aidez vos partenaires à s'inscrire pour profiter des services de DeclarVins et gagner du temps, notamment signer en ligne les contrats interprofessionnels : affectation d'un numéro de VISA, et envoi d'un mail avec le contrat valide en pdf à toutes les parties IMMEDIAT (dès signature par la dernière partie concernée).<br /><br />
-Pour toute information, vous pouvez <a href="<?php echo url_for('contact', array(), true); ?>">contacter votre interprofession</a><br /><br />
+Pour toute information, vous pouvez <a href="<?php echo ProjectConfiguration::getAppRouting()->generate('contact', array(), true); ?>">contacter votre interprofession</a><br /><br />
 Cordialement,<br /><br />
 L'équipe Declarvins.net 
 
