e265cac551cbb6048c94ea3b14ea1b909bded319
Jb Lm <lemetayer.jb@gmail.com>
2015-06-11 15:33:29 +0200

    Ticket 14259

--- project/plugins/acVinDRMPlugin/modules/drm/templates/informationsSuccess.php
+++ project/plugins/acVinDRMPlugin/modules/drm/templates/informationsSuccess.php
@@ -57,7 +57,7 @@
                     <div class="ligne_form">
 						<?php echo $form['confirmation']->renderError() ?>
 					</div>
-					<?php echo $form['confirmation']->render() ?>
+					<strong><?php echo $form['confirmation']->render() ?></strong>
 					<?php endif; ?>
 					<div class="ligne_btn">
 						<button type="submit" class="btn_suiv"><span>VALIDER</span></button>
