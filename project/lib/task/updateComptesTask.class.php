3d80cc8c1a4b29b099f957aeee8620d8983d3791
Jb Lm <lemetayer.jb@gmail.com>
2015-10-08 15:23:46 +0200

    Ticket 16076

--- project/lib/task/updateComptesTask.class.php
+++ project/lib/task/updateComptesTask.class.php
@@ -26,37 +26,20 @@ EOF;
         $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
         $campagne = null;
         
-        $fictifs = CompteAllView::getInstance()->findBy(1, 'CompteTiers', _Compte::STATUT_FICTIF)->rows;
-        $attentes = CompteAllView::getInstance()->findBy('INTERPRO-IR', 'CompteTiers', _Compte::STATUT_ATTENTE)->rows;
-        $inscrits = CompteAllView::getInstance()->findBy('INTERPRO-IR', 'CompteTiers', _Compte::STATUT_INSCRIT)->rows;
-        foreach ($fictifs as $f) {
-        	if ($f->value[CompteAllView::VALUE_NUMERO_CONTRAT]) {
-        		if ($object = ContratClient::getInstance()->find('CONTRAT-'.$f->value[CompteAllView::VALUE_NUMERO_CONTRAT])) {
-        			foreach ($object->etablissements as $etab) {
-        				echo _Compte::STATUT_FICTIF.';'.$object->no_contrat.';'.$etab->raison_sociale.';'.$etab->email.';'.$etab->adresse.';'.$etab->code_postal.';'.$etab->commune.';'.$etab->siret.';'.$object->nom.';'.$object->prenom.';'.$object->email;
-        				echo "\n";
-        			}	
-        		}				
-        	}
-        }
-    	foreach ($attentes as $a) {
-        	if ($a->value[CompteAllView::VALUE_NUMERO_CONTRAT]) {
-        		if ($object = ContratClient::getInstance()->find('CONTRAT-'.$a->value[CompteAllView::VALUE_NUMERO_CONTRAT])) {
-        			foreach ($object->etablissements as $etab) {
-        				echo _Compte::STATUT_ATTENTE.';'.$object->no_contrat.';'.$etab->raison_sociale.';'.$etab->email.';'.$etab->adresse.';'.$etab->code_postal.';'.$etab->commune.';'.$etab->siret.';'.$object->nom.';'.$object->prenom.';'.$object->email;
-        				echo "\n";
-        			}	
-        		}				
-        	}
-        }
-    	foreach ($inscrits as $i) {
-        	if ($i->value[CompteAllView::VALUE_NUMERO_CONTRAT]) {
-        		if ($object = ContratClient::getInstance()->find('CONTRAT-'.$i->value[CompteAllView::VALUE_NUMERO_CONTRAT])) {
-        			foreach ($object->etablissements as $etab) {
-        				echo _Compte::STATUT_INSCRIT.';'.$object->no_contrat.';'.$etab->raison_sociale.';'.$etab->email.';'.$etab->adresse.';'.$etab->code_postal.';'.$etab->commune.';'.$etab->siret.';'.$object->nom.';'.$object->prenom.';'.$object->email;
-        				echo "\n";
-        			}	
-        		}				
+
+
+        $comptes = CompteAllView::getInstance()->findAll()->rows;
+        $ldap = new Ldap();
+        foreach ($comptes as $compte) {
+        	if ($compte->key[CompteAllView::KEY_TYPE] == 'CompteTiers' && $compte->key[CompteAllView::KEY_STATUT] == _Compte::STATUT_INSCRIT) {
+        		if ($c = _CompteClient::getInstance()->find($compte->id)) {
+  					$result = $ldap->saveCompte($c);
+  					if (!$result) {
+  						$this->logSection("update", $compte->id." bug enregistrement LDAP", null, 'ERROR');
+  					} else {
+  						$this->logSection("update", $compte->id." enregistré avec succès", null, 'SUCCESS');
+  					}
+        		}
         	}
         }
     }
