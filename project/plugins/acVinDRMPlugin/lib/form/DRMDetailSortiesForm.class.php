d71d6a6a6402841940c6486fc5271103508f1c3d
Jb Lm <lemetayer.jb@gmail.com>
2015-12-18 14:24:20 +0100

    Bug validateurs 16570

--- project/plugins/acVinDRMPlugin/lib/form/DRMDetailSortiesForm.class.php
+++ project/plugins/acVinDRMPlugin/lib/form/DRMDetailSortiesForm.class.php
@@ -12,6 +12,7 @@ class DRMDetailSortiesForm  extends acCouchdbObjectForm {
     		} else {
     			$this->setWidget($key, new sfWidgetFormInputFloat(array('float_format' => "%01.04f")));
     		}
+    		$this->setValidator($key, new sfValidatorNumber(array('required' => false)));
     	}
         $this->widgetSchema->setNameFormat('drm_detail_sorties[%s]');
         $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
