bbcdd38b1bc846eb36033c9b8f48b18ccfeade34
Jb Lm <lemetayer.jb@gmail.com>
2016-01-14 16:36:36 +0100

    debug

--- project/plugins/acVinDRMPlugin/lib/model/DRM/DRMDetail.class.php
+++ project/plugins/acVinDRMPlugin/lib/model/DRM/DRMDetail.class.php
@@ -484,7 +484,7 @@ class DRMDetail extends BaseDRMDetail {
             $facturableArray = $this->getIsFacturableArray();
 
             $mouvement = DRMMouvement::freeInstance($this->getDocument());
-            echo $this->getHash()."<br />";
+            echo $this->getCepage()->getHash()."<br />";
             $mouvement->produit_hash = $this->getCepage()->getConfig()->getHash();
             $mouvement->facture = 0;
             $mouvement->interpro = $this->interpro;
