0f5af6573001a9e303b42786a1dd50f35b8a5a0c
Jb Lm <lemetayer.jb@gmail.com>
2016-08-23 18:04:17 +0200

    datepicker champs vrac prix definitif

--- project/plugins/acVinVracPlugin/modules/vrac/templates/_form_marche.php
+++ project/plugins/acVinVracPlugin/modules/vrac/templates/_form_marche.php
@@ -74,7 +74,7 @@
             <div id="bloc_vrac_determination_prix_date" class="section_label_strong bloc_conditionner" data-condition-value="<?php echo implode("|", $form->getTypePrixNeedDetermination()) ?>">
                 <?php echo $form['determination_prix_date']->renderError() ?>
                 <?php echo $form['determination_prix_date']->renderLabel('Date de détermination du prix définitif*: <a href="" class="msg_aide" data-msg="help_popup_vrac_determination_prix_date" title="Message aide"></a>') ?>
-                <?php echo $form['determination_prix_date']->render() ?> 
+                <?php echo $form['determination_prix_date']->render(array('class' => 'datepicker')) ?> 
             </div>
             <div id="bloc_vrac_determination_prix" class="section_label_strong bloc_conditionner" data-condition-value="<?php echo implode("|", $form->getTypePrixNeedDetermination()) ?>">
                 <?php echo $form['determination_prix']->renderError() ?>
