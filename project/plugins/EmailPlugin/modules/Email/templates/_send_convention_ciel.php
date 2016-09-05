7f80a832da6dfa0a2660702b1b7edcb868c75b87
Jb Lm <lemetayer.jb@gmail.com>
2016-09-05 15:44:29 +0200

    avenant a la convention ciel #17584

--- project/plugins/EmailPlugin/modules/Email/templates/_send_convention_ciel.php
+++ project/plugins/EmailPlugin/modules/Email/templates/_send_convention_ciel.php
@@ -2,7 +2,7 @@
 
 Bonjour,<br /><br />
 Vous venez d'adhérer en ligne au service d'échange de données CIEL-Declarvins.net et nous vous remercions.<br />
-Vous trouverez en pièce jointe en pdf la convention d'adhésion à imprimer et à retourner signée à votre service douane et votre interprofession.<br />
+Vous trouverez en pièce jointe en pdf la convention d'adhésion et l'avenant à votre contrat d'inscription à imprimer et à retourner signée à votre service douane et votre interprofession.<br />
 Pour toute question, n'hésitez pas à <a href="<?php echo ProjectConfiguration::getAppRouting()->generate('contact', array(), true); ?>">contacter votre interprofession</a><br /><br />
 Bonne journée<br /><br />
 L'équipe Declarvins.net
