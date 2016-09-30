5170e2586c0c0cd9d214a9340cbff62f427324c0
Jb Lm <lemetayer.jb@gmail.com>
2016-09-30 16:13:02 +0200

    layout prod

--- project/apps/declarvin/templates/_header.php
+++ project/apps/declarvin/templates/_header.php
@@ -12,7 +12,7 @@
     <div id="logo">
         <h1>
                 <a href="<?php echo url_for('ac_vin_login') ?>" title="Declarvin - Retour à l'accueil"><img src="/images/visuels/logo_declarvins.png" alt="Declarvin" /></a></h1>
-        <p style="font-weight: bold; text-align:center;">/!\ PREPROD V2 /!\</p>
+        <p>La plateforme déclarative des vins du Rhône, de Provence et du Sud Est</p>
     </div>
     <?php if ($sf_user->isAuthenticated()): ?>
     <p class="deconnexion"><a href="<?php echo url_for('@ac_vin_logout'); ?>">Se deconnecter</a></p>
