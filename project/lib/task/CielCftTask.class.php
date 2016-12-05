6a127fdea6d4b376dc72bad2a470772c5b7b58f4
Jb Lm <lemetayer.jb@gmail.com>
2016-12-05 16:09:11 +0100

    amelioration moulinette CFT

--- project/lib/task/CielCftTask.class.php
+++ project/lib/task/CielCftTask.class.php
@@ -2,6 +2,12 @@
 
 class CielCftTask extends sfBaseTask
 {
+	CONST RAPPORT_OK_KEY = 'OK';
+	CONST RAPPORT_DIFF_KEY = 'DIFF';
+	CONST RAPPORT_NONSAISIE_KEY = 'NONSAISIE';
+	CONST RAPPORT_PASS_KEY = 'PASS';
+	CONST RAPPORT_ERROR_KEY = 'ERROR';
+	
   protected function configure()
   {
 
@@ -13,7 +19,8 @@ class CielCftTask extends sfBaseTask
       new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name', 'declarvin'),
       new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'prod'),
       new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'default'),
-      new sfCommandOption('all', null, sfCommandOption::PARAMETER_REQUIRED, 'All time', 0)
+      new sfCommandOption('checking', null, sfCommandOption::PARAMETER_REQUIRED, 'Cheking mode', 0),
+      new sfCommandOption('interpro', null, sfCommandOption::PARAMETER_REQUIRED, 'Interprofession', ''),
     ));
 
     $this->namespace        = 'ciel';
@@ -23,6 +30,60 @@ class CielCftTask extends sfBaseTask
 
 EOF;
   }
+    
+  private function initRapport()
+  {
+  	$rapport = array();
+  	$rapport[self::RAPPORT_OK_KEY] = array();
+  	$rapport[self::RAPPORT_DIFF_KEY] = array();
+  	$rapport[self::RAPPORT_NONSAISIE_KEY] = array();
+  	$rapport[self::RAPPORT_PASS_KEY] = array();
+  	$rapport[self::RAPPORT_ERROR_KEY] = array();
+  	return $rapport;
+  }
+  
+  private function getTitle($key, $nb)
+  {
+  	$title = $nb;
+  	switch ($key) {
+  		case self::RAPPORT_OK_KEY:
+  			$title .= ($nb > 1)? ' DRM ont bien été réintégrées' : ' DRM a bien été réintégrée';
+  			break;
+  		case self::RAPPORT_DIFF_KEY:
+  			$title .= ($nb > 1)? ' DRM présentent des différences et nécessitent une rectification' : ' DRM présente des différences et nécessite une rectification';
+  			break;
+  		case self::RAPPORT_NONSAISIE_KEY:
+  			$title .= ($nb > 1)? ' DRM n\'ont pas été saisies sur la plateforme interprofessionnelle' : ' DRM n\'a pas été saisie sur la plateforme interprofessionnelle';
+  			break;
+  		case self::RAPPORT_PASS_KEY:
+  			$title .= ($nb > 1)? ' DRM déjà traitées' : ' DRM déjà traitée';
+  			break;
+  		case self::RAPPORT_ERROR_KEY:
+  			$title .= ($nb > 1)? ' Erreurs sont survenues' : ' Erreur est survenue';
+  			break;
+  		default:
+  			$title = '';
+  	}
+  	return $title;
+  }
+  
+  private function messagizeRapport($rapport)
+  {
+  	$message = '<h1>Monitoring du flux de DRM en provenance de Prodou@ne / CIEL</h1>';
+  	
+  	foreach ($rapport as $rapportKey => $rapportItem) {
+  		if (count($rapportItem) > 0 || $rapportKey != self::RAPPORT_PASS_KEY) {
+  			$message .= $this->getTitle($rapportKey, count($rapportItem));
+		  	$message .= '<ul>';
+		  	foreach ($rapportItem as $item) {
+		  		$message .= '<li>'.$item.'</li>';
+		  	}
+		  	$message .= '</ul>';
+  		}
+  	}
+  	
+  	return $message;
+  }
 
   protected function execute($arguments = array(), $options = array())
   {
@@ -30,11 +91,13 @@ EOF;
     $databaseManager = new sfDatabaseManager($this->configuration);
     $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
     
-
+    $checkingMode = $options['checking'];
+    $interpro = $options['interpro'];
     $contextInstance = sfContext::createInstance($this->configuration);
     
     $list = simplexml_load_file($arguments['target'], 'SimpleXMLElement', LIBXML_NOCDATA | LIBXML_NOBLANKS);
-    $rapport = array();
+    $rapport = $this->initRapport();
+    $files = array();
     if ($list !== FALSE) {
     	foreach ($list->children() as $item) {
     		$xmlIn = simplexml_load_file($item, 'SimpleXMLElement', LIBXML_NOCDATA | LIBXML_NOBLANKS);
@@ -49,51 +112,62 @@ EOF;
     						$xmlOut = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA | LIBXML_NOBLANKS);
     						$compare = new DRMCielCompare($xmlIn, $xmlOut);
     						if (!$compare->hasDiff()) {
-    							$drm->ciel->valide = 1;
-    							$drm->save();
-    							$rapport[] = 'OK // La DRM '.$drm->_id.' a été validée avec succès';
+    							if (!$checkingMode) {
+	    							$drm->ciel->valide = 1;
+	    							$drm->save();
+    							}
+    							$rapport[self::RAPPORT_OK_KEY][] = 'La DRM '.$drm->_id.' a été validée avec succès';
     						} else {
     							$exist = false;
     							if ($drm->isVersionnable()) {
-    								$drm_rectificative = $drm->generateRectificative(true);
-	    							$drm_rectificative->mode_de_saisie = DRMClient::MODE_DE_SAISIE_DTI;
-	    							$drm_rectificative->add('ciel', $drm->ciel);
-	    							$drm_rectificative->ciel->xml = null;
-	    							$drm_rectificative->ciel->diff = $xmlIn->asXML();
-	    							$drm_rectificative->save();
+    								if (!$checkingMode) {
+	    								$drm_rectificative = $drm->generateRectificative(true);
+		    							$drm_rectificative->mode_de_saisie = DRMClient::MODE_DE_SAISIE_DTI;
+		    							$drm_rectificative->add('ciel', $drm->ciel);
+		    							$drm_rectificative->ciel->xml = null;
+		    							$drm_rectificative->ciel->diff = $xmlIn->asXML();
+		    							$drm_rectificative->save();
+    								}
 	    							$diffs = '<ul>';
 	    							foreach ($compare->getDiff()as $k => $v) {
 	    								$diffs .= "<li>$k : $v</li>";
 	    							}
 	    							$diffs .= '</ul>';
-	    							$rapport[] = 'ATT // La DRM '.$drm->_id.' doit être rectifiée suite aux modifications suivantes : '.$diffs;
+	    							$rapport[self::RAPPORT_DIFF_KEY][] = 'La DRM '.$drm->_id.' ('.$ea.') doit être rectifiée suite aux modifications suivantes : '.$diffs;
+	    							$files[] = $item;
     							} else {
-    								$rapport[] = 'La DRM '.$drm->_id.' à déjà été traitée';
+    								$rapport[self::RAPPORT_PASS_KEY][] = 'La DRM '.$drm->_id.' à déjà été traitée';
     							}
     						}
     					} else {
-    						$rapport[] = 'Oups // Impossible de générer le XML de La DRM '.$drm->_id;
+    						$rapport[self::RAPPORT_ERROR_KEY][] = 'Impossible de générer le XML de La DRM '.$drm->_id;
     					}
     					
     				} else {
-    					$rapport[] = 'La DRM '.$drm->_id.' à déjà été traitée';
+    					$rapport[self::RAPPORT_PASS_KEY][] = 'La DRM '.$drm->_id.' à déjà été traitée';
     				}
     			} else {
-    				$rapport[] = 'Oups // La DRM '.$periode.' de l\'établissement '.$ea.' n\'a pas été saisie sur le portail interprofessionnel: '.$item;
+    				$rapport[self::RAPPORT_NONSAISIE_KEY][] = 'La DRM '.$periode.' de l\'établissement '.$ea.' n\'a pas été saisie sur le portail interprofessionnel';
+    				$files[] = $item;
     			}
     		} else {
-    			$rapport[] = 'Oups // Impossible d\'interroger la DRM : '.$item;
+    			$rapport[self::RAPPORT_ERROR_KEY][] = 'Impossible d\'interroger la DRM : '.$item;
     		}
     	}
     } else {
-		$rapport[] = 'Oups // Impossible d\'interroger le service : '.$arguments['target'];
+		$rapport[self::RAPPORT_ERROR_KEY][] = 'Impossible d\'interroger le service : '.$arguments['target'];
     }
-    $s = '<ul>';
-    foreach ($rapport as $item) {
-    	$s .= '<li>'.$item.'</li>';
+    $s = $this->messagizeRapport($rapport);
+    if ($checkingMode) {
+    	echo str_replace("<li>", "\t", str_replace(array("<ul>", "</ul>", "</li>"), "\n", $s));
+    } else {
+	    $message = $this->getMailer()->compose(sfConfig::get('app_email_from_notification'), sfConfig::get('app_email_to_notification'), "DeclarVins // Rapport CFT ".$interpro, $s)->setContentType('text/html');
+	    if (count($files) > 0) {
+	    	foreach ($files as $file) {
+	    		$message->attach(Swift_Attachment::fromPath($file));
+	    	}
+	    }
+	    $this->getMailer()->sendNextImmediately()->send($message);
     }
-    $s .= '</ul>';
-    $message = $message = $this->getMailer()->compose(sfConfig::get('app_email_from_notification'), sfConfig::get('app_email_to_notification'), "DeclarVins // Rapport CFT", $s)->setContentType('text/html');
-    $this->getMailer()->sendNextImmediately()->send($message);
   }
 }
