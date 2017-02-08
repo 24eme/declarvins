a756cd5529d6a1d00162c80f4c9f3f9324b71a73
Jb Lm <lemetayer.jb@gmail.com>
2017-02-08 15:28:41 +0100

    specification text dans le libelle du xml

--- project/plugins/acVinEdiPlugin/modules/edi_export/templates/_xml.php
+++ project/plugins/acVinEdiPlugin/modules/edi_export/templates/_xml.php
@@ -25,7 +25,7 @@
 <?php if ($produit->getInao()): ?>
 				<code-inao><?php echo $produit->getInao() ?></code-inao>
 <?php endif; ?>
-				<libelle-personnalise><?php echo trim(html_entity_decode($produit->getLibelle(), ENT_QUOTES | ENT_HTML401)) ?><?php if($produit->hasLabel()): ?> <?php echo $produit->getLabelKeyString(); ?><?php endif; ?></libelle-personnalise>
+				<libelle-personnalise><![CDATA[<?php echo trim($produit->getLibelle()) ?><?php if($produit->hasLabel()): ?> <?php echo $produit->getLabelKeyString(); ?><?php endif; ?>]]></libelle-personnalise>
 <?php if ($produit->getTav()): ?>
 				<tav><?php echo sprintf("%01.02f", $produit->getTav()) ?></tav>
 <?php endif; ?>
