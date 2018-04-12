6b5f4d3e3f9f15a3c1abfe49144ebfae0f3c4543
Jb Lm <lemetayer.jb@gmail.com>
2018-04-12 15:32:39 +0200

    suppression delai paiement contrat civp ivse

--- project/plugins/VracPlugin/lib/form/VracConditionIvseForm.class.php
+++ project/plugins/VracPlugin/lib/form/VracConditionIvseForm.class.php
@@ -7,7 +7,7 @@ class VracConditionIvseForm extends VracConditionForm
 		$this->getValidator('conditions_paiement')->setOption('required', false);
 		$this->getWidget('reference_contrat_pluriannuel')->setLabel('Si vous avez un contrat pluriannuel écrit, veuillez noter sa référence:');
 		$this->setWidget('vin_livre', new sfWidgetFormInputHidden());
-        unset($this['clause_reserve_retiraison']);
+        unset($this['clause_reserve_retiraison'], $this['delai_paiement']);
     }
 
 
