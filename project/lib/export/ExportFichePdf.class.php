8cfc3eae932080bca4c4e0e2ec8c489752de0158
Jb Lm <lemetayer.jb@gmail.com>
2018-06-08 12:34:02 +0200

    gestion cache pdf profil

--- project/lib/export/ExportFichePdf.class.php
+++ project/lib/export/ExportFichePdf.class.php
@@ -15,10 +15,21 @@ class ExportFichePdf
 		return sfContext::getInstance()->getController()->getAction('export', 'main')->getPartial('export/' . $partial, $vars);
 	}
 	
+	public function getCountRevs()
+	{
+		$rev = explode('-', $this->compte->_rev);
+		$revs = $rev[0];
+		foreach ($this->compte->getTiersCollection() as $i => $etablissement) {
+			$rev = explode('-', $etablissement->_rev);
+			$revs += $rev[0];
+		}
+		return $revs;
+	}
+	
 	public function render($response, $debug = false)
 	{
 		$targetClass = ($debug)? 'PrintableHTML' : 'PrintablePDF';
-		$document = new $targetClass('FICHE_'.$this->contrat->no_contrat.'.pdf');
+		$document = new $targetClass('FICHE_'.$this->contrat->no_contrat.'_'.$this->getCountRevs().'.pdf');
 		$document->setPaper(PrintableOutput::FORMAT_A4, PrintableOutput::ORIENTATION_PORTRAIT);
 		$document->addHtml($this->getPartial('fiche_pdf', array('contrat' => $this->contrat, 'compte' => $this->compte)));
         $output = $document->output();
@@ -29,7 +40,7 @@ class ExportFichePdf
 	public function generate($debug = false)
 	{
 		$targetClass = ($debug)? 'PrintableHTML' : 'PrintablePDF';
-		$document = new $targetClass('FICHE_'.$this->contrat->no_contrat.'.pdf');
+		$document = new $targetClass('FICHE_'.$this->contrat->no_contrat.'_'.$this->getCountRevs().'.pdf');
 		$document->removeCache();
 		$document->setPaper(PrintableOutput::FORMAT_A4, PrintableOutput::ORIENTATION_PORTRAIT);
 		$document->addHtml($this->getPartial('fiche_pdf', array('contrat' => $this->contrat, 'compte' => $this->compte)));
