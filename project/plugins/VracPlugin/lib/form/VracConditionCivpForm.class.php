f87b12f79edc11b0b0f290fd3ad30b6a0a201a22
Jb Lm <lemetayer.jb@gmail.com>
2018-06-13 15:09:38 +0200

    reactivation delai paiement vrac civp

--- project/plugins/VracPlugin/lib/form/VracConditionCivpForm.class.php
+++ project/plugins/VracPlugin/lib/form/VracConditionCivpForm.class.php
@@ -6,7 +6,6 @@ class VracConditionCivpForm extends VracConditionForm
 		$this->getWidget('conditions_paiement')->setLabel('Conditions de vente:');
 		$this->getValidator('conditions_paiement')->setOption('required', false);
 		$this->getWidget('reference_contrat_pluriannuel')->setLabel('Si vous avez un contrat pluriannuel écrit, veuillez noter sa référence:');
-        unset($this['delai_paiement']);
     }
 
 
