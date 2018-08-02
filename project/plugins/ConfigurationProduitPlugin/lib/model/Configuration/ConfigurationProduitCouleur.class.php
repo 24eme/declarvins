249b5a21379a1b095515c42f5cd56e4be7691071
Mathurin Petit <mathurin.petit@gmail.com>
2018-08-02 19:46:08 +0200

    mis a jour ded la fct getCOuleur

--- project/plugins/ConfigurationProduitPlugin/lib/model/Configuration/ConfigurationProduitCouleur.class.php
+++ project/plugins/ConfigurationProduitPlugin/lib/model/Configuration/ConfigurationProduitCouleur.class.php
@@ -1,73 +1,77 @@
 <?php
-class ConfigurationProduitCouleur extends BaseConfigurationProduitCouleur 
+class ConfigurationProduitCouleur extends BaseConfigurationProduitCouleur
 {
 	const TYPE_NOEUD = 'couleur';
 	const CODE_APPLICATIF_NOEUD = 'CO';
-	
-	public function getChildrenNode() 
+
+	public function getChildrenNode()
 	{
       return $this->cepages;
     }
 
-    public function getCertification() 
+    public function getCertification()
     {
         return $this->getAppellation()->getCertification();
     }
 
-    public function getGenre() 
+    public function getGenre()
     {
         return $this->getAppellation()->getGenre();
     }
-  	
-	public function getAppellation() 
+
+	public function getAppellation()
 	{
         return $this->getCouleur()->getLieu()->getAppellation();
     }
 
-    public function getMention() 
+    public function getMention()
     {
         return $this->getLieu()->getMention();
     }
 
-    public function getLieu() 
+    public function getLieu()
     {
     	return $this->getParentNode();
     }
-    
-	public function hasCepage() 
+	public function getCouleur()
+    {
+    	return $this;
+    }
+
+	public function hasCepage()
 	{
     	return (count($this->cepages) > 1 || (count($this->cepages) == 1 && $this->cepages->getFirst()->getKey() != ConfigurationProduit::DEFAULT_KEY));
     }
-	
-	public function getTotalLieux() 
+
+	public function getTotalLieux()
 	{
 		return array();
 	}
 
     public function getTotalCouleurs($onlyForDrmVrac = false, $cvoNeg = false, $date = null, $exception = null)
     {
-    
+
     	if ($onlyForDrmVrac) {
     		if (!$this->getCurrentDrmVrac(true)) {
     			return array();
     		}
     	}
-    	
+
     	if ($exception) {
     		//echo '/'.str_replace('/', '\/', $exception).'/';exit;
     		if (preg_match('/'.str_replace('/', '\/', $exception).'/', $this->getHash())) {
     			return array();
     		}
     	}
-    
+
     	if($cvoNeg){
     		return array($this->getHash() => $this);
     	}
-    
+
     	return $this->getProduitWithTaux($date);
-    
+
     }
-    
+
     protected function getProduitWithTaux($date = null) {
         $date_cvo = (!$date)? date('Y-m-d') : $date;
         $droit = $this->getCurrentDroit(ConfigurationProduit::NOEUD_DROIT_CVO, $date_cvo, true);
@@ -76,34 +80,34 @@ class ConfigurationProduitCouleur extends BaseConfigurationProduitCouleur
         }
         return array();
     }
-    
+
 	/*
      * Les fonctions ci-dessous sont relatives à la gestion de la configuration du catalogue produit
      */
-    
+
   	public function hasLabels() { return false; }
-    
+
   	public function hasDepartements() { return false; }
-  	
+
 	public function hasPrestations() { return false; }
-  	
+
   	public function hasCvo() { return true; }
-  	
+
   	public function hasDouane() { return false; }
-  	
+
   	public function hasDRMVrac() { return false; }
-  	
+
   	public function hasCiel() { return false; }
-  	  	
+
   	public function hasOIOC() { return false; }
-  	
+
   	public function hasDefinitionDrm() { return false; }
-  	
+
   	public function getTypeNoeud() { return self::TYPE_NOEUD; }
-  	
+
   	public function getCodeApplicatif() { return self::CODE_APPLICATIF_NOEUD; }
-  	
+
   	public function getCsvLibelle() { return ConfigurationProduitCsvFile::CSV_PRODUIT_COULEUR_LIBELLE; }
-  	
+
   	public function getCsvCode() { return ConfigurationProduitCsvFile::CSV_PRODUIT_COULEUR_CODE; }
 }
