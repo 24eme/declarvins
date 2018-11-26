4fa422671d894f07ad018890f7c1522872516483
Jb Lm <lemetayer.jb@gmail.com>
2018-11-26 17:57:44 +0100

    correctif envoie fichier erreur dae

--- project/apps/declarvin/modules/dae/actions/actions.class.php
+++ project/apps/declarvin/modules/dae/actions/actions.class.php
@@ -1,12 +1,7 @@
 <?php
 class daeActions extends acVinDAEActions {
 	
-    public function fileErrorUploadEdi($file, $files, $etablissement, $periode) {
-    	if (is_array($files) && count($files) > 0) {
-	    	if ($resultat = move_uploaded_file($files['tmp_name'], '/tmp/'.strtolower($files['name']))) {
-	    		$file = '/tmp/'.strtolower($files['name']);
-	    	}
-    	}
+    public function fileErrorUploadEdi($file, $etablissement, $periode) {
     	if ($file) {
     		Email::getInstance()->daeErrorFileSend($file, $etablissement);
     		$this->getUser()->setFlash("notice", 'Votre fichier a bien été importé. Il sera traité dans les plus bref délais.');
