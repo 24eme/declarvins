1b161d09a7fe4b3d01283c7ec670c8726c505c85
Jb Lm <lemetayer.jb@gmail.com>
2019-07-04 11:57:38 +0200

    finalisation script stats douanes

--- project/bin/douanestats.sh
+++ project/bin/douanestats.sh
@@ -3,13 +3,23 @@
 
 INTERPRO=$1
 
+if [[ -f /tmp/bilans.json ]]; then
+	rm -f /tmp/bilans.json
+fi
+if [[ -f /tmp/grc.csv ]]; then
+	rm -f /tmp/grc.csv
+fi
+if [[ -f /tmp/drms.xml ]]; then
+	rm -f /tmp/drms.xml
+fi
+
 wget -q -O /tmp/bilans.json "$STATSCIEL_BILANDRM"
 
-if [ $INTERPRO = "IR" ]; then
+if [[ $INTERPRO = "IR" ]]; then
 	wget -q -O /tmp/grc.csv "$STATSCIEL_GRC_IR"
 	wget -q -O /tmp/drms.xml "$STATSCIEL_XML_IR"
 fi
-if [ $INTERPRO = "CIVP" ]; then
+if [[ $INTERPRO = "CIVP" ]]; then
 	wget -q -O /tmp/grc.csv "$STATSCIEL_GRC_CIVP"
 	wget -q -O /tmp/drms.xml "$STATSCIEL_XML_CIVP"
 fi
@@ -17,6 +27,9 @@ fi
 echo "IDENTIFIANT;INTERPRO_REF;ZONES;FAMILLE;SOUS_FAMILLE;NUM_ACCISES;RAISON_SOCIALE;CODE_POSTAL;SERVICE_DOUANE;EST_CONVENTIONNE_DV;NUM_CONVENTION_DV;TELEDECL_DRM_DV;EST_CONVENTIONNE_CIEL;TRANSMET_DRM_DV_CIEL;TELEDECL_DRM_CIEL"
 
 cat /tmp/grc.csv | while IFS='' read -r line; do
+	if [[ $line =~ ^\# ]]; then
+		continue
+	fi
 	ID=$(echo "$line" | cut -d ';' -f 1)
 	EA=$(echo "$line" | cut -d ';' -f 8)
 	INFOS=$(echo "$line" | awk -F ";" '{print $1 ";" $4 ";" $36 ";" $19 ";" $20 ";" $8 ";" $13 ";" $17 ";" $25 }')
@@ -37,19 +50,19 @@ cat /tmp/grc.csv | while IFS='' read -r line; do
 	fi
 
 	
-	if [ $CONV > 0 ]; then
+	if [[ $CONV > 0 ]]; then
 		HAS_CONV=1
 	fi
-	if [ $CONV_CIEL = "oui" ]; then
+	if [[ $CONV_CIEL = "oui" ]]; then
 		HAS_CONV_CIEL=1
 	fi
-	if [ $NB_BILAN > 0 ]; then
+	if [[ $NB_BILAN > 0 ]]; then
 		HAS_DRM=1
 	fi
-	if [ $NB_DV_CIEL > 0 ]; then
+	if [[ $NB_DV_CIEL > 0 ]]; then
 		HAS_DRM_DVCIEL=1
 	fi
-	if [ $NB_DRM_CIEL > 0 ]; then
+	if [[ $NB_DRM_CIEL > 0 ]]; then
 		HAS_DRM_CIEL=1
 	fi
 
