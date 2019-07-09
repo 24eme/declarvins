d55d01c79a292fca780069871543c2d4fd071a20
Jb Lm <lemetayer.jb@gmail.com>
2019-07-09 17:32:46 +0200

    ajout du statut et opti drm demat

--- project/bin/douanestats.sh
+++ project/bin/douanestats.sh
@@ -24,7 +24,7 @@ if [[ $INTERPRO = "CIVP" ]]; then
 	wget -q -O /tmp/drms.xml "$STATSCIEL_XML_CIVP"
 fi
 
-echo "IDENTIFIANT;INTERPRO_REF;ZONES;FAMILLE;SOUS_FAMILLE;NUM_ACCISES;RAISON_SOCIALE;CODE_POSTAL;SERVICE_DOUANE;EST_CONVENTIONNE_DV;NUM_CONVENTION_DV;TELEDECL_DRM_DV;EST_CONVENTIONNE_CIEL;TRANSMET_DRM_DV_CIEL;TELEDECL_DRM_CIEL"
+echo "IDENTIFIANT;STATUT;INTERPRO_REF;ZONES;FAMILLE;SOUS_FAMILLE;NUM_ACCISES;RAISON_SOCIALE;CODE_POSTAL;SERVICE_DOUANE;EST_CONVENTIONNE_DV;NUM_CONVENTION_DV;TELEDECL_DRM_DV;EST_CONVENTIONNE_CIEL;TRANSMET_DRM_DV_CIEL;TELEDECL_DRM_CIEL"
 
 cat /tmp/grc.csv | while IFS='' read -r line; do
 	if [[ $line =~ ^\# ]]; then
@@ -32,7 +32,7 @@ cat /tmp/grc.csv | while IFS='' read -r line; do
 	fi
 	ID=$(echo "$line" | cut -d ';' -f 1)
 	EA=$(echo "$line" | cut -d ';' -f 8)
-	INFOS=$(echo "$line" | awk -F ";" '{print $1 ";" $4 ";" $36 ";" $19 ";" $20 ";" $8 ";" $13 ";" $17 ";" $25 }')
+	INFOS=$(echo "$line" | awk -F ";" '{print $1 ";" $27 ";" $4 ";" $36 ";" $19 ";" $20 ";" $8 ";" $13 ";" $17 ";" $25 }')
 
 	HAS_CONV=0
 	HAS_CONV_CIEL=0
@@ -42,7 +42,7 @@ cat /tmp/grc.csv | while IFS='' read -r line; do
 
 	CONV=$(echo "$line" | cut -d ';' -f 3)
 	CONV_CIEL=$(echo "$line" | cut -d ';' -f 38)
-	NB_BILAN=$(cat /tmp/bilans.json | grep "BILAN-DRM-$ID" | grep -E "DRM_STATUS_BILAN_VALIDE|CIEL|MANQUANT" | wc -l)
+	NB_BILAN=$(cat /tmp/bilans.json | grep "BILAN-DRM-$ID" | grep -E "DTI|EDI" | grep -E "DRM_STATUS_BILAN_VALIDE|CIEL|MANQUANT" | wc -l)
 	NB_DV_CIEL=$(cat /tmp/bilans.json | grep "BILAN-DRM-$ID" | grep "CIEL" | wc -l)
 	NB_DRM_CIEL=0
 	if [ ! -z "$EA" ]; then
