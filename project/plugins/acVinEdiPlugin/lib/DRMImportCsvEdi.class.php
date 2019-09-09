836edd111c497e21bd3a40a9762a4f4f7d1c914c
Jb Lm <lemetayer.jb@gmail.com>
2019-09-09 15:15:57 +0200

    import drm correction 5 decimal

--- project/plugins/acVinEdiPlugin/lib/DRMImportCsvEdi.class.php
+++ project/plugins/acVinEdiPlugin/lib/DRMImportCsvEdi.class.php
@@ -269,7 +269,7 @@ class DRMImportCsvEdi extends DRMCsvEdi {
 	  		
 	  		$mvt = ($categorieMvt)? $produit->getOrAdd($categorieMvt) : $produit;
 	  		$old = (in_array(str_replace('acq_', '', $typeMvt), array('total_debut_mois', 'total', 'tav')))? 0 : floatval($mvt->getOrAdd($typeMvt));
-	  		$mvt->add($typeMvt, round(($old + $this->floatize($valeur)), 4));
+	  		$mvt->add($typeMvt, round(($old + $this->floatize($valeur)), 5));
 	  		$result = $this->drm->setImportableMvtDetails($typeMvt, $mvt, $datas);
 	  		if (!$result) {
 	  			$this->csvDoc->addErreur($this->mvtDetailsNotValidError($numLigne, $datas));
