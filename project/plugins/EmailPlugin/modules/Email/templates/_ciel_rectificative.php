d53afc5fa6a80edbf5b243bacef8a0f57c225443
Vincent LAURENT <vince.laurent@gmail.com>
2019-09-20 10:46:47 +0200

    Correction d'une faute d'ortographe

--- project/plugins/EmailPlugin/modules/Email/templates/_ciel_rectificative.php
+++ project/plugins/EmailPlugin/modules/Email/templates/_ciel_rectificative.php
@@ -6,7 +6,7 @@
 Entreprise :  <?php if($etablissement->nom) { echo $etablissement->nom; } if($etablissement->raison_sociale) { echo ($etablissement->nom)? ' / '.$etablissement->raison_sociale : $etablissement->raison_sociale; } echo ($etablissement->famille)? ' - '.ucfirst($etablissement->famille) : ''; ?><?php if ($etablissement->telephone) {echo ' '.$etablissement->telephone;} if ($etablissement->fax) {echo ' '.$etablissement->fax;} if ($etablissement->email) {echo ' '.$etablissement->email;} ?><br /><br />
 Madame, Monsieur,<br /><br />
 Votre DRM du <strong><?php echo $drm->getMois() ?>/<?php echo $drm->getAnnee() ?></strong> n° <strong><?php echo $drmCiel->identifiant_declaration ?></strong> a bien été transmise par declarvins.net au téléservice CIEL des douanes le <strong><?php echo format_date($drmCiel->horodatage_depot, 'dd/MM/yyyy') ?></strong> à <strong><?php echo format_date($drmCiel->horodatage_depot, 'H:m') ?></strong>.<br /><br />
-CIEL, Plateforme de la Douane, nous indique que vous avez apporté des moficiations à votre DRM avant de la valider.<br />
+CIEL, Plateforme de la Douane, nous indique que vous avez apporté des modifications à votre DRM avant de la valider.<br />
 Vous trouverez ci-dessous les différences constatées :<br />
 <ul>
 <?php foreach ($diffs as $k => $v): ?>
