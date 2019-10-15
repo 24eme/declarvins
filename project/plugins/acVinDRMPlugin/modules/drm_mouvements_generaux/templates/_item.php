e61f7d74923a3ca2a6692646ea7dfa1e7855cda7
Vincent LAURENT <vince.laurent@gmail.com>
2019-10-15 11:39:45 +0200

    On affiche le petit crayon pour modifier les libellés tous le temps

--- project/plugins/acVinDRMPlugin/modules/drm_mouvements_generaux/templates/_item.php
+++ project/plugins/acVinDRMPlugin/modules/drm_mouvements_generaux/templates/_item.php
@@ -6,12 +6,7 @@
     	<a href="<?php echo url_for('drm_mouvements_generaux_produit_delete', $detail) ?>" class="supprimer">Supprimer</a>
     	<?php endif; ?>
     	<?php echo $detail->getFormattedLibelle(ESC_RAW); ?>
-    	<?php
-    	$drm = $detail->getDocument();
-		if ($drm->getEtablissement()->isTransmissionCiel()): 
-		?>
     	<a href="<?php echo url_for('drm_mouvements_generaux_product_edit', $detail) ?>" class="btn_popup" data-popup="#popup_edit_produit_<?php echo $detail->getIdentifiantHTML() ?>" data-popup-config="configFormEdit"><img src="/images/pictos/pi_edit.png" alt="edit" /></a>
-    	<?php endif; ?>
     </td>
 	<td>
         <?php echo echoLongFloat($detail->total_debut_mois) ?> <span class="unite"><?php echoHl($detail) ?> </span>
