d890d3036e80e2b1b1c3fe8fe28dae101ff3fbcc
Tangui Morlier <tangui@tangui.eu.org>
2019-10-24 17:41:51 +0200

    5 décimales dans le PHP

--- project/plugins/acVinDRMPlugin/lib/form/DRMDetailStocksFinForm.class.php
+++ project/plugins/acVinDRMPlugin/lib/form/DRMDetailStocksFinForm.class.php
@@ -7,9 +7,9 @@ class DRMDetailStocksFinForm  extends acCouchdbObjectForm {
     	$contraintes = Configuration::getContraintes($this->getObject()->getParent()->getGenre()->code);
     	foreach (Configuration::getStocksFin($this->getOption('acquittes', false)) as $key => $item) {
     		if ($contraintes && !in_array('stocks_fin/'.$key, $contraintes)) {
-    			$this->setWidget($key, new sfWidgetFormInputFloat(array('float_format' => "%01.04f"), array('readonly' => 'readonly')));
+    			$this->setWidget($key, new sfWidgetFormInputFloat(array('float_format' => "%01.05f"), array('readonly' => 'readonly')));
     		} else {
-    			$this->setWidget($key, new sfWidgetFormInputFloat(array('float_format' => "%01.04f")));
+    			$this->setWidget($key, new sfWidgetFormInputFloat(array('float_format' => "%01.05f")));
     		}
     		$this->setValidator($key, new sfValidatorNumber(array('required' => false)));
     	}
