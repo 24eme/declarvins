f54de20ece85582328dcb26c2255334fce35e050
Gabriel Poma <gpoma@24eme.fr>
2019-10-24 16:01:07 +0200

    On enlève volume de l'échéancier

--- project/plugins/acVinVracPlugin/lib/form/base/VracPaiementForm.class.php
+++ project/plugins/acVinVracPlugin/lib/form/base/VracPaiementForm.class.php
@@ -9,17 +9,14 @@ class VracPaiementForm extends acCouchdbObjectForm
 	{
 		$this->setWidgets(array(
 	       'date' => new sfWidgetFormInputText(),
-		   'volume' => new sfWidgetFormInputFloat(),
 		   'montant' => new sfWidgetFormInputFloat()
 		));
 		$this->widgetSchema->setLabels(array(
 	       'date' => 'Date du paiement :',
-	       'volume' => 'Volume :',
 	       'montant' => 'Montant :'
 		));
 		$this->setValidators(array(
 	       'date' => new sfValidatorDate(array('date_output' => 'Y-m-d', 'date_format' => '~(?P<day>\d{2})/(?P<month>\d{2})/(?P<year>\d{4})~', 'required' => false), array('invalid' => 'Format valide : dd/mm/aaaa')),
-	       'volume' => new sfValidatorNumber(array('required' => false)),
 	       'montant' => new sfValidatorNumber(array('required' => false))
 		));
 		$this->widgetSchema->setNameFormat('paiement[%s]');
