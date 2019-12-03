cb587d1d72cc4751439255639994024468f93ca9
Jb Lm <lemetayer.jb@gmail.com>
2019-12-03 14:55:44 +0100

    obligation de choisir bio ou conv contrat de vente

--- project/plugins/acVinVracPlugin/lib/validator/VracProduitValidator.class.php
+++ project/plugins/acVinVracPlugin/lib/validator/VracProduitValidator.class.php
@@ -16,6 +16,7 @@ class VracProduitValidator extends sfValidatorBase {
             if (
                 (in_array('conv', $values['labels_arr']) && in_array('biol', $values['labels_arr'])) ||
                 (in_array('biol', $values['labels_arr']) && in_array('bioc', $values['labels_arr'])) ||
+                (!in_array('biol', $values['labels_arr']) && !in_array('conv', $values['labels_arr'])) ||
                 (in_array('conv', $values['labels_arr']) && in_array('biol', $values['labels_arr']) && in_array('bioc', $values['labels_arr']))
                 ) {
                     $errorSchema->addError(new sfValidatorError($this, 'labels_incoherent'), 'labels_arr');
