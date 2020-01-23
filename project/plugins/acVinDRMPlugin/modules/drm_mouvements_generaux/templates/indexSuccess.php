748f9500803fd0c5022546dbe9ac26bdbe38c5ce
Jb Lm <lemetayer.jb@gmail.com>
2020-01-23 15:54:51 +0100

    Revert "suppression rectificative par les operateurs"
    This reverts commit 2680e6b68ccf4b8b2e22a1ab24638ac077a1e575.

--- project/plugins/acVinDRMPlugin/modules/drm_mouvements_generaux/templates/indexSuccess.php
+++ project/plugins/acVinDRMPlugin/modules/drm_mouvements_generaux/templates/indexSuccess.php
@@ -109,7 +109,7 @@
 		                <?php endif; ?>
 			        </div>
 
-					<?php if($drm->isRectificative() && $drm->exist('ciel') && $drm->ciel->transfere && !$sf_user->hasCredential(myUser::CREDENTIAL_OPERATEUR)): ?>
+					<?php if($drm->isRectificative() && $drm->exist('ciel') && $drm->ciel->transfere): ?>
 					<?php else: ?>
 			        <div class="ligne_btn">
            				<a href="<?php echo url_for('drm_delete_one', $drm) ?>" class="annuler_saisie btn_remise"><span>supprimer la drm</span></a>
