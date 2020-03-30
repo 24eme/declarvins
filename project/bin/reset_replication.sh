46c4c741056d2fd19d60eda58d6a169a2807c39f
Vincent LAURENT <vince.laurent@gmail.com>
2020-03-31 00:33:36 +0200

    reset replication avec les urls complete

--- project/bin/reset_replication.sh
+++ project/bin/reset_replication.sh
@@ -9,4 +9,4 @@ fi
 
 rev=$(curl -s http://$COUCHHOST:$COUCHPORT/_replicator/$REPLICATIONDOC | sed 's/.*rev":"//' | sed 's/".*//' )
 curl -s -X DELETE http://$COUCHHOST:$COUCHPORT/_replicator/$REPLICATIONDOC?rev=$rev  > /dev/null
-curl -s -X PUT -d '{"_id":"$REPLICATIONDOC","target":"'$COUCHBASE'","source":"'http://$COUCHDISTANTHOST:$COUCHPORT/$COUCHBASE'","continuous":true,"user_ctx": {"roles": ["_admin"]}}' http://$COUCHHOST:$COUCHPORT/_replicator/$REPLICATIONDOC  > /dev/null
+curl -s -X PUT -d '{"_id":"$REPLICATIONDOC","target":"'http://$COUCHHOST:$COUCHPORT/$COUCHBASE'","source":"'http://$COUCHDISTANTHOST:$COUCHPORT/$COUCHBASE'","continuous":true,"user_ctx": {"roles": ["_admin"]}}' http://$COUCHHOST:$COUCHPORT/_replicator/$REPLICATIONDOC  > /dev/null
