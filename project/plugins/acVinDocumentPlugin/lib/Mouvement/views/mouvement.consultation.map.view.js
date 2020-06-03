31cfa1b1b12ef225f9266b915c8b07f50c0e7903
Jb Lm <lemetayer.jb@gmail.com>
2020-06-03 12:32:25 +0200

    mise a jour vue mvts drm

--- project/plugins/acVinDocumentPlugin/lib/Mouvement/views/mouvement.consultation.map.view.js
+++ project/plugins/acVinDocumentPlugin/lib/Mouvement/views/mouvement.consultation.map.view.js
@@ -1,22 +1,38 @@
 function(doc) {
-    if (doc.type != "DRM" && doc.type != 'SV12') {
-
-        return;
-    }
-
-    if (!doc.valide.date_saisie) {
-
-        return;
-    }
-
-    for(identifiant in doc.mouvements) {
-        for (key in doc.mouvements[identifiant]) {
-            var mouv = doc.mouvements[identifiant][key];
-            pays = "";
-	    if (mouv.type_hash == "export_details") {
-		pays = mouv.detail_libelle;
-	    }
-            emit([doc.type, identifiant, doc.campagne, doc.periode, doc._id, mouv.produit_hash, mouv.type_hash, mouv.vrac_numero, mouv.detail_identifiant], [doc.declarant.nom, mouv.produit_libelle, mouv.type_libelle, mouv.volume, mouv.vrac_destinataire, mouv.detail_libelle, mouv.date_version, mouv.version, mouv.cvo, mouv.facturable, doc._id+'/mouvements/'+identifiant+'/'+key, pays]);
-        }
-    }
-}
+     if (doc.type != "DRM" && doc.type != 'SV12') {
+ 
+         return;
+     }
+ 
+     if (!doc.valide.date_saisie) {
+ 
+         return;
+     }
+ 
+     for(identifiant in doc.mouvements) {
+         if(identifiant == doc.identifiant) {
+             for (key in doc.mouvements[identifiant]) {
+                 var mouv = doc.mouvements[identifiant][key];
+                 pays = "";
+                 if (mouv.type_hash.match(/^export.*_details$/)) {
+                      pays = mouv.detail_libelle;
+                 }
+                 var coefficient_facturation = -1;
+                 if(mouv.coefficient_facturation) {
+                     coefficient_facturation = mouv.coefficient_facturation;
+                 }
+                 familledrm = "PRODUCTEUR"
+                 if (doc.declarant.famille) {
+                     familledrm = doc.declarant.famille;
+                 }
+				 type_drm = "SUSPENDU";
+                 type_drm_libelle = "Suspendu";
+                 if (mouv.type_hash.match(/acq_/)) {
+				 	type_drm = "ACQUITTE";
+                 	type_drm_libelle = "Acquitte";
+                 }
+                 emit([doc.type, identifiant, doc.campagne, doc.periode, doc._id, mouv.produit_hash, type_drm, mouv.type_hash, mouv.vrac_numero, mouv.detail_identifiant], [doc.declarant.nom, mouv.produit_libelle, type_drm_libelle, mouv.type_libelle, mouv.volume, mouv.vrac_destinataire, mouv.detail_libelle, mouv.date_version, mouv.version, mouv.cvo, mouv.facturable, doc._id+'/mouvements/'+identifiant+'/'+key, pays, mouv.facture, coefficient_facturation, mouv.date, familledrm, mouv.interpro]);
+             }
+         }
+     }
+ }
