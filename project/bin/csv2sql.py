a6ebf047ccdb7ea576a6290e669bdaf9b520fa1b
Jb Lm <lemetayer.jb@gmail.com>
2020-06-29 15:36:51 +0200

    export bi complet

--- project/bin/csv2sql.py
+++ project/bin/csv2sql.py
@@ -10,3 +10,19 @@ csv = pd.read_csv("export_bi_contrats.csv", encoding='iso-8859-1', delimiter=";"
        'contrat interne (OUI, NON)': 'contrat interne', 'original (OUI, NON)' : 'original',
        'type de contrat(SPOT, PLURIANNUEL)' : "type de contrat",'type de produit (GENERIQUE, DOMAINE)': 'type de produit', 'nature de la cvo (MARCHE_DEFINITIF, COMPENSATION, NON_FINANCIERE, VINAIGRERIE)': 'nature de la cvo'})
 csv.to_sql('contrat', con=engine, if_exists='replace')
+
+sys.stderr.write("export_bi_drm.csv\n")
+csv = pd.read_csv("export_bi_drm.csv", encoding='iso-8859-1', delimiter=";", index_col=False).rename(columns={"#DRM ID": "DRM ID", 'numéro archivage': 'numero archivage'})
+csv.to_sql('drm', con=engine, if_exists='replace')
+
+sys.stderr.write("export_bi_mouvements.csv\n")
+csv = pd.read_csv("export_bi_mouvements.csv", encoding='iso-8859-1', delimiter=";", index_col=False).rename(columns={'pays export (si export)': 'pays export', '#MOUVEMENT': "type de document"})
+csv.to_sql('mouvement', con=engine, if_exists='replace')
+
+sys.stderr.write("export_bi_etablissements.csv\n")
+csv = pd.read_csv("export_bi_etablissements.csv", encoding='iso-8859-1', delimiter=";", index_col=False).rename(columns={ 'statut (ACTIF, SUSPENDU)': 'statut', "#ETABLISSEMENT": "type de document"})
+csv.to_sql('etablissement', con=engine, if_exists='replace')
+
+sys.stderr.write("export_bi_drm_stock.csv\n")
+csv = pd.read_csv("export_bi_drm_stock.csv", encoding='iso-8859-1', delimiter=";", index_col=False).rename(columns={"#ID": "id stock"})
+csv.to_sql('DRM_Stock', con=engine, if_exists='replace')
