cbdf40d32e32f42eda9125c34033722b6ef066ef
mathurin petit <mathurin.petit@gmail.com>
2020-11-05 17:20:03 +0100

    changement du repérage dans l'edi des type de CRD => il est a présent permis d'écrire PERSONALISE (sans ES à la fin)

--- project/plugins/acVinEdiPlugin/lib/DRMImportCsvEdi.class.php
+++ project/plugins/acVinEdiPlugin/lib/DRMImportCsvEdi.class.php
@@ -486,7 +486,8 @@ class DRMImportCsvEdi extends DRMCsvEdi {
   			$this->csvDoc->addErreur($this->categorieCrdNotFoundError($numLigne, $datas));
   			return;
   		}
-  		if (!$this->configuration->isTypeCrdAccepted($type)) {
+      $typeDroit = $this->configuration->isTypeCrdAccepted($type);
+  		if (!$typeDroit) {
   			$this->csvDoc->addErreur($this->typeCrdNotFoundError($numLigne, $datas));
   			return;
   		}
@@ -494,13 +495,13 @@ class DRMImportCsvEdi extends DRMCsvEdi {
   		if (!$this->configuration->isCentilisationCrdAccepted($centilisation)) {
   			$isBib = null;
   			if (preg_match('/^(BIB|CL)_([0-9]+)/i', $centilisation, $m)) {
-  				$crd = $this->drm->addCrd($categorie, $type, 'AUTRE', $m[2], ($m[1] == 'BIB'));
+  				$crd = $this->drm->addCrd($categorie, $typeDroit, 'AUTRE', $m[2], ($m[1] == 'BIB'));
   			} else {
   				$this->csvDoc->addErreur($this->centilisationCrdNotFoundError($numLigne, $datas));
   				return;
   			}
   		} else {
-  			$crd = $this->drm->addCrd($categorie, $type, $centilisation, null, null);
+  			$crd = $this->drm->addCrd($categorie, $typeDroit, $centilisation, null, null);
   		}
 
   		if ($categorieCrd && !$crd->exist($categorieCrd)) {
