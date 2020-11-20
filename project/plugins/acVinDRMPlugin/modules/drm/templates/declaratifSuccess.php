2e439e0fbd3dffc31f3ff177948db957028d3469
Jb Lm <lemetayer.jb@gmail.com>
2020-11-20 11:45:45 +0100

    gestion suppression drm rectif en admin

--- project/plugins/acVinDRMPlugin/modules/drm/templates/declaratifSuccess.php
+++ project/plugins/acVinDRMPlugin/modules/drm/templates/declaratifSuccess.php
@@ -256,7 +256,7 @@
                     <button type="submit" class="btn_suiv"><span>suivant</span></button>
                 </div>
 
-				<?php if($drm->isRectificative() && $drm->exist('ciel') && $drm->ciel->transfere && !$sf_user->hasCredential(myUser::CREDENTIAL_OPERATEUR): ?>
+				<?php if($drm->isRectificative() && $drm->exist('ciel') && $drm->ciel->transfere && !$sf_user->hasCredential(myUser::CREDENTIAL_OPERATEUR)): ?>
 				<?php else: ?>
                 <div class="ligne_btn">
                     <a href="<?php echo url_for('drm_delete_one', $drm) ?>" class="annuler_saisie btn_remise"><span>supprimer la drm</span></a>
