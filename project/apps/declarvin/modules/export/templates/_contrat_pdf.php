b30fb84c954070f248a59903badb9a1124ba4436
Jb Lm <lemetayer.jb@gmail.com>
2021-01-04 18:52:09 +0100

    correction mise en forme pdf contrat d'inscription suite au passage a php7

--- project/apps/declarvin/modules/export/templates/_contrat_pdf.php
+++ project/apps/declarvin/modules/export/templates/_contrat_pdf.php
@@ -6,13 +6,13 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<meta name="author" content="Actualys" />
-	<meta name="Description" content="" /> 
+	<meta name="Description" content="" />
 	<meta name="Keywords" content="" />
 	<meta name="robots" content="index,follow" />
-	<meta name="Content-Language" content="fr-FR" /> 
+	<meta name="Content-Language" content="fr-FR" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta name="copyright" content="Vins de Provence - 2011" />
-	
+
 	<style type="text/css" media="all">
 		/**
 		 * Reset
@@ -26,18 +26,18 @@
 		dl, dt, dd, ol, ul, li,
 		fieldset, form, label, legend,
 		table, caption, tbody, tfoot, thead, tr, th, td,
-		article, aside, figure,  header, 
+		article, aside, figure,  header,
 		hgroup, menu, nav, section, menu,
 		time, mark, audio, video
 		{
 			border: 0;
-			font-size: 80%;
+			font-size: 100%;
 			margin: 0;
 			outline: 0;
 			padding: 0;
 			vertical-align: baseline;
-		} 
-		
+		}
+
 		html, body { margin: 0 15pt; height: 100%; }
 		a img, fieldset { border: 0; }
 		a *, label, button, input[type=image], input[type=button], input[type=submit] { cursor: pointer; }
@@ -55,10 +55,10 @@
 		q:before, q:after { content: ''; content: none; }
 		hr { border: 0; border-top: 1px solid #ccc; display: block; height: 1px; margin: 1em 0; padding: 0; }
 		input, select { vertical-align: middle; }
-		
-		article, aside, figure,  header, 
+
+		article, aside, figure,  header,
 		hgroup, nav, section { display: block; }
-		
+
 		/**
 		 * Eléments génériques
 		 ******************************************/
@@ -67,7 +67,7 @@
 			overflow-y: scroll;
 			-webkit-font-smoothing: antialiased;
 		}
-		
+
 		body
 		{
 			background: #fff;
@@ -76,34 +76,34 @@
 			font-size: 12px;
 			line-height: 1.2;
 		}
-		 
+
 		a { color: #000; text-decoration: none; }
 		a:hover { text-decoration: underline; }
-		
+
 		h1, h2, h3, h4, h5, h6 { font-weight: bold; }
-		
+
 		textarea, input, select { font: 12px Arial, Verdana, Helvetica, sans-serif; }
-		
+
 		input[type=search] { -webkit-appearance: none; }
 		input[type="radio"] { height: 14px; vertical-align: text-bottom; width: 14px; }
 		input[type="checkbox"] { vertical-align: bottom; *vertical-align: baseline; }
 		.ie6 input { vertical-align: text-bottom; }
-		
+
 		button { background: 0; border: 0; font-family: Arial, Verdana, Helvetica, sans-serif; margin: 0; outline: 0; overflow: visible; padding: 0; }
 		button::-moz-focus-inner { border: 0; padding: 0; }
 		button span { position: relative; margin: -1px 0 -2px; }
-		
+
 		pre, code, kbd, samp { font-family: monospace, sans-serif; }
-		
+
 		pre
-		{ 
+		{
 			padding: 15px;
 			white-space: pre; /* CSS2 */
 			white-space: pre-wrap; /* CSS 2.1 */
 			white-space: pre-line; /* CSS 3 (and 2.1 as well, actually) */
 			word-wrap: break-word; /* IE */
 		}
-		
+
 		/**
 		 * Structure générique
 		 ******************************************/
@@ -116,7 +116,7 @@
 		#global .important p { margin: 10px 0; }
 		#global .souligne { text-decoration: underline; }
 		#global .page_break { page-break-after: always; }
-		
+
 		/**
 		 * Entête
 		 ******************************************/
@@ -125,7 +125,7 @@
 		#entete_doc h1 span { font-style: italic; }
 		#entete_doc p { font-size: 16px; margin: 0 0 20px; }
 		#entete_doc p.note { margin: 0; text-align: left; }
-		
+
 		/**
 		 * Forumulaire
 		 ******************************************/
@@ -134,24 +134,24 @@
 		#formulaire #declarant h2 {  }
 		#formulaire #declarant p { margin: 0; }
 		#formulaire .societe { margin: 0 0 10px; }
-		
+
 		/**
 		 * Interprofessions
 		 ******************************************/
 		#interprofessions { font-size: 13px; margin: 0 0 30px; }
-	
-		
+
+
 		/**
 		 * Articles
 		 ******************************************/
 		#articles { margin: 0 0 20px;}
 		#articles h2 { font-size: 16px; margin: 10px 0; }
 		#articles h3 { font-family: "Times New Roman", Times, serif; font-size: 16px; font-style: italic; margin: 10px 0; }
-                
+
                 #footer {
                     position: fixed; left: 0px;
-                }              
-                
+                }
+
         </style>
 </head>
 
@@ -169,11 +169,11 @@
             $w = $pdf->get_width();
             $h = $pdf->get_height();
             $font = Font_Metrics::get_font("helvetica");
-            $pdf->page_text($w / 2 - 115, $h - 20, 'Declarvins.net - Contrat inscription n<?php echo utf8_encode('°').' '.$contrat->no_contrat ?> - {PAGE_NUM} / {PAGE_COUNT}', $font, 10, array(0,0,0));
+            $pdf->page_text($w / 2 - 115, $h - 40, 'Declarvins.net - Contrat inscription n<?php echo utf8_encode('°').' '.$contrat->no_contrat ?> - {PAGE_NUM} / {PAGE_COUNT}', $font, 10, array(0,0,0));
     }
 </script>
 <!-- #global -->
-	<div id="global">                
+	<div id="global">
 		<div id="entete_doc">
 			<div id="logo">
 				<table width="100%">
@@ -188,34 +188,30 @@
 				Contrat d'inscription <br />
 				&laquo; DeclarVins.net &raquo; N° : <?php echo $contrat->no_contrat ?>&nbsp;&nbsp;&nbsp;<sup>(1)</sup><br />
 			</h1>
-                    <br />
 			<p class="note">(1) Numéro Interprofessionnel d'enregistrement de l'inscription</p>
 		</div>
-                <br />
-                <br />
                 <p>A un système d'identification sécurisé permettant :<br />
                 - de réaliser des Déclarations Informatisées sur DeclarVins.net et de les transmettre aux différents destinataires.<br />
                 - et d'accéder à des services spécifiques à votre Interprofession. </p>
 
-		
+
 		<div id="formulaire">
-			
+
 			<div id="declarant">
 				<h2>D&Eacute;CLARANT</h2>
-                                <br/><br/>
+                                <br/>
 				<p>
-                Je soussigné,&nbsp;&nbsp;Nom : <strong><?php echo $contrat->nom ?></strong>&nbsp;&nbsp;Prénom : <strong><?php echo $contrat->prenom ?></strong>&nbsp;&nbsp;<br/> 
+                Je soussigné,&nbsp;&nbsp;Nom : <strong><?php echo $contrat->nom ?></strong>&nbsp;&nbsp;Prénom : <strong><?php echo $contrat->prenom ?></strong>&nbsp;&nbsp;<br/>
 				Fonction : <strong><?php echo $contrat->fonction ?></strong><br />
                 <?php if($compte->exist('telephone') && $compte->telephone): ?> Tel : <strong><?php echo $compte->telephone ?></strong><br /><?php endif; ?>
                 <?php if($compte->exist('fax') && $compte->fax): ?>Fax : <strong><?php echo $compte->fax ?></strong><br /><?php endif; ?>
                 <?php if($compte->exist('email') && $compte->email): ?>Email : <strong><?php echo $compte->email ?></strong><br /><?php endif; ?>
 				</p>
-                                <br/>
                                 <p class="note">(Attention à vérifier les sécurités, paramètres, et espaces disponibles sur cette adresse mail : des informations importantes vous y seront envoyées. Notamment certains systèmes de sécurité pourraient classer en &laquo; SPAM &raquo; ces informations)</p>
                         </div>
-			
-			<?php 
-				foreach ($contrat->etablissements as $i => $etablissement): 
+
+			<?php
+				foreach ($contrat->etablissements as $i => $etablissement):
 					$zones = array();
 					if ($etablissement->exist('zones')) {
                 		foreach ($etablissement->zones as $zone): if (!$zone->transparente) {$zones[] = $zone->libelle;} endforeach;
@@ -247,7 +243,6 @@
                               	<p>Zones : <strong><?php echo implode(', ', $zones); ?></strong></p>
                                 <p>Provenance EDI : <strong><?php echo ($etablissement->edi) ? "Oui" : "Non" ?></strong></p>
 
-                                <br />
                                 <?php if ($etablissement->comptabilite_adresse): ?>
 				<p>
 					Lieu où est tenue la comptabilité matière (si différente de l'adresse du chai) :<br />
@@ -266,7 +261,7 @@
                         <div class="page_break">&nbsp;</div>
                         <?php endforeach; ?>
 		</div>
-		
+
 		<div class="important">
                     <p>Le déclarant, pour le compte des Etablissements précitées, demande à bénéficier d'une connexion au système informatique &laquo; DeclarVins.net &raquo;, </p>
 		</div>
@@ -292,14 +287,15 @@
         <?php echo include_partial('export/contrat_pdf_conditions',array('contrat' => $contrat)); ?>
                 <br/><br/>
 		<div id="signature">
-                        <p>Fait à 
+                        <p>Fait à
                         <strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</strong>, Le <strong></strong>
                         </p>
-			<p>Le déclarant, <strong><?php echo $contrat->nom ?> <?php echo $contrat->prenom ?></strong> signature et cachet :</p><br/> 								
+			<p>Le déclarant, <strong><?php echo $contrat->nom ?> <?php echo $contrat->prenom ?></strong> signature et cachet :</p><br/>
+			<br /><br /><br />
 			<p>L'e-mail d'activation, que vous recevrez après traitement et validation de votre inscription, vaudra engagement de l'interprofession.<strong></strong></p>
-		</div>                
+		</div>
 	</div>
-        		
+
 <!-- fin #global -->
 
 <!-- ####### A REPRENDRE ABSOLUMENT ####### -->
