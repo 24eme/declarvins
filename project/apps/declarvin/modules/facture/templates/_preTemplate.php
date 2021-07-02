4382ee1a53c8948b6bdd62aa3cc08f3f49cf83bd
Jb Lm <lemetayer.jb@gmail.com>
2021-07-02 17:50:05 +0200

    gestion de nav dans le pretemplate

--- project/apps/declarvin/modules/facture/templates/_preTemplate.php
+++ project/apps/declarvin/modules/facture/templates/_preTemplate.php
@@ -1,2 +1,8 @@
-<?php include_component('global', 'navTop', array('active' => 'factures')); ?>
+<?php
+if($sf_request->getAttribute('sf_route')->getRawValue() instanceof InterfaceEtablissementRoute):
+  include_component('global', 'navTop', array('active' => 'factures'));
+else:
+  include_component('global', 'navBack', array('active' => 'operateurs', 'subactive' => 'factures'));
+endif;
+?>
 <section id="contenu">
