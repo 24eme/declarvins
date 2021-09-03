7bc13dbaddd5d372e5853a0a51454aad91a5a88d
Jb Lm <lemetayer.jb@gmail.com>
2021-09-03 15:13:19 +0200

    formulaire de connexion aux etablissements depuis l'admin factures

--- project/apps/declarvin/modules/facture/templates/_preTemplate.php
+++ project/apps/declarvin/modules/facture/templates/_preTemplate.php
@@ -6,3 +6,11 @@ else:
 endif;
 ?>
 <section id="contenu">
+  <?php if (!($sf_request->getAttribute('sf_route')->getRawValue() instanceof InterfaceEtablissementRoute)): ?>
+    <div class="clearfix" id="application_dr">
+        <div id="mon_compte">
+            <?php include_partial('admin/etablissement_login_form', array('form' => new EtablissementSelectionForm($sf_user->getCompte()->getGerantInterpro()->get('_id'), array(), array('admin' => $sf_user->hasCredential(myUser::CREDENTIAL_ADMIN))), 'route' => '@etablissement_login_factures')) ?>
+        </div>
+    </div>
+
+  <?php endif; ?>
