b2d48ec26583a1b665e081fe29433a97c93cb95a
Jb Lm <lemetayer.jb@gmail.com>
2021-09-20 17:27:30 +0200

    amelioration tache de generation de societe et possibilite d'affectation du code comptable en option

--- project/lib/task/generateSocieteByEtablissementTask.class.php
+++ project/lib/task/generateSocieteByEtablissementTask.class.php
@@ -11,6 +11,7 @@ class generateSocieteByEtablissementTask extends sfBaseTask
       new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name', 'declarvin'),
       new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
       new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'default'),
+      new sfCommandOption('code-comptable', null, sfCommandOption::PARAMETER_OPTIONAL, 'Numero de code comptable'),
       // add your own options here
     ));
 
@@ -28,16 +29,40 @@ EOF;
     $databaseManager = new sfDatabaseManager($this->configuration);
     $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
 
+    if ($s = SocieteClient::getInstance()->find($arguments['identifiant'])) {
+      $s->delete();
+    }
+    exit;
+
   	$etablissement = EtablissementClient::getInstance()->find($arguments['identifiant']);
+    $cc = null;
+    if (isset($options['code-comptable']) && $options['code-comptable'] && !in_array($options['code-comptable'], ['4110000C0', '4110000C'])) {
+      $cc = $options['code-comptable'];
+    }
 
     if (!$etablissement) {
       $this->logSection("generate:societe-by-etablissement", "Etablissement not found with id : ".$arguments['identifiant'], null, 'ERROR');
+      return;
     }
 
-    $societe = $etablissement->getGenerateSociete();
-    $societe->save();
+    if ($s = SocieteClient::getInstance()->find($etablissement->identifiant)) {
+        $this->logSection("generate:societe-by-etablissement", "Société ".$arguments['identifiant']." ($cc) existante avec le code comptable : ".$s->code_comptable_client, null, 'WARNING');
+        return;
+    }
 
-    $this->logSection("debug", "Société créée avec succès ".$societe->_id, null, 'SUCCESS');
+    try {
+      $societe = $etablissement->getGenerateSociete();
+    } catch (Exception $e) {
+        $this->logSection("generate:societe-by-etablissement", "Generation impossible : ".$e->getMessage(), null, 'ERROR');
+        return;
+    }
+
+    if ($cc) {
+      $societe->code_comptable_client = $cc;
+    }
+
+    $societe->save();
+    $this->logSection("debug", "Société créée avec succès ".$societe->_id." (".$societe->code_comptable_client.")", null, 'SUCCESS');
 
   }
 }
