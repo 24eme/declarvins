a468ac39c673c6d03668c01758b26a0234bc8366
Jb Lm <lemetayer.jb@gmail.com>
2021-09-27 21:34:03 +0200

    formulaire etablissement seulement dans la page facture admin

--- project/apps/declarvin/modules/facture/templates/_preTemplate.php
+++ project/apps/declarvin/modules/facture/templates/_preTemplate.php
@@ -6,11 +6,10 @@ else:
 endif;
 ?>
 <section id="contenu">
-  <?php if (!($sf_request->getAttribute('sf_route')->getRawValue() instanceof InterfaceEtablissementRoute)): ?>
+  <?php if ($sf_context->getInstance()->getRouting()->getCurrentRouteName() == 'facture'): ?>
     <div class="clearfix" id="application_dr">
         <div id="mon_compte">
             <?php include_partial('admin/etablissement_login_form', array('form' => new EtablissementSelectionForm($sf_user->getCompte()->getGerantInterpro()->get('_id'), array(), array('admin' => $sf_user->hasCredential(myUser::CREDENTIAL_ADMIN))), 'route' => '@etablissement_login_factures')) ?>
         </div>
     </div>
-
   <?php endif; ?>
