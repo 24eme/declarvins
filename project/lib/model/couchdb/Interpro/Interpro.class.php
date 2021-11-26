c4e847caf40df018f3b0b36eace0013a07de2da9
Jb Lm <lemetayer.jb@gmail.com>
2021-11-26 18:24:47 +0100

    refacto car utilisation du fichier de grc pour la tache de generation des bilans

--- project/lib/model/couchdb/Interpro/Interpro.class.php
+++ project/lib/model/couchdb/Interpro/Interpro.class.php
@@ -4,10 +4,28 @@ class Interpro extends BaseInterpro {
 	const INTER_RHONE_ID = 'IR';
 	const CIVP_ID = 'CIVP';
 	const INTERVINS_SUD_EST_ID = 'IVSE';
+    const GRC_FILENAME = 'etablissements.csv';
   public function __toString() {
     return $this->nom;
   }
   public function getKey() {
     return $this->_id;
+  }
+  public function getEtablissementsArrayFromGrcFile() {
+      $file = $this->getAttachmentUri(self::GRC_FILENAME);
+      $content = file_get_contents($file);
+      $arr = array();
+      if ($content) {
+          $lines = explode(PHP_EOL, $content);
+          foreach ($lines as $line) {
+              $lineTrimed = trim($line);
+              if (!$lineTrimed||$lineTrimed[0] == '#') {
+                  continue;
+              }
+              $lineArr = str_getcsv($lineTrimed, ';');
+              $arr[$lineArr[0]] = $lineArr;
+          }
+      }
+      return $arr;
   }
 }
