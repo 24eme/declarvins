ada38bd1a67762a4c59f1390fab4e9cc65ddac7c
Jb Lm <lemetayer.jb@gmail.com>
2021-11-30 15:13:52 +0100

    ajout libelle a cote de plus et adaptation inté

--- project/plugins/acVinDRMPlugin/modules/drm_vrac/templates/_form_contrat_item.php
+++ project/plugins/acVinDRMPlugin/modules/drm_vrac/templates/_form_contrat_item.php
@@ -1,11 +1,11 @@
 <tr class="contenu">
-    <td width="600px">
+    <td width="500px">
         <?php echo $form['vrac']->render(array('class' => 'drm_vrac_contrats')) ?>
     </td>
     <td align="center">
         <?php echo $form['volume']->render(array('class' => ' num_light num_float')) ?> (hl)
     </td>
-    <td align="center" width="35px">
+    <td align="center" width="125px">
         <a class="btn_supprimer_ligne_template" data-container="tr" href="#"><img src="/images/pictos/pi_supprimer.png" alt="X" /></a>
     </td>
 </tr>
