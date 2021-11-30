00555aaec6f6bb4493af5901c973fefae1c6be03
Jb Lm <lemetayer.jb@gmail.com>
2021-11-30 14:24:23 +0100

    gestion du bilan drm pour IVSE en ciblant les etablissements

--- project/lib/model/couchdb/Interpro/Interpro.class.php
+++ project/lib/model/couchdb/Interpro/Interpro.class.php
@@ -12,6 +12,9 @@ class Interpro extends BaseInterpro {
     return $this->_id;
   }
   public function getEtablissementsArrayFromGrcFile() {
+      if($this->identifiant == self::INTERVINS_SUD_EST_ID) {
+          return $this->getEtablissementsDelegues($this->identifiant);
+      }
       $file = $this->getAttachmentUri(self::GRC_FILENAME);
       $content = file_get_contents($file);
       $arr = array();
@@ -27,5 +30,25 @@ class Interpro extends BaseInterpro {
           }
       }
       return $arr;
+  }
+  private function getEtablissementsDelegues($interpro) {
+      $etablissements = array_merge(
+          InterproClient::getInstance()->retrieveById(self::INTER_RHONE_ID)->getEtablissementsArrayFromGrcFile(),
+          InterproClient::getInstance()->retrieveById(self::CIVP_ID)->getEtablissementsArrayFromGrcFile()
+      );
+      foreach($etablissements as $k => $etablissement) {
+          $zones = explode('|', $etablissement[EtablissementCsv::COL_ZONES]);
+          $inZone = false;
+      	  foreach ($zones as $zone) {
+      		if (strtoupper(trim($zone)) == $interpro) {
+                $inZone = true;
+                break;
+            }
+      	  }
+          if (!$inZone) {
+              unset($etablissements[$k]);
+          }
+      }
+      return $etablissements;
   }
 }
