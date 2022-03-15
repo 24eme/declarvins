624edd56deb8a0c50090bb1e98ac7aacfdbf1b84
Jb Lm <lemetayer.jb@gmail.com>
2022-03-15 16:01:37 +0100

    suppression du plugin statistique pour refacto

--- project/plugins/StatistiquePlugin/modules/statistique/templates/_form_produits_item.php
+++
@@ -1,8 +0,0 @@
-<div class="filtre_produits_item">
-	<div class="ligne_form">
-		<?php echo $form['declaration']->renderError() ?>
-		<?php echo $form['declaration']->renderLabel(null, array('class' => 'intitule_champ')) ?>
-		<?php echo $form['declaration']->render() ?>
-		<a href="#" data-container="div.filtre_produits_item" class="btn_supprimer_ligne_template">Supprimer</a>
-	</div>
-</div>
--- project/plugins/StatistiquePlugin/modules/statistique/templates/_paginationStatistiqueFilter.php
+++
@@ -1,9 +0,0 @@
-<div style="text-align: right; margin: 20px 0;">
-<?php if ($page > 1): ?>
-<a class="pagination_link" href="<?php echo url_for('statistiques_'.$type, array('p' => ($page - 1))) ?>">&lt;&lt;</a>
-<?php endif; ?>
-(<strong><?php echo $page ?></strong>/<?php echo $nbPage ?>)
-<?php if ($page < $nbPage): ?>
-<a class="pagination_link" href="<?php echo url_for('statistiques_'.$type, array('p' => ($page + 1))) ?>">&gt;&gt;</a>
-<?php endif; ?>
-</div>
