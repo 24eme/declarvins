2b7ef5fad496ed6917f90806d1259f9878ab33d9
Jb Lm <lemetayer.jb@gmail.com>
2022-08-05 22:49:38 +0200

    harmonisation du bandeau contrat adossé à la visu des pluriannuel

--- project/plugins/acVinVracPlugin/modules/vrac/templates/etapeSuccess.php
+++ project/plugins/acVinVracPlugin/modules/vrac/templates/etapeSuccess.php
@@ -3,10 +3,10 @@
 <section id="contenu" class="vracs vrac_<?php echo $etape ?>">
     <?php include_component('vrac', 'etapes', array('vrac' => $form->getObject(), 'actif' => $etape, 'vracEtape' => ($form->getObject()->etape)? $form->getObject()->etape : $etape, 'etablissement' => $etablissement)); ?>
     <?php if($configurationVrac->isContratPluriannuelActif() && $form->getObject()->isPluriannuelAdosse()): ?>
-        <div class="titre" style="background-color: #eee; margin-top: 10px;padding: 5px 0;text-align:center;">
-            <strong style="font-size: 13px; margin-right: 50px;background: url('/images/pictos/pi_pluriannuel.png') left 0 no-repeat;padding: 0px 5px 0 20px;">
-                Contrat adossé au contrat pluriannuel n°<a target="_blank" href="<?php echo url_for('vrac_visualisation', array('contrat' => $form->getObject()->reference_contrat_pluriannuel)) ?>"><?php echo $form->getObject()->reference_contrat_pluriannuel ?></a>
-            </strong>
+        <div class="titre" style="background-color: #eee; margin: 10px 0;padding: 5px 0;text-align:center;">
+            <span class="style_label" style="font-size: 15px; font-weight: bold; text-transform: uppercase; margin-right: 50px;background: url('/images/pictos/pi_pluriannuel.png') left 0 no-repeat;padding: 0px 5px 0 20px;">
+                Contrat adossé au contrat pluriannuel n° <a target="_blank" href="<?php echo url_for('vrac_visualisation', array('contrat' => $form->getObject()->reference_contrat_pluriannuel)) ?>"><?php echo $form->getObject()->reference_contrat_pluriannuel ?></a>
+            </span>
         </div>
     <?php endif; ?>
     <?php include_partial('form_'.$etape, array('form' => $form, 'etape' => $etape, 'configurationVrac' => $configurationVrac, 'etablissement' => $etablissement, 'pluriannuel' => $pluriannuel)) ?>
