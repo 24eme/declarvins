9ae8b30cf51ac34232b6868c0b605712f1682fb5
Honorine-MC <honorine.mariechristy@gmail.com>
2022-09-09 10:46:09 +0200

    ajout filtre par produit: seulement les produits avec IGP pour l'api

--- project/plugins/acVinVracPlugin/lib/model/Vrac/client/VracClient.class.php
+++ project/plugins/acVinVracPlugin/lib/model/Vrac/client/VracClient.class.php
@@ -314,7 +314,13 @@ class VracClient extends acCouchdbClient {
             if ($mustActive && $c->value[VracAllView::VRAC_VIEW_STATUT] != self::STATUS_CONTRAT_NONSOLDE) {
                 continue;
             }
+
             $contrat = parent::retrieveDocumentById($c->key[VracAllView::VRAC_VIEW_ID]);
+
+            if(!preg_match("/IGP/",$contrat->produit)){
+                continue;
+            }
+
             if($contrat->millesime != $millesime){
                 continue;
             }
