c41d9c8a7dcb40246d666c734201d56de7104fe1
Jb Lm <lemetayer.jb@gmail.com>
2022-10-27 18:35:02 +0200

    tâche de mise a jour des codes comptables pour le multi interpro

---
+++ project/lib/task/updateSocietesModelCodesComptablesTask.class.php
@@ -0,0 +1,43 @@
+<?php
+
+class updateSocietesModelCodesComptablesTask extends sfBaseTask {
+
+    protected function configure() {
+        $this->addOptions(array(
+            new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'app name', 'declarvin'),
+            new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+            new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'default'),
+                // add your own options here
+        ));
+
+        $this->namespace = 'update';
+        $this->name = 'societes-model-codes-comptables';
+        $this->briefDescription = '';
+        $this->detailedDescription = <<<EOF
+The [update|INFO] task does things.
+Call it with:
+  [php symfony update|INFO]
+EOF;
+    }
+
+    protected function execute($arguments = array(), $options = array()) {
+		ini_set('memory_limit', '2048M');
+  		set_time_limit(0);
+        // initialize the database connection
+        $databaseManager = new sfDatabaseManager($this->configuration);
+        $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
+
+        $items = SocieteAllView::getInstance()->findByInterpro('INTERPRO-declaration');
+        foreach($items as $item) {
+            $societe = SocieteClient::getInstance()->find($item->id);
+            if ($societe->code_comptable_client) {
+                $codes = $societe->getOrAdd('codes_comptables_client');
+                $codes->add('INTERPRO-IVSE', $societe->code_comptable_client);
+                $societe->save();
+                echo $societe->_id." updated\n";
+            }
+        }
+
+    }
+
+}
