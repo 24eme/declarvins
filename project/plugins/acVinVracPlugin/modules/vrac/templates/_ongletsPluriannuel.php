61464d6de3e7b78a1febdcb46bbf36cbf7e4fe6c
Gabriel Poma <gpoma@24eme.fr>
2022-12-20 14:44:50 +0100

    pluri: onglet avec cadre / application

--- project/plugins/acVinVracPlugin/modules/vrac/templates/_ongletsPluriannuel.php
+++ project/plugins/acVinVracPlugin/modules/vrac/templates/_ongletsPluriannuel.php
@@ -2,7 +2,7 @@
     <?php foreach ($contrats as $contrat): ?>
         <li role="presentation"<?php if ($contrat->id === $vrac->_id) { echo 'class="active"'; } ?>>
             <a href="<?php echo url_for('vrac_visualisation', ['identifiant' => $etablissement, 'contrat' => $contrat->value[VracHistoryView::VRAC_VIEW_NUM]]) ?>">
-                <?php echo $contrat->id ?>
+                <?php echo ($contrat->value[VracHistoryView::VRAC_REF_PLURIANNUEL] === null) ? 'Cadre' : 'Application' ?> <?php echo $contrat->value[VracHistoryView::VRAC_VIEW_NUM] ?>
             </a>
         </li>
     <?php endforeach ?>
