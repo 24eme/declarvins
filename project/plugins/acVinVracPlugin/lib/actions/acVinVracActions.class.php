40b43cea185e14838bff7fc9c7386f604d5e5b84
Jb Lm <lemetayer.jb@gmail.com>
2022-12-23 17:00:42 +0100

    bug en attente non remontes

--- project/plugins/acVinVracPlugin/lib/actions/acVinVracActions.class.php
+++ project/plugins/acVinVracPlugin/lib/actions/acVinVracActions.class.php
@@ -29,6 +29,9 @@ class acVinVracActions extends sfActions
     	}
         $this->forward404Unless($this->interpro = $this->getUser()->getCompte()->getGerantInterpro());
         $this->statut = $request->getParameter('statut');
+        if (!$this->statut) {
+            $this->statut = 0;
+        }
         $this->forward404Unless(in_array($this->statut, array_merge(VracClient::getInstance()->getStatusContrat(true), array('TOUS'))));
 		$this->pluriannuel = (int)$request->getParameter('pluriannuel', 0);
 		$this->configurationVrac = $this->getConfigurationVrac($this->interpro->_id);
