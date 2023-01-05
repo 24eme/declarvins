8a003e0c1ff63f1298242925dcdd4b98d58c9dce
Jb Lm <lemetayer.jb@gmail.com>
2023-01-05 12:25:50 +0100

    on affiche le numero d'application

--- project/plugins/acVinVracPlugin/modules/vrac/templates/_ongletsPluriannuel.php
+++ project/plugins/acVinVracPlugin/modules/vrac/templates/_ongletsPluriannuel.php
@@ -3,7 +3,7 @@
         <li role="presentation"<?php if ($contrat->id === $vrac->_id) { echo ' class="active" style="font-weight: bold"'; } ?>>
             <a href="<?php echo url_for('vrac_visualisation', ['etablissement' => $etablissement, 'contrat' => $contrat->value[VracHistoryView::VRAC_VIEW_NUM]]) ?>">
                 <?php echo ($contrat->value[VracHistoryView::VRAC_REF_PLURIANNUEL] === null) ? 'Cadre' : 'Application' ?>
-                <?php echo (strpos($contrat->value[VracHistoryView::VRAC_VIEW_NUM], '-A') === false)? $contrat->value[VracHistoryView::VRAC_VIEW_NUM] : substr($contrat->value[VracHistoryView::VRAC_VIEW_NUM], 0, strpos($contrat->value[VracHistoryView::VRAC_VIEW_NUM], '-A')); ?>
+                <?php echo (strpos($contrat->value[VracHistoryView::VRAC_VIEW_NUM], '-A') === false)? $contrat->value[VracHistoryView::VRAC_VIEW_NUM] : substr($contrat->value[VracHistoryView::VRAC_VIEW_NUM], -2); ?>
             </a>
         </li>
     <?php endforeach ?>
