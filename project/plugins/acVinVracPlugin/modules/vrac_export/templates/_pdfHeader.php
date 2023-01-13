9eec4940ea2006cc295dc4961c4f41569300b733
Jb Lm <lemetayer.jb@gmail.com>
2023-01-13 16:48:05 +0100

    pdf contrat annulé mis en evidence

--- project/plugins/acVinVracPlugin/modules/vrac_export/templates/_pdfHeader.php
+++ project/plugins/acVinVracPlugin/modules/vrac_export/templates/_pdfHeader.php
@@ -4,7 +4,7 @@
 	</div>
 	<center>
 		<h1>
-			<?php if ($vrac->numero_contrat): ?>Contrat<?php else: ?>Proposition<?php endif; ?><?php if ($vrac->isPluriannuel()): ?> pluriannuel cadre<?php endif; ?> de <?php echo strtolower($configurationVrac->formatTypesTransactionLibelle(array($vrac->type_transaction))); ?><?php if ($vrac->type_transaction == 'vrac'): ?> en vrac<?php endif; ?>
+			<?php if ($vrac->numero_contrat): ?>Contrat<?php else: ?>Proposition<?php endif; ?><?php if ($vrac->valide->statut == VracClient::STATUS_CONTRAT_ANNULE): ?> ANNULÉ<?php if (!$vrac->numero_contrat): ?>E<?php endif; ?><?php else: ?><?php if ($vrac->isPluriannuel()): ?> pluriannuel cadre<?php endif; ?> de <?php echo strtolower($configurationVrac->formatTypesTransactionLibelle(array($vrac->type_transaction))); ?><?php if ($vrac->type_transaction == 'vrac'): ?> en vrac<?php endif; ?><?php endif; ?>
 			<?php if($vrac->isRectificative()): ?>
  			- <span class="rectificative">Rectificatif <?php echo sprintf('%02d', $vrac->rectificative) ?></span>
 			<?php endif; ?>
@@ -23,7 +23,7 @@
 		<tr>
 			<td width="50%">Saisie le <?php echo $vrac->getEuSaisieDate(); ?></td>
 			<?php if ($vrac->isValide()): ?>
-			<td width="50%" style="text-align: right;">N° de Visa du contrat : <?php echo $vrac->numero_contrat; ?></td>
+			<td width="50%" style="text-align: right;">N° de Visa du contrat : <?php if ($vrac->valide->statut == VracClient::STATUS_CONTRAT_ANNULE): ?> <strike style="font-size:11px;"><?php echo $vrac->numero_contrat; ?></strike> ANNULÉ<?php else: ?><?php echo $vrac->numero_contrat; ?><?php endif; ?></td>
 			<?php else: ?>
 			<td width="50%" style="text-align: right;">&nbsp;</td>
 			<?php endif; ?>
