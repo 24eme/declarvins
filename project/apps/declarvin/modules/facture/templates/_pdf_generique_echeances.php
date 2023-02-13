9002f93cccf4e445f013aa7d4ab047dfb8db6b38
Jb Lm <lemetayer.jb@gmail.com>
2023-02-13 15:20:34 +0100

    inversion position cheque / traite

--- project/apps/declarvin/modules/facture/templates/_pdf_generique_echeances.php
+++ project/apps/declarvin/modules/facture/templates/_pdf_generique_echeances.php
@@ -16,14 +16,14 @@ $echeances = $facture->getEcheancesPapillon();
         \CutlnPapillonEntete
         <?php if($facture->getNbPaiementsAutomatique()): ?>
           &
-          \centering \fontsize{7}{8}\selectfont Par chèque à l'ordre : <?php echo ($chequesOrdre)? $chequesOrdre : "Ordre chèque"; ?> \\ ~ &
+          \centering \fontsize{7}{8}\selectfont Par traite sur le compte \textbf{<?php echo $facture->getSociete()->getMandatSepa()->getBanqueNom() ?>} \\  \textbf{RIB~}:<?php echo $facture->getSociete()->getMandatSepa()->getRibFormate() ?>~ &
 
            \centering \small{Echéance} &
            \centering \small{Client~/~Facture} &
            \multicolumn{1}{c}{\small{Montant TTC}} \\
 
                        \centering \small{~} &
-                       \centering \fontsize{7}{8}\selectfont Par traite sur le compte \textbf{<?php echo $facture->getSociete()->getMandatSepa()->getBanqueNom() ?>} \\  \textbf{RIB~}:<?php echo $facture->getSociete()->getMandatSepa()->getRibFormate() ?>~ &
+                       \centering \fontsize{7}{8}\selectfont Par chèque à l'ordre : <?php echo ($chequesOrdre)? $chequesOrdre : "Ordre chèque"; ?> ~ &
 
                   \centering \small{\textbf{<?php echo format_date($facture->date_echeance,'dd/MM/yyyy'); ?>}} &
                   \centering \small{\FactureRefCodeComptableClient~/~\FactureNum} &
