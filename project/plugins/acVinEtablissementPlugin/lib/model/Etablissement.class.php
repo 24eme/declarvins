d0cb10b4a0005f15fb0314bbff5e357122c1a67f
Gabriel Poma <gpoma@24eme.fr>
2023-04-06 13:38:02 +0200

    etablissement: on update la société au save de l'etablissement

--- project/plugins/acVinEtablissementPlugin/lib/model/Etablissement.class.php
+++ project/plugins/acVinEtablissementPlugin/lib/model/Etablissement.class.php
@@ -140,6 +140,10 @@ class Etablissement extends BaseEtablissement {
     		$this->famille = EtablissementFamilles::FAMILLE_PRODUCTEUR;
     		$this->sous_famille = EtablissementFamilles::SOUS_FAMILLE_CAVE_PARTICULIERE;
     	}
+
+        $societe = $this->getGenerateSociete();
+        $societe->save();
+
     	parent::save();
     }
 
