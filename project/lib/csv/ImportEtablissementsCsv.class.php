2e2b7203d89a4cf1326bfd370fb6d59335b0674b
Jb Lm <lemetayer.jb@gmail.com>
2023-11-23 11:14:18 +0100

    controle ecriture scientifique numero siret

--- project/lib/csv/ImportEtablissementsCsv.class.php
+++ project/lib/csv/ImportEtablissementsCsv.class.php
@@ -173,6 +173,10 @@ class ImportEtablissementsCsv {
     			throw new sfException('has errors');
     		}
     	}
+        if ($line[EtablissementCsv::COL_SIRET] && strpos($line[EtablissementCsv::COL_SIRET], 'E+') !== false) {
+            $this->_errors[$ligne] = array('Colonne (indice '.(EtablissementCsv::COL_SIRET + 1).')  le format du numero siret est incorrect');
+            throw new sfException('has errors');
+        }
     	$etab->identifiant = trim($line[EtablissementCsv::COL_ID]);
         $etab->num_interne = trim($line[EtablissementCsv::COL_NUM_INTERNE]);
         $etab->siret = trim($line[EtablissementCsv::COL_SIRET]);
