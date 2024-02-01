1cae40802674e451c7bea877cb64bbcaf9c60b84
Jb Lm <lemetayer.jb@gmail.com>
2024-02-01 10:17:04 +0100

    les contrats tiré bouché ne sont pas transmis a l'oioc

--- project/plugins/acVinVracPlugin/lib/model/Vrac/Vrac.class.php
+++ project/plugins/acVinVracPlugin/lib/model/Vrac/Vrac.class.php
@@ -613,7 +613,7 @@ class Vrac extends BaseVrac implements InterfaceVersionDocument
     {
     	$produit = $this->getProduitObject();
     	if ($organisme = $produit->getCurrentOrganisme($this->valide->date_saisie, true)) {
-	    	return true;
+	    	return ($this->type_transaction == 'vrac' && ($this->type_retiraison == 'vrac' || !$this->type_retiraison));
     	}
     	return false;
 
