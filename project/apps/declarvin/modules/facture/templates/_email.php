a8d5dbc6d39058ed578edc42bacb1913cd2ad65c
Gabriel Poma <gpoma@24eme.fr>
2024-04-03 12:25:50 +0200

    mail: pas de point à la fin de l'url de la facture

--- project/apps/declarvin/modules/facture/templates/_email.php
+++ project/apps/declarvin/modules/facture/templates/_email.php
@@ -15,7 +15,7 @@ Vous pouvez retrouver vos factures dans votre espace Déclarvins, onglet « fact
 
 Une nouvelle facture est disponible.
 
-Vous pouvez la télécharger directement en cliquant sur le lien : <?php echo ProjectConfiguration::getAppRouting()->generate('facture_pdf_auth', array('id' => $facture->_id, 'auth' => FactureClient::generateAuthKey($facture->_id)), true); ?>.
+Vous pouvez la télécharger directement en cliquant sur le lien : <?php echo ProjectConfiguration::getAppRouting()->generate('facture_pdf_auth', array('id' => $facture->_id, 'auth' => FactureClient::generateAuthKey($facture->_id)), true); ?>
 
 Dans le cas d'une modification à apporter, n'hésitez pas nous contacter au 04 90 42 90 04 ou à contact@intervins-sudest.org.
 
@@ -25,7 +25,7 @@ Bonjour,
 
 Une nouvelle facture émise par le <?php echo $interproNom ?> est disponible.
 
-Vous pouvez la télécharger directement en cliquant sur le lien : <?php echo ProjectConfiguration::getAppRouting()->generate('facture_pdf_auth', array('id' => $facture->_id, 'auth' => FactureClient::generateAuthKey($facture->_id)), true); ?>.
+Vous pouvez la télécharger directement en cliquant sur le lien : <?php echo ProjectConfiguration::getAppRouting()->generate('facture_pdf_auth', array('id' => $facture->_id, 'auth' => FactureClient::generateAuthKey($facture->_id)), true); ?>
 
 En cas de question concernant les volumes de cette facture, contactez declarvins@provenwines.com
 
@@ -39,7 +39,7 @@ Bonjour,
 
 Une nouvelle facture émise par <?php echo $interproNom ?> est disponible.
 
-Vous pouvez la télécharger directement en cliquant sur le lien : <?php echo ProjectConfiguration::getAppRouting()->generate('facture_pdf_auth', array('id' => $facture->_id, 'auth' => FactureClient::generateAuthKey($facture->_id)), true); ?>.
+Vous pouvez la télécharger directement en cliquant sur le lien : <?php echo ProjectConfiguration::getAppRouting()->generate('facture_pdf_auth', array('id' => $facture->_id, 'auth' => FactureClient::generateAuthKey($facture->_id)), true); ?>
 
 Bien cordialement,
 
