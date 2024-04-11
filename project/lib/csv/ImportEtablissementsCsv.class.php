dea0da77a19f5fda9c94869c9f540da2c2dba797
Tangui Morlier <tangui@tangui.eu.org>
2024-04-11 16:07:02 +0200

    Ignore l'entête

--- project/lib/csv/ImportEtablissementsCsv.class.php
+++ project/lib/csv/ImportEtablissementsCsv.class.php
@@ -339,6 +339,9 @@ class ImportEtablissementsCsv {
     	$ligne = 0;
       	foreach ($this->_csv as $line) {
       		$ligne++;
+            if (strpos($line[EtablissementCsv::COL_ID], 'ID') !== false) {
+                continue;
+            }
             if (!$this->isEtablissementIdFormatChecked(trim($line[EtablissementCsv::COL_ID]))) {
                 $this->_errors[$ligne] = array('Colonne (indice '.(EtablissementCsv::COL_ID + 1).') le format d\'identifiant de l\'etablissement est incorrect');
                 continue;
