492109513bc302ca2b1c5976aad0e50369fb2905
Tangui Morlier <tangui@tangui.eu.org>
2024-04-11 15:00:22 +0200

    Cohabitation comptes OpenODG dans Giilda

--- project/lib/model/couchdb/_Compte/client/CompteClient.class.php
+++ project/lib/model/couchdb/_Compte/client/CompteClient.class.php
@@ -2,5 +2,22 @@
 
 class CompteClient extends _CompteClient
 {
-const TYPE_COMPTE_SOCIETE = "SOCIETE";
+    const TYPE_COMPTE_SOCIETE = "SOCIETE";
+
+    public static function getInstance() {
+        return acCouchdbManager::getClient("Compte");
+    }
+
+    public function findOrCreateCompteSociete($societe) {
+        $compte = null;
+        if ($societe->compte_societe) {
+            $compte = $this->find($societe->compte_societe);
+        }
+        return $compte;
+    }
+
+    public static function isRealSyncCompte() {
+        return false;
+    }
+
 }
