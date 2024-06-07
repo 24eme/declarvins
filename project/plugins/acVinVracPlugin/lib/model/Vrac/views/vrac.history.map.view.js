e4b4a7016267fac11bbdf0ed721fa5cd1dbe25d7
JB LM <lemetayer.jb@gmail.com>
2024-06-07 16:41:53 +0200

    update view history vrac avec notamment l'ajout du label dans le libelle produit

--- project/plugins/acVinVracPlugin/lib/model/Vrac/views/vrac.history.map.view.js
+++ project/plugins/acVinVracPlugin/lib/model/Vrac/views/vrac.history.map.view.js
@@ -17,7 +17,15 @@ function(doc) {
         else if (doc.surface) vrac_quantite_libelle = doc.surface+' ha';
         else if (doc.pourcentage_recolte) vrac_quantite_libelle = doc.surface+' %R';
         else vrac_quantite_libelle = '';
-	var vue = [doc.valide.statut, doc._id, doc.acheteur_identifiant, doc.acheteur.nom, doc.acheteur.raison_sociale, doc.vendeur_identifiant, doc.vendeur.nom, doc.vendeur.raison_sociale, doc.mandataire_identifiant, doc.mandataire.nom, doc.mandataire.raison_sociale, doc.type_transaction, doc.produit, doc.produit_libelle, doc.volume_propose, doc.volume_enleve, doc.prix_total, doc.prix_unitaire, doc.part_cvo, doc.millesime, doc.labels, doc.mentions, doc.mode_de_saisie, doc.acheteur_identifiant, doc.mandataire_identifiant, doc.vendeur_identifiant, doc.valide.date_validation_acheteur, doc.valide.date_validation_mandataire, doc.valide.date_validation_vendeur, doc.valide.date_saisie, doc.date_relance, doc.vous_etes, doc.numero_contrat, vrac_version, vrac_referente, vrac_oioc_identifiant, vrac_oioc_statut, vrac_oioc_date_reception, vrac_oioc_date_traitement, doc.poids, vrac_reference_pluriannuel, vrac_quantite_libelle];
+        var statut_acheteur = doc.valide.date_validation_acheteur;
+        var statut_vendeur = doc.valide.date_validation_vendeur;
+        var statut_mandataire = doc.valide.date_validation_mandataire;
+        if (doc.annulation) {
+        	statut_acheteur = doc.annulation.date_annulation_acheteur;
+        	statut_vendeur = doc.annulation.date_annulation_vendeur;
+        	statut_mandataire = doc.annulation.date_annulation_mandataire;
+        }
+	var vue = [doc.valide.statut, doc._id, doc.acheteur_identifiant, doc.acheteur.nom, doc.acheteur.raison_sociale, doc.vendeur_identifiant, doc.vendeur.nom, doc.vendeur.raison_sociale, doc.mandataire_identifiant, doc.mandataire.nom, doc.mandataire.raison_sociale, doc.type_transaction, doc.produit, doc.produit_libelle+' '+doc.labels_libelle, doc.volume_propose, doc.volume_enleve, doc.prix_total, doc.prix_unitaire, doc.part_cvo, doc.millesime, doc.labels, doc.mentions, doc.mode_de_saisie, doc.acheteur_identifiant, doc.mandataire_identifiant, doc.vendeur_identifiant, statut_acheteur, statut_mandataire, statut_vendeur, doc.valide.date_saisie, doc.date_relance, doc.vous_etes, doc.numero_contrat, vrac_version, vrac_referente, vrac_oioc_identifiant, vrac_oioc_statut, vrac_oioc_date_reception, vrac_oioc_date_traitement, doc.poids, vrac_reference_pluriannuel, vrac_quantite_libelle];
 	emit([vrac_pluriannuel, statut, doc.interpro, doc.valide.date_saisie, doc._id], vue);
     	if (doc.mandataire_exist) {
     		emit([doc.mandataire_identifiant, vrac_pluriannuel, statut, doc.interpro, doc.valide.date_saisie, doc._id],  vue);
