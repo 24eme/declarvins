4cf0ad72266923e4eb19d85303af8677eb85df77
JB LM <lemetayer.jb@gmail.com>
2024-09-13 15:18:40 +0200

    pas de modification de reserve sur la page de validation drm pour eviter le conflit de formulaire

--- project/plugins/acVinDRMPlugin/modules/drm/templates/validationSuccess.php
+++ project/plugins/acVinDRMPlugin/modules/drm/templates/validationSuccess.php
@@ -99,7 +99,7 @@
                 <?php } ?>
 
                 <div id="contenu_onglet">
-                    <?php include_partial('drm/reserveinterpro', array('drm' => $drm)) ?>
+                    <?php include_partial('drm/reserveinterpro', array('drm' => $drm, 'hideFormReserve' => true)) ?>
                     <?php if(($drm->declaration->hasMouvement() && !$drm->declaration->hasStockEpuise()) || $drm->hasMouvementsCrd()):  ?>
                         <?php include_partial('drm/recap', array('drm' => $drm)) ?>
                 		<?php include_partial('drm/droits', array('drm' => $drm, 'circulation' => $droits_circulation, 'hide_cvo' => true)) ?>
