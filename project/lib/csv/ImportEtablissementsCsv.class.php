80b20a7fa5f33f7a8c3682e5610a7817c7c3b25e
JB LM <lemetayer.jb@gmail.com>
2024-10-25 11:07:03 +0200

    on ne genere pas de societe si elle existe

--- project/lib/csv/ImportEtablissementsCsv.class.php
+++ project/lib/csv/ImportEtablissementsCsv.class.php
@@ -355,11 +355,14 @@ class ImportEtablissementsCsv {
 		  				$etab->compte = $contrat->compte;
 		  			}
 		        $etab->save();
-            $societe = $etab->getGenerateSociete();
-            if ($societe && isset($line[EtablissementCsv::COL_NB_PAIEMENT_SV12]) && is_int($line[EtablissementCsv::COL_NB_PAIEMENT_SV12]) && $line[EtablissementCsv::COL_NB_PAIEMENT_SV12] > 1) {
+            $societe = $etab->getSociete();
+            if (!$societe) {
+                $societe = $etab->getGenerateSociete();
+            }
+            if (isset($line[EtablissementCsv::COL_NB_PAIEMENT_SV12]) && is_int($line[EtablissementCsv::COL_NB_PAIEMENT_SV12]) && $line[EtablissementCsv::COL_NB_PAIEMENT_SV12] > 1) {
                 $societe->setMetasForFacturation(FactureClient::TYPE_FACTURE_MOUVEMENT_SV12, [Societe::FACTURATION_NB_PAIEMENTS_NODE => round($line[EtablissementCsv::COL_NB_PAIEMENT_SV12])]);
             }
-            if ($societe && !$societe->getDataFromInterproMetas($this->_interpro->_id, 'code_comptable_client') && in_array($this->_interpro->_id, InterproClient::$_drm_interpros)) {
+            if (!$societe->getDataFromInterproMetas($this->_interpro->_id, 'code_comptable_client') && in_array($this->_interpro->_id, InterproClient::$_drm_interpros)) {
                 $cc = (strpos($societe->identifiant, '-') !== false)? substr($societe->identifiant, 0, strpos($societe->identifiant, '-')) : $societe->identifiant;
                 $societe->addCodeComptableClient($cc, $this->_interpro->_id);
             }
