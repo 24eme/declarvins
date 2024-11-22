98a8471585d5a9caa200e7fe1d3f2878985275f5
JB LM <lemetayer.jb@gmail.com>
2024-11-22 17:30:00 +0100

    on vire le checked par defaut qui n'a pas lieu d'etre

--- project/apps/declarvin/templates/_stronger_password.php
+++ project/apps/declarvin/templates/_stronger_password.php
@@ -44,7 +44,7 @@
         <li class="list-group-item" data-regex="[0-9]">Au moins 1 chiffre</li>
         <li class="list-group-item" data-regex="[A-Z]">Au moins 1 lettre majuscule</li>
         <li class="list-group-item" data-regex="[a-z]">Au moins 1 lettre minuscule</li>
-        <li class="list-group-item checked" data-regex="[^A-Za-z0-9]">Au moins 1 caractère spécial (@, #, $, ...)</li>
+        <li class="list-group-item" data-regex="[^A-Za-z0-9]">Au moins 1 caractère spécial (@, #, $, ...)</li>
     </ul>
 </div>
 
