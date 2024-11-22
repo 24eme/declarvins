d8ca00d3aea30c6cc74fd3e93b4fd9fc3647a539
JB LM <lemetayer.jb@gmail.com>
2024-11-22 15:24:16 +0100

    partial de controle de robustesse de mot de passe

---
+++ project/apps/declarvin/templates/_stronger_password.php
@@ -0,0 +1,11 @@
+<div class="password-rules">
+    <h2>Le mot de passe doit contenir</h2>
+    <ul class="list-group">
+        <li class="list-group-item"><?php echo $target ?></li>
+        <li class="list-group-item">8 caractères minimum</li>
+        <li class="list-group-item">Au moins 1 chiffre</li>
+        <li class="list-group-item">Au moins 1 lettre majuscule</li>
+        <li class="list-group-item">Au moins 1 lettre minuscule</li>
+        <li class="list-group-item">Au moins 1 caractère spécial (@, #, $, etc.)</li>
+    </ul>
+</div>
