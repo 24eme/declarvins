a07490d74bfd1e15dbbe976794d9eef65e85f75a
JB LM <lemetayer.jb@gmail.com>
2024-11-22 16:23:51 +0100

    regex a appliquer pour le controle

--- project/apps/declarvin/templates/_stronger_password.php
+++ project/apps/declarvin/templates/_stronger_password.php
@@ -40,11 +40,10 @@
 <div class="password-rules">
     <h2>Le mot de passe doit contenir</h2>
     <ul class="list-group">
-        <li class="list-group-item"><?php echo $target ?></li>
-        <li class="list-group-item">8 caractères minimum</li>
-        <li class="list-group-item">Au moins 1 chiffre</li>
-        <li class="list-group-item">Au moins 1 lettre majuscule</li>
-        <li class="list-group-item">Au moins 1 lettre minuscule</li>
-        <li class="list-group-item">Au moins 1 caractère spécial (@, #, $, etc.)</li>
+        <li class="list-group-item" data-regex="/.{8,}/">8 caractères minimum</li>
+        <li class="list-group-item" data-regex="/[0-9]/">Au moins 1 chiffre</li>
+        <li class="list-group-item" data-regex="/[A-Z]/">Au moins 1 lettre majuscule</li>
+        <li class="list-group-item" data-regex="/[a-z]/">Au moins 1 lettre minuscule</li>
+        <li class="list-group-item checked" data-regex="[^A-Za-z0-9]">Au moins 1 caractère spécial (@, #, $, ...)</li>
     </ul>
 </div>
