23109b910f84bded1bcfaa312a6e22c617aeceba
JB LM <lemetayer.jb@gmail.com>
2024-11-25 16:03:47 +0100

    meilleur placement du bouton de reinitialisation du mdp

--- project/apps/declarvin/modules/ComptePartenaire/templates/_formCompteModification.php
+++ project/apps/declarvin/modules/ComptePartenaire/templates/_formCompteModification.php
@@ -1,16 +1,16 @@
 <form method="post" action="<?php echo url_for(array('sf_route' => 'compte_partenaire_modification', 'login' => $form->getObject()->login)); ?>">
-    
+
+    <div class="ligne_form reinit_mdp">
+        <a href="<?php echo url_for('partenaire_compte_password', array('login' => $form->getObject()->login)) ?>" class="btn_orange" style="float: right;">Envoyer une demande de redéfinition de mot de passe</a>
+    </div>
+
     <div class="ligne_form">
         <label>Login</label>
         <span><?php echo $form->getObject()->login ?></span>
     </div>
-    <?php 
+    <?php
     include_partial('ComptePartenaire/formCompteRenderer', array('form' => $form));
     ?>
-
-    <div class="ligne_form reinit_mdp">
-        <a href="<?php echo url_for('partenaire_compte_password', array('login' => $form->getObject()->login)) ?>" class="btn_orange">Redéfinir mon mot de passe</a>
-    </div>
 
     <div class="ligne_form ligne_entiere ecart_check">
         <?php echo $form['contrat']->renderLabel() ?>
@@ -23,7 +23,7 @@
         <?php echo $form['acces']->render() ?>
         <?php echo $form['acces']->renderError() ?>
     </div>
-    
+
     <div class="btnValidation">
         <input class="btn_valider" type="submit" value="Modifier"/>
     </div>
--- project/apps/declarvin/modules/acVinCompteAdmin/templates/_formCompteModification.php
+++ project/apps/declarvin/modules/acVinCompteAdmin/templates/_formCompteModification.php
@@ -1,16 +1,17 @@
 <form method="post" action="<?php echo url_for(array('sf_route' => 'compte_modification', 'login' => $form->getObject()->login)); ?>">
-    
+
+
+    <div class="ligne_form reinit_mdp">
+        <a href="<?php echo url_for('admin_compte_password', array('login' => $form->getObject()->login)) ?>" class="btn_orange" style="float: right;">Envoyer une demande de redéfinition de mot de passe</a>
+    </div>
+
     <div class="ligne_form">
         <label>Login</label>
         <span><strong><?php echo $form->getObject()->login ?></strong></span>
     </div>
-    <?php 
+    <?php
     include_partial('acVinCompteAdmin/formCompteRenderer', array('form' => $form));
     ?>
-
-    <div class="ligne_form reinit_mdp">
-        <a href="<?php echo url_for('admin_compte_password', array('login' => $form->getObject()->login)) ?>" class="btn_orange">Redéfinir mon mot de passe</a>
-    </div>
 
     <div class="ligne_form ligne_entiere ecart_check">
         <?php echo $form['droits']->renderLabel() ?>
@@ -23,7 +24,7 @@
         <?php echo $form['acces']->render() ?>
         <?php echo $form['acces']->renderError() ?>
     </div>
-    
+
     <div class="btnValidation">
         <input class="btn_valider" type="submit" value="Modifier"/>
     </div>
