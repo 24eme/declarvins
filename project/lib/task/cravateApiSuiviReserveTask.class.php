4f4162d420e686875bb6fa76957bae23b86ed269
JB LM <lemetayer.jb@gmail.com>
2025-02-20 21:11:21 +0100

    ajout des contrats dans les metas des remontées d'infos pour cravate

--- project/lib/task/cravateApiSuiviReserveTask.class.php
+++ project/lib/task/cravateApiSuiviReserveTask.class.php
@@ -31,6 +31,8 @@ class cravateApiSuiviReserveTask extends sfBaseTask
             'EMAIL' => null,
             'TELEPHONE' => null
         ];
+        $contrats = [];
+
         $etablissement = EtablissementClient::getInstance()->find($arguments['identifiant']);
         if (!$etablissement) {
             $etablissements = EtablissementIdentifiantView::getInstance()->findByIdentifiant($arguments['identifiant'])->rows;
@@ -46,7 +48,29 @@ class cravateApiSuiviReserveTask extends sfBaseTask
             $result['TELEPHONE'] = $etablissement->email;
         }
 
-        echo json_encode(['form' => $result], JSON_PRETTY_PRINT);
+        $vracs = VracAllView::getInstance()->findByEtablissement($etablissement->identifiant);
+        foreach ($vracs->rows as $c) {
+            if ($c->key[VracAllView::VRAC_VIEW_STATUT] == VracClient::STATUS_CONTRAT_NONSOLDE) {
+                if (!isset($contrats[$c->key[VracAllView::VRAC_VIEW_PRODUIT]])) {
+                    $contrats[$c->key[VracAllView::VRAC_VIEW_PRODUIT]] = [];
+                }
+                $vrac = VracClient::getInstance()->find($c->key[VracAllView::VRAC_VIEW_ID]);
+                if ($vrac->type_transaction != 'vrac') {
+                    continue;
+                }
+                $contrats[$c->key[VracAllView::VRAC_VIEW_PRODUIT]][$vrac->_id] = [
+                    'NUMVISA' => $vrac->numero_contrat,
+                    'PRODUIT_LIBELLE' => trim($vrac->produit_libelle),
+                    'LABELS' => $vrac->labels_libelle,
+                    'MILLESIME' => $vrac->millesime,
+                    'VOLUME' => $vrac->volume_propose,
+                    'URL' => ProjectConfiguration::getAppRouting()->generate('vrac_visualisation', ['sf_subject' => $vrac, 'etablissement' => $etablissement->identifiant], true)
+                ];
+           }
+
+        }
+
+        echo json_encode(['form' => $result, 'CONTRAT_VRAC' => $contrats], JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
     }
 
 }
