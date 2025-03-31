e9398494a3b3cdea07cd910d35b568c8498f476a
JB LM <lemetayer.jb@gmail.com>
2025-03-31 11:04:18 +0200

    on retire la clause clause_initiative_contractuelle_producteur pour le civp

--- project/plugins/VracPlugin/lib/form/VracClauseCivpForm.class.php
+++ project/plugins/VracPlugin/lib/form/VracClauseCivpForm.class.php
@@ -4,6 +4,6 @@ class VracClauseCivpForm extends VracClauseForm
 {
     public function configure() {
         parent::configure();
-        unset($this['autres_conditions']);
+        unset($this['autres_conditions'], $this['clause_initiative_contractuelle_producteur']);
     }
 }
