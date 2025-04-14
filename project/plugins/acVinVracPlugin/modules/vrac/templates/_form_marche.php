9e8d75860fb909d3ae1d1a80618dec3dcb1e581a
JB LM <lemetayer.jb@gmail.com>
2025-04-14 11:44:24 +0200

    il faut au moins un prix plancher pour afficher l'intervalle de prix

--- project/plugins/acVinVracPlugin/modules/vrac/templates/_form_marche.php
+++ project/plugins/acVinVracPlugin/modules/vrac/templates/_form_marche.php
@@ -49,7 +49,7 @@
                 <?php echo $form['volume_propose']->renderLabel() ?>
                 <?php echo $form['volume_propose']->render() ?> <strong><?php if($form->getObject()->type_transaction == 'raisin'): ?>Kg<?php else: ?>HL<?php endif; ?></strong>
             </div>
-            <?php if($form->getObject()->isAdossePluriannuel()): ?>
+            <?php if($form->getObject()->isAdossePluriannuel() && $form->getObject()->pluriannuel_prix_plancher): ?>
                 <p style="padding-left: 210px;">
                     <svg style="vertical-align: -.35em;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
                       <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
