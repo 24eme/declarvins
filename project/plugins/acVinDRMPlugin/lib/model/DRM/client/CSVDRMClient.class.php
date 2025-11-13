dd046b729147d3edd5ffd552cbc0ecc47983500f
Tangui Morlier <tangui@tangui.eu.org>
2025-11-13 12:36:19 +0100

    Gestion de l'impact de l'héritage de CSVClient

--- project/plugins/acVinDRMPlugin/lib/model/DRM/client/CSVDRMClient.class.php
+++ project/plugins/acVinDRMPlugin/lib/model/DRM/client/CSVDRMClient.class.php
@@ -2,6 +2,10 @@
 
 class CSVDRMClient extends CSVClient {
 
+    public static function getInstance() {
+        return acCouchdbManager::getClient("CSVDRM");
+    }
+
     public function findCSVDRM($identifiant, $from = "0000-00", $hydrate = acCouchdbClient::HYDRATE_DOCUMENT) {
         return $this
             ->startkey(sprintf("CSV-DRM-%s-%s", $identifiant, $from))
