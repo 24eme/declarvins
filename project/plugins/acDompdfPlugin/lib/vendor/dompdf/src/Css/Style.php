b3b6b6b569898260a3f170d98ec66ded6da7ab2c
Tangui Morlier <tangui@tangui.eu.org>
2026-08-11 11:43:00 +0200

    PHP8

--- project/plugins/acDompdfPlugin/lib/vendor/dompdf/src/Css/Style.php
+++ project/plugins/acDompdfPlugin/lib/vendor/dompdf/src/Css/Style.php
@@ -3083,7 +3083,7 @@ class Style
      */
     function set_z_index($val)
     {
-        if (round($val) != $val && $val !== "auto") {
+        if ($val !== "auto" && round($val) != $val) {
             return;
         }
 
