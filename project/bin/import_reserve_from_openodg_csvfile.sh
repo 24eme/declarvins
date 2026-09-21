1a492c49b2bf02655cf99fe9abc09e0094bb1060
JB LM <lemetayer.jb@gmail.com>
2026-09-21 17:21:56 +0200

    demarrage script d'import auto des reserves depuis les donnees openodg

---
+++ project/bin/import_reserve_from_openodg_csvfile.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+. bin/config.inc
+
+if [[ $# -ne 2 ]]; then
+    echo "Arguments manquants : $0 <interpro> <openodg_reserve_csvfile>"
+    exit 1
+fi
+
+interpro="$1"
+csvfile="$2"
+
+if [[ -z "$csvfile" ]]; then
+    echo "Erreur : aucun fichier spécifié"
+    exit 1
+fi
+
+if [[ ! -f "$csvfile" ]]; then
+    echo "Erreur : le fichier '$csvfile' n'existe pas"
+    exit 1
+fi
+
+TMPI=$TMP/$interpro
+mkdir -p $TMPI
+
+awk -F';' -v OFS=';' 'NR > 1 {
+hash = "declaration/certifications/" $11 "/genres/" $13 "/appellations/" $15 "/mentions/DEFAUT/lieux/" $17 "/couleurs/" $19 "/cepages/" $21
+print $3, $2, hash, substr($1, 1, 4), $27
+}' "$csvfile" > "$TMPI/reserves.csv"
+
+echo php symfony import:reserve-interpro $SYMFONYTASKOPTIONS --interpro="$interpro" --checking="1" "$TMPI/reserves.csv"
+
+rm "$TMPI/reserves.csv"
